7f57434883d7001b46788ac8f9c43a7dadd9e716
sunhpark42 <sunhpark42@gmail.com>
2020-12-10 03:36:54 +0900

    feat: add div in MenuBottonList for subview

--- src/Menu/MenuView.js
+++ src/Menu/MenuView.js
@@ -5,6 +5,7 @@ export default class MenuView {
     <button id="line-manager-button">2. 노선 관리</button>
     <button id="section-manager-button">3. 구간 관리</button>
     <button id="map-print-manager-button">4. 지하철 노선도 출력</button>
+    <div id="sub-view-container"></div>
     `;
   }
 
