85f273743ac25de31e37b00a197a2688454dbd7c
sunhpark42 <sunhpark42@gmail.com>
2020-12-10 03:39:14 +0900

    feat: add Station Manager subView under Menus

--- src/Menu/MenuView.js
+++ src/Menu/MenuView.js
@@ -9,9 +9,27 @@ export default class MenuView {
     `;
   }
 
-  StationManagerView() {
-
+  static StationManagerView() {
+    document.getElementById('sub-view-container').innerHTML = `
+    <h4>역 이름</h4>
+    <form>
+      <input type='text' place-holder='역 이름을 입력해주세요.'/>
+      <button id='station-manager-button'>역 추가</button>
+    </form>
+    <h2>🚉 지하철 역 목록</h2>
+    <table border='1px solid black' width='30%'>
+      <tr>
+        <th align='center'>역 이름</th>
+        <th align='center'>설정</th>
+      </tr>
+      <tr>
+        <td align="center">hello</td>
+        <td align="center"><button>삭제</button></td>
+      </tr>
+    </table>
+    `;
   }
+
   LineManagerView() {
 
   }
