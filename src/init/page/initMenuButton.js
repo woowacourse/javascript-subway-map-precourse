29ecac0e3ab8b1a71c22c797cf7c0ca33b787af6
yujo <yujo@student.42seoul.kr>
2020-12-10 01:28:15 +0900

    feat: 지하철 역 관리에 필요한 요소들 구현

--- src/init/page/initMenuButton.js
+++ src/init/page/initMenuButton.js
@@ -4,7 +4,8 @@ function menuButton() {
             <button id="line-manager-button">2. 노선 관리</button>
             <button id="section-manager-button">3. 구간 관리</button>
             <button id="map-print-manager-button">4. 지하철 노선도 출력</button>
-          </div>`;
+          </div>
+          <br>`;
 }
 
 export default function initMenuButton() {
--- src/init/page/manager-page/initStationManagerPage.js
+++ src/init/page/manager-page/initStationManagerPage.js
@@ -1,5 +1,24 @@
+// eslint-disable-next-line max-lines-per-function
 function stationManagerPage() {
-  return `<div class="station-manager-page" hidden>Station Manger Page</div>`;
+  return `<div class="station-manager-page" hidden>
+            <div>
+              역 이름
+              <div class="station-input-container">
+                <input
+                  id="station-name-input"
+                  placeholder="역 이름을 입력해주세요."
+                />
+                <button id="station-add-button">역 추가</button>
+              </div>
+            </div>
+            <h3>🚉 지하철 역 목록</h3>
+            <table border="1">
+              <tr>
+                <th>역 이름</th>
+                <th>설정</th>
+              </tr>
+            </table>
+          </div>`;
 }
 
 export default function initStationManagerPage() {
