3f655825fdedba649273531242625dc4667a4db8
HyuuunjuKim <gus7wn@gmail.com>
2020-12-09 20:11:44 +0900

    :sparkles: 지하철 역 추가 완료시 input 초기화

--- src/station-manager.js
+++ src/station-manager.js
@@ -2,7 +2,6 @@ export const addStationtoList = (stationList) => {
   const newRow = document.createElement("tr");
   const nameCol = document.createElement("td");
   const deleteCol = document.createElement("td");
-
   nameCol.innerHTML = stationList;
   deleteCol.innerHTML = '<button class="station-delete-class">삭제</button>';
   newRow.appendChild(nameCol);
@@ -10,4 +9,5 @@ export const addStationtoList = (stationList) => {
 
   const table = document.getElementById("staion-list-table");
   table.children[1].appendChild(newRow);
+  document.getElementById("station-name-input").value = "";
 };
