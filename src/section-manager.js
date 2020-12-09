0dd0d14159523f8af0943c8768bcdfb9b8ea3fd4
HyuuunjuKim <gus7wn@gmail.com>
2020-12-09 23:07:56 +0900

    :bug: 구간 관리 선택 할 때마다 메뉴 늘어나는 버그 수정

--- src/section-manager.js
+++ src/section-manager.js
@@ -1,8 +1,9 @@
 export const showLineList = (lineList) => {
   console.log(lineList);
+  const lineMenu = document.getElementById("section-line-list");
+  lineMenu.innerHTML = "";
   lineList.forEach((line) => {
     const lineMenuButton = document.createElement("button");
-    const lineMenu = document.getElementById("section-line-list");
     lineMenuButton.setAttribute("class", "section-line-menu-button");
     lineMenuButton.setAttribute("id", `${line.name}`);
     lineMenuButton.innerHTML = line.name;
