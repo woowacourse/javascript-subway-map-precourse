46ef53653e8403a61f7306d3fd55ea6764677a1c
HyuuunjuKim <gus7wn@gmail.com>
2020-12-09 20:15:14 +0900

    :sparkles: addStationtoList 내 변수명 변경

--- src/station-manager.js
+++ src/station-manager.js
@@ -1,13 +1,13 @@
-export const addStationtoList = (stationList) => {
-  const newRow = document.createElement("tr");
-  const nameCol = document.createElement("td");
-  const deleteCol = document.createElement("td");
-  nameCol.innerHTML = stationList;
-  deleteCol.innerHTML = '<button class="station-delete-class">삭제</button>';
-  newRow.appendChild(nameCol);
-  newRow.appendChild(deleteCol);
+export const addStationtoList = (newStationName) => {
+  const newStation = document.createElement("tr");
+  const stationName = document.createElement("td");
+  const deleteButton = document.createElement("td");
+  stationName.innerHTML = newStationName;
+  deleteButton.innerHTML = '<button class="station-delete-class">삭제</button>';
+  newStation.appendChild(stationName);
+  newStation.appendChild(deleteButton);
 
   const table = document.getElementById("staion-list-table");
-  table.children[1].appendChild(newRow);
+  table.children[1].appendChild(newStation);
   document.getElementById("station-name-input").value = "";
 };
