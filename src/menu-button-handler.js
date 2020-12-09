7a79e645eb17018a4fb81238c6c78d9f17d24b7b
Tanney-102 <dhxodms89@gmail.com>
2020-12-09 23:13:37 +0900

    Add function that shows name input form upon click station manager button

--- src/menu-button-handler.js
+++ src/menu-button-handler.js
@@ -1,19 +1,42 @@
-// const MANAGERS_ID = [
-//   "station-manager",
-//   "line-manager",
-//   "section-manager",
-//   "map-print-manager",
-// ];
+const BUTTONS_ID = [
+  "station-manager-button",
+  "line-manager-button",
+  "section-manager-button",
+  "map-print-manager-button",
+];
 
-// const getChildById = (parentElement, id) => {
-//   const childrenList = parentElement.children;
-//   for (let i = 0; i < childrenList.length; i++) {
-//     if (childrenList[i].id === id) {
-//       return childrenList[i];
-//     }
-//   }
+const getChildById = (parentElement, id) => {
+  const childrenList = parentElement.children;
+  for (let i = 0; i < childrenList.length; i++) {
+    if (childrenList[i].id === id) {
+      return childrenList[i];
+    }
+  }
 
-//   return null;
-// };
+  return null;
+};
 
-export function menuButtonHandler() {}
+const renderStationAddForm = (container) => {
+  container.innerHTML += `
+    <label for="station-name-input">
+      역 이름<br />
+      <input type="text" id="station-name-input" placeholder="역 이름을 입력해주세요." />
+    </label>
+    <button id="station-add-button">역 추가</button>
+  `;
+};
+
+const showStationManagerPage = (e) => {
+  const $mainContentsContainer = getChildById(
+    e.target.closest("#app"),
+    "main-contents-container"
+  );
+  $mainContentsContainer.innerHTML = "";
+  renderStationAddForm($mainContentsContainer);
+};
+
+export default function menuButtonHandler(e) {
+  if (e.target.id === BUTTONS_ID[0]) {
+    showStationManagerPage(e);
+  }
+}
