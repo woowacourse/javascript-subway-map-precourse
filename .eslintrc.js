1a03492863c3cd577ada64d655808f7b8f6a52f9
임광열 <mkitigy@gmail.com>
2020-12-09 18:13:31 +0900

    chore: update .eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,23 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "rules": {
-    }
+  plugins: ["prettier"],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  rules: {
+    "no-new": "off",
+    "class-methods-use-this": "off",
+    "import/no-named-as-default": "off",
+    "no-underscore-dangle": "off",
+    "no-console": "off",
+    "no-alert": "off",
+    "max-depth": ["error", 2],
+    "max-lines-per-function": ["error", 15],
+  },
 };
