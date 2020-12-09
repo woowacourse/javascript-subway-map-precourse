c723ec88aeb3e22b90a0014b6df0d202daebffd6
Tanney-102 <dhxodms89@gmail.com>
2020-12-09 22:52:42 +0900

    Remove function 'showManagerPageById' & Reset function menuButtonHandler

--- src/menu-button-handler.js
+++ src/menu-button-handler.js
@@ -1,42 +1,19 @@
-const MANAGERS_ID = [
-  "station-manager",
-  "line-manager",
-  "section-manager",
-  "map-print-manager",
-];
+// const MANAGERS_ID = [
+//   "station-manager",
+//   "line-manager",
+//   "section-manager",
+//   "map-print-manager",
+// ];
 
-const getChildById = (parentElement, id) => {
-  const childrenList = parentElement.children;
-  for (let i = 0; i < childrenList.length; i++) {
-    if (childrenList[i].id === id) {
-      return childrenList[i];
-    }
-  }
+// const getChildById = (parentElement, id) => {
+//   const childrenList = parentElement.children;
+//   for (let i = 0; i < childrenList.length; i++) {
+//     if (childrenList[i].id === id) {
+//       return childrenList[i];
+//     }
+//   }
 
-  return null;
-};
+//   return null;
+// };
 
-const showManagerPageById = (container, id) => {
-  MANAGERS_ID.forEach((_managersId) => {
-    if (_managersId === id) {
-      getChildById(container, _managersId).style.display = "block";
-    } else {
-      getChildById(container, _managersId).style.display = "none";
-    }
-  });
-};
-
-export function menuButtonHandler(e) {
-  const app = e.target.closest("#app");
-  const id = e.target.id;
-
-  if (e.target.id === "station-manager-button") {
-    showManagerPageById(app, "station-manager");
-  } else if (id === "line-manager-button") {
-    showManagerPageById(app, "line-manager");
-  } else if (id === "section-manager-button") {
-    showManagerPageById(app, "section-manager");
-  } else if (id === "map-print-manager-button") {
-    showManagerPageById(app, "map-print-manager");
-  }
-}
+export function menuButtonHandler() {}
