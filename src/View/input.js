fd7a38e0a1b9eceea0cb8f81b1dd46b1ba7cffe8
unknown <dudtjr913@naver.com>
2020-12-11 01:32:12 +0900

    Feat : 버튼 클릭 시 화면에 출력 함수 구현 중
    <description>
    display : none으로 되어 있는 section들을 버튼을 클릭하면 그에 맞는
    section이 display:block으로 바뀌면서 화면에 나타난다.

---
+++ src/View/input.js
@@ -0,0 +1,12 @@
+import {hideScreen} from './hide-screen.js';
+import {showScreen} from './show-screen.js';
+
+export const $screenButton = document.body.querySelectorAll('#app > button');
+export const $stationContainer = document.body.querySelector('#station');
+export const $lineContainer = document.body.querySelector('#line');
+export const $sectionContainer = document.body.querySelector('#subway-section');
+export const $mapContainer = document.body.querySelector('#map-section');
+
+Array.from($screenButton).forEach((button) =>
+  button.addEventListener('click', showScreen),
+);
---
+++ src/View/show-screen.js
@@ -0,0 +1,21 @@
+import {
+  $stationContainer,
+  $lineContainer,
+  $sectionContainer,
+  $mapContainer,
+} from './input.js';
+
+export const showScreen = (e) => {
+  if (e.target.id === 'station-manager-button') {
+    return ($stationContainer.style.display = 'block');
+  }
+  if (e.target.id === 'line-manager-button') {
+    return ($lineContainer.style.display = 'block');
+  }
+  if (e.target.id === 'section-manager-button') {
+    return ($sectionContainer.style.display = 'block');
+  }
+  if (e.target.id === 'map-print-manager-button') {
+    return ($mapContainer.style.display = 'block');
+  }
+};
