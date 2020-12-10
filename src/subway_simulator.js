06f117e797516e8b17efe0b792b99036aa2a38e4
dawit jeong <wjdekdnlt1215@gmail.com>
2020-12-11 02:46:56 +0900

    add clearResultDIV

--- src/subway_simulator.js
+++ src/subway_simulator.js
@@ -1,11 +1,17 @@
 import { getLocalStorageData } from "./utils.js";
 const SubwaySimulator = function () {
+  const resultDIV = document.getElementById("result");
+
   const [stations, lines] = getLocalStorageData();
   this.stations = stations;
   this.lines = lines;
 
   this.currentPage = "0"; // "1": 역 관리, "2": "노선 관리", "3": 구간 관리, "4": 지하철 노선도 관리
   this.isAlreadyRenderedPage = (page) => this.currentPage === page;
+
+  this.clearResultDIV = () => {
+    resultDIV.innerHTML = "";
+  };
 };
 
-export const { isAlreadyRenderedPage } = new SubwaySimulator();
+export const { clearResultDIV, isAlreadyRenderedPage } = new SubwaySimulator();
