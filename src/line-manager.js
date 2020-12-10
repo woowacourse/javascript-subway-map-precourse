77e450e08f42b8bbcf12c642827e349a1d1f8b2e
HyuuunjuKim <gus7wn@gmail.com>
2020-12-10 23:57:43 +0900

    :sparkles: 노선 삭제하기 구현 및 함수 분리

--- src/line-manager.js
+++ src/line-manager.js
@@ -1,5 +1,18 @@
-export const addLineToList = (line) => {
-  const newLine = document.createElement("tr");
+import { manager } from "./index.js";
+
+export const deleteLineInList = (lineName) => {
+  const parent = document.querySelector("table#line-list-table tbody");
+  const deleteIdx = manager.lineList.findIndex((line) => {
+    return line.name === lineName;
+  });
+  parent.removeChild(
+    document.querySelector(`table#line-list-table tbody tr#${lineName}`)
+  );
+  console.log(manager.lineList);
+  manager.lineList.splice(deleteIdx, 1);
+  console.log(manager.lineList);
+};
+export const makeChildInLine = (line) => {
   const lineName = document.createElement("td");
   const startStation = document.createElement("td");
   const endStation = document.createElement("td");
@@ -7,12 +20,31 @@ export const addLineToList = (line) => {
   lineName.innerHTML = line.name;
   startStation.innerHTML = line.getStartStation();
   endStation.innerHTML = line.getEndStation();
-  deleteButton.innerHTML = `<button class="line-delete-button">삭제</button>`;
+  deleteButton.innerHTML = "삭제";
+  deleteButton.setAttribute("class", "line-delete-button");
+  deleteButton.onclick = () => {
+    deleteLineInList(`${line.name}`);
+  };
+  deleteButton.innerHTML = "삭제";
+
+  return [lineName, startStation, endStation, deleteButton];
+};
+export const makeLineBox = (line) => {
+  const lineName = makeChildInLine(line)[0];
+  const startStation = makeChildInLine(line)[1];
+  const endStation = makeChildInLine(line)[2];
+  const deleteButton = makeChildInLine(line)[3];
+  const newLine = document.createElement("tr");
+  newLine.setAttribute("id", `${line.name}`);
   newLine.appendChild(lineName);
   newLine.appendChild(startStation);
   newLine.appendChild(endStation);
   newLine.appendChild(deleteButton);
 
+  return newLine;
+};
+export const addLineToList = (line) => {
+  const newLine = makeLineBox(line);
   const table = document.getElementById("line-list-table");
   table.children[1].appendChild(newLine);
   document.getElementById("line-name-input").value = "";
