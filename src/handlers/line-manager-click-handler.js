a220374d1518103dbd1359d207120dec2eac78c6
Tanney-102 <dhxodms89@gmail.com>
2020-12-10 23:14:36 +0900

    Create line manager click handler

---
+++ src/handlers/line-manager-click-handler.js
@@ -0,0 +1,10 @@
+import { ADD_BUTTONS_ID } from "../html-constants/html-id-values.js";
+// import { DELETE_BUTTONS_CLASS } from "../html-constants/html-classnames.js";
+
+// const lineAddButtonHandler = (e) => {};
+
+export default function lineManagerClickHandler(e) {
+  if (e.target.id === ADD_BUTTONS_ID.lineAddButton) {
+    // lineAddButtonHandler(e);
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import menuButtonHandler from "./handlers/menu-button-handler.js";
 import stationManagerClickHandler from "./handlers/station-manager-click-handler.js";
+import lineManagerClickHandler from "./handlers/line-manager-click-handler.js";
 
 document.addEventListener("click", menuButtonHandler);
 document.addEventListener("click", stationManagerClickHandler);
+document.addEventListener("click", lineManagerClickHandler);
