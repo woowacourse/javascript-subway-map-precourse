9524673ebd05529b26c7e2e9d7c9ea7847ee0e43
Tanney-102 <dhxodms89@gmail.com>
2020-12-10 12:26:02 +0900

    Add methods which get and update station data in localStroage

--- src/station-manager.js
+++ src/station-manager.js
@@ -1,5 +1,18 @@
 export default class StationManager {
   constructor() {
     this.stationList = [];
+    this.setStationList();
+  }
+
+  setStationList() {
+    const storedStationList = localStorage.stationList;
+    if (storedStationList !== undefined) {
+      this.stationList = JSON.parse(storedStationList);
+    }
+  }
+
+  addStation(station) {
+    this.stationList.push(station);
+    localStorage.stationList = JSON.stringify(this.stationList);
   }
 }
