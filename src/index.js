deb50e7d8a8846e021c37044c58cb1edde3d6c4e
Daniel <elrion018@gmail.com>
2020-12-10 20:26:26 +0900

    feat(SubwayMapView) : Add addEventListenerToStationManagerButton function

--- src/index.js
+++ src/index.js
@@ -3,7 +3,14 @@ import { SubwayMapViewModel } from './viewModels';
 import { SubwayMapModel } from './models';
 
 window.addEventListener('DOMContentLoaded', () => {
+  const stationManagerButton = document.getElementById(
+    '#station-manager-button',
+  );
+
   const subwayMapModel = new SubwayMapModel();
   const subwayMapViewModel = new SubwayMapViewModel(subwayMapModel);
-  const subwayMapView = new SubwayMapView(subwayMapViewModel);
+  const subwayMapView = new SubwayMapView(
+    subwayMapViewModel,
+    stationManagerButton,
+  );
 });
--- src/views/SubwayMapView/index.js
+++ src/views/SubwayMapView/index.js
@@ -1,5 +1,17 @@
 export default class SubwayMapView {
-  constructor(subwayMapViewModel) {
+  constructor(subwayMapViewModel, stationManagerButton) {
     this.subwayMapViewModel = subwayMapViewModel;
+    this.stationManagerButton = stationManagerButton;
+
+    this.addEventListenerToStationManagerButton(this);
   }
+
+  addEventListenerToStationManagerButton(self) {
+    this.stationManagerButton.addEventListener(
+      'click',
+      this.renderStationManager.bind(self),
+    );
+  }
+
+  renderStationManager() {}
 }
