9d62a567ef958a27ec033655285ab7f678ff2603
seojihwan <seojihwan213@naver.com>
2020-12-10 21:37:52 +0900

    feat: 지하철 노선도 관리 객체 정의

---
+++ src/index.js
@@ -0,0 +1,22 @@
+export default function SubwayMapManagement() {
+  this.subwayStations = [];
+  this.subwayLines = [];
+
+  this.addSubwayStation = () => {};
+
+  this.deleteSubwayStation = () => {};
+
+  this.getSubwayStations = () => {};
+
+  this.addSubwayLine = () => {};
+
+  this.deleteSubwayLine = () => {};
+
+  this.getSubwayLines = () => {};
+
+  this.addSubwaySection = () => {};
+
+  this.deleteSubwaySection = () => {};
+
+  this.getSubwayMap = () => {};
+}
