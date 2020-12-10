bbb51cf3423b159a979cdd651e6d27f7d44315bf
ChayanRin <k852012@naver.com>
2020-12-10 18:43:39 +0900

    feat: implement addStation

--- src/station.js
+++ src/station.js
@@ -4,6 +4,14 @@ class Station {
     this.handleAddNameClick();
   }
 
+  addStation = () => {
+    const name = this.getNameInput();
+
+    if (this.checkVaildName(name)) {
+      this.stations.push(name);
+    }
+  };
+
   handleAddNameClick = () => {
     const stationAddBtn = document.getElementById("station-add-button");
     stationAddBtn.addEventListener("click", this.addStation);
