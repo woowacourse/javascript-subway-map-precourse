17d9fa26d54d4f2068234d90ccef4a80c66f49fd
yujo <yujo@student.42seoul.kr>
2020-12-10 20:48:20 +0900

    refactor: 역 이름을 저장할 때 템플릿이 아닌 String값만 저장하도록 변경

--- src/setState.js
+++ src/setState.js
@@ -1,20 +1,13 @@
-function stationTemplate(text) {
-  return `<div>${text}</div>`;
-}
-
 function addStationState() {
   const inputStationName = document.querySelector('#station-name-input').value;
   const stations = JSON.parse(localStorage.getItem('stations'));
 
   if (!stations) {
-    return localStorage.setItem(
-      'stations',
-      JSON.stringify(stationTemplate(inputStationName)),
-    );
+    return localStorage.setItem('stations', JSON.stringify(inputStationName));
   }
   return localStorage.setItem(
     'stations',
-    JSON.stringify(stations + stationTemplate(inputStationName)),
+    JSON.stringify(`${stations} ${inputStationName}`),
   );
 }
 
