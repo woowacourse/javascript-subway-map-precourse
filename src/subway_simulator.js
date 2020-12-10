eae48a6f680f55fa544b05a2a8c213af35dbd28d
dawit jeong <wjdekdnlt1215@gmail.com>
2020-12-11 02:46:14 +0900

    add currentPage,isAlreadyRenderedPage

--- src/subway_simulator.js
+++ src/subway_simulator.js
@@ -3,6 +3,9 @@ const SubwaySimulator = function () {
   const [stations, lines] = getLocalStorageData();
   this.stations = stations;
   this.lines = lines;
+
+  this.currentPage = "0"; // "1": 역 관리, "2": "노선 관리", "3": 구간 관리, "4": 지하철 노선도 관리
+  this.isAlreadyRenderedPage = (page) => this.currentPage === page;
 };
 
-export const {} = new SubwaySimulator();
+export const { isAlreadyRenderedPage } = new SubwaySimulator();
