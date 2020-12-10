983acc34cf072056661979157701b09aede71858
ChayanRin <k852012@naver.com>
2020-12-10 18:39:37 +0900

    feat: implement handleAddNameClick

--- src/station.js
+++ src/station.js
@@ -1,7 +1,13 @@
 class Station {
   constructor() {
     this.stations = [];
+    this.handleAddNameClick();
   }
+
+  handleAddNameClick = () => {
+    const stationAddBtn = document.getElementById("station-add-button");
+    stationAddBtn.addEventListener("click", this.addStation);
+  };
 }
 
 export default Station;
