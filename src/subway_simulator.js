7df8fab554d59fc131b58499d14e7b88ac514ee1
dawit jeong <wjdekdnlt1215@gmail.com>
2020-12-11 02:36:06 +0900

    add stations,lines at SubwaySimulator

--- src/subway_simulator.js
+++ src/subway_simulator.js
@@ -1,1 +1,8 @@
-const SubwaySimulator = function () {};
+import { getLocalStorageData } from "./utils.js";
+const SubwaySimulator = function () {
+  const [stations, lines] = getLocalStorageData();
+  this.stations = stations;
+  this.lines = lines;
+};
+
+export const {} = new SubwaySimulator();
