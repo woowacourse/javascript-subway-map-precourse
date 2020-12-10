097e345b5ded2705e8655ec817a4c37186ff454b
Tanney-102 <dhxodms89@gmail.com>
2020-12-10 13:45:07 +0900

    Rename 'click-event-handler.js => menu-button-handler.js'

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { menuButtonHandler, addButtonHandler } from "./click-event-handler.js";
+import { menuButtonHandler, addButtonHandler } from "./menu-button-handler.js";
 
 document.addEventListener("click", menuButtonHandler);
 document.addEventListener("click", addButtonHandler);
--- src/menu-button-handler.js
+++ src/menu-button-handler.js
@@ -1,28 +1,34 @@
-const BUTTONS_ID = [
-  "station-manager-button",
-  "line-manager-button",
-  "section-manager-button",
-  "map-print-manager-button",
-];
+import StationManager from "./station-manager.js";
+import { getChildById } from "./custom-dom-handler.js";
 
-const getChildById = (parentElement, id) => {
-  const childrenList = parentElement.children;
-  for (let i = 0; i < childrenList.length; i++) {
-    if (childrenList[i].id === id) {
-      return childrenList[i];
-    }
-  }
+const MENU_BUTTONS_ID = {
+  stationManagerButton: "station-manager-button",
+  lineManagerButton: "line-manager-button",
+  sectionManagerButton: "section-manager-button",
+  mapPrintManagerButton: "map-print-manager-button",
+};
 
-  return null;
+const ADD_BUTTONS_ID = {
+  stationAddButton: "station-add-button",
+  lineAddButton: "line-add-button",
+  sectionAddButton: "section-add-button",
+};
+
+const INPUT_FORM_ID = {
+  stationNameInput: "station-name-input",
+  lineNameInput: "line-name-input",
+  sectionOrderInput: "section-order-input",
 };
 
 const renderStationAddForm = (container) => {
   container.innerHTML += `
+  <div>
     <label for="station-name-input">
       역 이름<br />
       <input type="text" id="station-name-input" placeholder="역 이름을 입력해주세요." />
     </label>
     <button id="station-add-button">역 추가</button>
+  </div>
   `;
 };
 
@@ -35,8 +41,46 @@ const showStationManagerPage = (e) => {
   renderStationAddForm($mainContentsContainer);
 };
 
-export default function menuButtonHandler(e) {
-  if (e.target.id === BUTTONS_ID[0]) {
+const isInputStringLengthTwoOrGreater = (input) => {
+  if (input.length < 2) {
+    alert("두 글자 이상 입력해주세요.");
+    return false;
+  }
+  return true;
+};
+
+const isStationNameInputDuplicate = (input) => {
+  if (new StationManager().hasName(input)) {
+    alert("해당 역이 이미 존재합니다.");
+    return true;
+  }
+  return false;
+};
+
+const isValidStationName = (name) => {
+  return (
+    isInputStringLengthTwoOrGreater(name) && !isStationNameInputDuplicate(name)
+  );
+};
+
+const stationAddButtonHandler = (e) => {
+  const stationNameInputValue = getChildById(
+    e.target.parentNode.children[0],
+    INPUT_FORM_ID.stationNameInput
+  ).value;
+  if (isValidStationName(stationNameInputValue)) {
+    new StationManager().addStation(stationNameInputValue);
+  }
+};
+
+export const menuButtonHandler = (e) => {
+  if (e.target.id === MENU_BUTTONS_ID.stationManagerButton) {
     showStationManagerPage(e);
   }
-}
+};
+
+export const addButtonHandler = (e) => {
+  if (e.target.id === ADD_BUTTONS_ID.stationAddButton) {
+    stationAddButtonHandler(e);
+  }
+};
