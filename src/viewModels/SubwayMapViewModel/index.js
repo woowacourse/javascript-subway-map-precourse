42ad1dfe7b4dd695138cebd9cdd83d045c758b18
Daniel <elrion018@gmail.com>
2020-12-11 00:31:24 +0900

    feat(SubwayMapViewModel) : delete setProxy function

--- src/viewModels/SubwayMapViewModel/index.js
+++ src/viewModels/SubwayMapViewModel/index.js
@@ -3,19 +3,6 @@ export default class SubwayMapViewModel {
     this.subwayMapModel = subwayMapModel;
   }
 
-  // setProxy() {
-  //   return new Proxy(this, {
-  //     set(target, property, value) {
-  //       if (property === 'stations') {
-  //         target.subwayMapModel.addStation(value);
-  //         console.log(target.subwayMapModel.getStation());
-  //       }
-
-  //       return true;
-  //     },
-  //   });
-  // }
-
   getStations() {
     return this.subwayMapModel.getStations();
   }
