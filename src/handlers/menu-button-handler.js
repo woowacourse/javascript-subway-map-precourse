56600d60a151ba2946a86d2b218b57d584f7f0e9
Tanney-102 <dhxodms89@gmail.com>
2020-12-10 20:48:28 +0900

    Add functions which set station selectors

--- src/handlers/menu-button-handler.js
+++ src/handlers/menu-button-handler.js
@@ -3,6 +3,7 @@ import { getChildById } from "./custom-dom-handler.js";
 import {
   MENU_BUTTONS_ID,
   MANAGER_PAGES_ID,
+  SELECTORS_ID,
 } from "../html-constants/html-id-values.js";
 
 const showManagerPageById = (container, id) => {
@@ -15,9 +16,62 @@ const showManagerPageById = (container, id) => {
   });
 };
 
+const getStartStationSelector = (appContainer) => {
+  const $startSationLabel = getChildById(
+    appContainer,
+    MANAGER_PAGES_ID.lineManager
+  ).getElementsByTagName("label")[1];
+
+  return getChildById($startSationLabel, SELECTORS_ID.lineStartStation);
+};
+
+const getEndStationSelector = (appContainer) => {
+  const $endSationLabel = getChildById(
+    appContainer,
+    MANAGER_PAGES_ID.lineManager
+  ).getElementsByTagName("label")[2];
+
+  return getChildById($endSationLabel, SELECTORS_ID.lineEndStation);
+};
+
+const setSelectorOptions = ($selector, exceptionalOption = "") => {
+  $selector.innerHTML = `<option value="">--선택--</option>`;
+  new StationManager().stationList.forEach((_station) => {
+    if (_station !== exceptionalOption) {
+      $selector.innerHTML += `
+        <option value=${_station}>${_station}</option>
+      `;
+    }
+  });
+};
+
+const setEndStationSelector = (appContainer) => {
+  const $startStationSelector = getStartStationSelector(appContainer);
+  const $endStationSelector = getEndStationSelector(appContainer);
+  const startStation = $startStationSelector.value;
+  if (startStation === "") {
+    $endStationSelector.innerHTML = "";
+  } else {
+    setSelectorOptions($endStationSelector, startStation);
+  }
+};
+
+const setStartStationSelector = (appContainer) => {
+  const $startStationSelector = getStartStationSelector(appContainer);
+  const $endStationSelector = getEndStationSelector(appContainer);
+  setSelectorOptions($startStationSelector);
+  $endStationSelector.innerHTML = "";
+  $startStationSelector.onchange = () => setEndStationSelector(appContainer);
+};
+
 const showStationManagerPage = (appContainer) => {
   showManagerPageById(appContainer, MANAGER_PAGES_ID.stationManager);
   new StationManager().renderStationNameTable();
+};
+
+const showLineManagerPage = (appContainer) => {
+  showManagerPageById(appContainer, MANAGER_PAGES_ID.lineManager);
+  setStartStationSelector(appContainer);
 };
 
 export default function menuButtonHandler(e) {
@@ -26,7 +80,7 @@ export default function menuButtonHandler(e) {
   if (id === MENU_BUTTONS_ID.stationManagerButton) {
     showStationManagerPage(app);
   } else if (id === MENU_BUTTONS_ID.lineManagerButton) {
-    showManagerPageById(app, MANAGER_PAGES_ID.lineManager);
+    showLineManagerPage(app);
   } else if (id === MENU_BUTTONS_ID.sectionManagerButton) {
     showManagerPageById(app, MANAGER_PAGES_ID.sectionManager);
   } else if (id === MENU_BUTTONS_ID.mapPrintManagerButton) {
