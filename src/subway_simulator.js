20b89b193ff74fff64a2eda1a3e783796e012179
dawit jeong <wjdekdnlt1215@gmail.com>
2020-12-11 02:49:03 +0900

    add setCurrentPage

--- src/subway_simulator.js
+++ src/subway_simulator.js
@@ -7,6 +7,7 @@ const SubwaySimulator = function () {
   this.lines = lines;
 
   this.currentPage = "0"; // "1": 역 관리, "2": "노선 관리", "3": 구간 관리, "4": 지하철 노선도 관리
+  this.setCurrentPage = (page) => (this.currentPage = page);
   this.isAlreadyRenderedPage = (page) => this.currentPage === page;
 
   this.clearResultDIV = () => {
