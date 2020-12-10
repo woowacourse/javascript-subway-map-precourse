744caa9539d1a53da2ce0227512619633af14a59
2SOOY <1401660@naver.com>
2020-12-10 11:18:19 +0900

    Feat - App 설계에 필요한 선택자 이름 상수 추가
    - (요구사항) 각종 태그 관련 id, class 주어진 이름 사용

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,30 @@
+export const DOM = {
+  APP: `app`,
+
+  STATION_MANAGER: `station-manager-button`,
+  LINE_MANAGER: `line-manager-button`,
+  SECTION_MANAGER: `section-manager-button`,
+  MAP_PRINT_MANAGER: `map-print-manager-button`,
+
+  STATION_NAME_INPUT: `station-name-input`,
+  STATION_ADD_BUTTON: `station-add-button`,
+  STATION_DELETE_BUTTON: `station-delete-button`,
+
+  LINE_NAME_INPUT: `line-name-input`,
+  LINE_START_STATION_SELECT: `line-start-station-selector`,
+  LINE_END_STATION_SELECT: `line-end-station-selector`,
+  LINE_ADD_BUTTON: `line-add-button`,
+  LINE_DELETE_BUTTON: `line-delete-button`,
+
+  SECTION_LINE_MENU_BUTTON: `section-line-menu-button`,
+  SECTION_STATION_SELECT: `section-station-selector`,
+  SECTION_ORDER_INPUT: `section-order-input`,
+  SECTION_ADD_BUTTON: `section-add-button`,
+  SECTION_DELETE_BUTTON: `section-delete-button`,
+
+  STATION_MAP: `map`,
+};
+
 export const NUM = {
-  STATION_NAME_LIMIT = 2,
-}
+  STATION_NAME_LIMIT: 2,
+};
