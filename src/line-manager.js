f2b159b2db10a75499b04b0894272c5e2944666d
Tanney-102 <dhxodms89@gmail.com>
2020-12-11 00:11:20 +0900

    Add methods 'set', 'addLine' 'hasLine'

--- src/line-manager.js
+++ src/line-manager.js
@@ -1,5 +1,47 @@
+import StationLine from "./station-line.js";
+
 export default class LineManager {
   constructor() {
-    this.lines = [];
+    this.lineList = [];
+    this.setLineList();
+  }
+
+  setLineList() {
+    const storedLineList = localStorage.lineList;
+    if (storedLineList !== undefined) {
+      const storedLineListObj = JSON.parse(storedLineList);
+      this.lineList = Object.entries(storedLineListObj).map((_line) => {
+        return new StationLine(_line[0], _line[1]);
+      });
+    }
+  }
+
+  convertLineListToObject() {
+    const converted = {};
+    this.lineList.forEach((_line) => {
+      converted[_line.name] = _line.section;
+    });
+
+    return converted;
+  }
+
+  addLine(lineName, startStation, endStation) {
+    const newLine = new StationLine(lineName, [startStation, endStation]);
+    this.lineList.push(newLine);
+    const convertedLineObject = this.convertLineListToObject();
+    localStorage.lineList = JSON.stringify(convertedLineObject);
+  }
+
+  hasLineName(lineName) {
+    for (let i = 0; i < this.lineList.length; i++) {
+      if (this.lineList[i].name === lineName) {
+        return true;
+      }
+    }
+    return false;
   }
 }
+// localStorage.lineList = {
+//   lineName: [],
+//    ...
+// }
