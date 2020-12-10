d30bfc9f7ad601d8ff3ce1fd8b72fc7c322b2b4c
Tanney-102 <dhxodms89@gmail.com>
2020-12-10 15:02:00 +0900

    Add function which render station name table

--- src/custom-dom-handler.js
+++ src/custom-dom-handler.js
@@ -1,3 +1,5 @@
+import StationManager from "./station-manager.js";
+
 export const getChildById = (parentElement, id) => {
   const childrenList = parentElement.children;
   for (let i = 0; i < childrenList.length; i++) {
@@ -7,4 +9,16 @@ export const getChildById = (parentElement, id) => {
   }
 
   return null;
+};
+
+export const renderStationNameTable = () => {
+  const tbody = document.getElementById("station-name-tbody");
+  new StationManager().stationList.forEach((_station, _index) => {
+    tbody.innerHTML += `
+      <tr>
+        <td>${_station}</td>
+        <td><button data-station-index=${_index}>삭제</button></td>
+      </tr>
+    `;
+  });
 };
