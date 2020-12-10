3f242d76259958fde3e1795ea1bf00b038e29b93
Tanney-102 <dhxodms89@gmail.com>
2020-12-10 13:01:37 +0900

    Add 'add' button click event

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-import { menuButtonHandler } from "./click-event-handler.js";
+import { menuButtonHandler, addButtonHandler } from "./click-event-handler.js";
 
 document.addEventListener("click", menuButtonHandler);
+document.addEventListener("click", addButtonHandler);
