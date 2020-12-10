0693caafe77c6d88cf0c5c05d5179e5f0d7186a5
Tanney-102 <dhxodms89@gmail.com>
2020-12-10 13:00:59 +0900

    Add function which check length of station name input

--- src/click-event-handler.js
+++ src/click-event-handler.js
@@ -1,9 +1,21 @@
-const BUTTONS_ID = [
-  "station-manager-button",
-  "line-manager-button",
-  "section-manager-button",
-  "map-print-manager-button",
-];
+const MENU_BUTTONS_ID = {
+  stationManagerButton: "station-manager-button",
+  lineManagerButton: "line-manager-button",
+  sectionManagerButton: "section-manager-button",
+  mapPrintManagerButton: "map-print-manager-button",
+};
+
+const ADD_BUTTONS_ID = {
+  stationAddButton: "station-add-button",
+  lineAddButton: "line-add-button",
+  sectionAddButton: "section-add-button",
+};
+
+const INPUT_FORM_ID = {
+  stationNameInput: "station-name-input",
+  lineNameInput: "line-name-input",
+  sectionOrderInput: "section-order-input",
+};
 
 const getChildById = (parentElement, id) => {
   const childrenList = parentElement.children;
@@ -18,11 +30,13 @@ const getChildById = (parentElement, id) => {
 
 const renderStationAddForm = (container) => {
   container.innerHTML += `
+  <div>
     <label for="station-name-input">
       역 이름<br />
       <input type="text" id="station-name-input" placeholder="역 이름을 입력해주세요." />
     </label>
     <button id="station-add-button">역 추가</button>
+  </div>
   `;
 };
 
@@ -35,8 +49,42 @@ const showStationManagerPage = (e) => {
   renderStationAddForm($mainContentsContainer);
 };
 
-export function menuButtonHandler(e) {
-  if (e.target.id === BUTTONS_ID[0]) {
+const isInputStringLengthTwoOrGreater = (input) => {
+  if (input.length < 2) {
+    alert("두 글자 이상 입력해주세요.");
+    return false;
+  }
+
+  return true;
+};
+
+const isValidStationName = (name) => {
+  return isInputStringLengthTwoOrGreater(name);
+};
+
+// const addNewStationName = (name) => {};
+
+const stationAddButtonHandler = (e) => {
+  const stationNameInputValue = getChildById(
+    e.target.parentNode.children[0],
+    INPUT_FORM_ID.stationNameInput
+  ).value;
+  console.log(stationNameInputValue);
+  console.log(isValidStationName(stationNameInputValue));
+  // if (isValidStationName(stationNameInputValue)) {
+  //   addNewStationName(stationNameInputValue);
+  // }
+};
+
+export const menuButtonHandler = (e) => {
+  if (e.target.id === MENU_BUTTONS_ID.stationManagerButton) {
     showStationManagerPage(e);
   }
-}
+};
+
+export const addButtonHandler = (e) => {
+  if (e.target.id === ADD_BUTTONS_ID.stationAddButton) {
+    console.log("station name button");
+    stationAddButtonHandler(e);
+  }
+};
