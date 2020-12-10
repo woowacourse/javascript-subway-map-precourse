b4683e5bbc94ca9b6ad1fbe6992ff99c073aa289
Tanney-102 <dhxodms89@gmail.com>
2020-12-10 14:46:33 +0900

    Add delete button class names

---
+++ src/html-constants/html-classnames.js
@@ -0,0 +1,5 @@
+export const DELETE_BUTTONS_CLASS = {
+  stationDeleteButton: "station-delete-button",
+  lineDeleteButton: "line-delete-button",
+  sectoinDeleteButton: "sectoin-delete-button",
+};
