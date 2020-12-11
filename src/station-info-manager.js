40467fa696b0155f1b50e003cb4b854392aa0470
권세진 <0307kwon@gmail.com>
2020-12-11 23:01:16 +0900

    feat: 노선의 정보를 저장할 수 있는 자료구조를 구현

--- src/station-info-manager.js
+++ src/station-info-manager.js
@@ -1,17 +1,18 @@
 export default class StationINFOManager {
   constructor() {
     this.stations_ = [];
+    this.lines_ = [];
   }
 
   addNewStation({ name }) {
-    const newStation = {
-      name: name,
-      lineNumber: null,
-    };
-    if (this.isOverlapName_(name)) {
-      alert(OVERLAP_ERROR_MESSAGE);
+    if (this.isOverlapName_(this.stations_, name)) {
+      alert(OVERLAP_STATION_ERROR_MESSAGE);
       return;
     }
+    const newStation = {
+      name: name,
+      linesOfStation: new Set(),
+    };
     this.stations_.push(newStation);
   }
   getStationsNames() {
@@ -32,13 +33,38 @@ export default class StationINFOManager {
     this.stations_.splice(stationIndexToDelete, 1);
   }
 
-  isOverlapName_(inputName) {
-    const overlapIndex = this.stations_.findIndex(
+  addNewLine({ lineName, startStationName, endStationName }) {
+    if (this.isOverlapName_(this.lines_, lineName)) {
+      alert(OVERLAP_LINE_ERROR_MESSAGE);
+      return;
+    }
+    const startStationPtr = this.getPointerFromStationsArray_(startStationName);
+    const endStationPtr = this.getPointerFromStationsArray_(endStationName);
+    const newLine = {
+      name: lineName,
+      stationsOfLine: [startStationPtr, endStationPtr],
+    };
+    startStationPtr.linesOfStation.add(lineName);
+    endStationPtr.linesOfStation.add(lineName);
+    this.lines_.push(newLine);
+    console.log(this.stations_);
+    console.log(this.lines_);
+  }
+
+  getPointerFromStationsArray_(targetName) {
+    const targetIndex = this.stations_.findIndex(({ name }) => {
+      return name === targetName;
+    });
+    return this.stations_[targetIndex];
+  }
+  isOverlapName_(targetToFindOverlap, inputName) {
+    const overlapIndex = targetToFindOverlap.findIndex(
       ({ name }) => name === inputName
     );
     return overlapIndex !== -1;
   }
 }
 
-const OVERLAP_ERROR_MESSAGE = "기본 역 이름과 중복되는 이름입니다.";
+const OVERLAP_STATION_ERROR_MESSAGE = "기존 역 이름과 중복되는 이름입니다.";
+const OVERLAP_LINE_ERROR_MESSAGE = "기존 노선 이름과 중복되는 이름입니다.";
 const NOT_EXIST_NAME_ERROR_MESSAGE = "제거할 역이 이미 존재하지 않습니다.";
