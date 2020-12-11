f65064ad716e286a0d7cbcaf7889f9ca6e2ed9b4
SEONBIN <tjsqls2003@gmail.com>
2020-12-11 19:36:20 +0900

    refactor(station): split templates.js, rename feature, applt data*

--- src/station/actions.js
+++ src/station/actions.js
@@ -1,68 +1,54 @@
-const stationManagerBtn = document.getElementById("station-manager-button");
-const stationInputForm = `
-<div id="station-input-form">
-  <label for="station-name">역 이름</label>
-  <div>
-    <input 
-      type="text" 
-      id="station-name-input" 
-      name="station-name" 
-      placeholder="역 이름을 입력해주세요.">
-    </input> 
-    <button id="station-add-button">역 추가</button>
-  </div>
-</div>
-`;
-const stationListOpenTag = `
-<div id="station-list">
-  <h2>🚉 지하철 역 목록</h2>
-  <table id="station-names">
-    <tr>
-      <th>역 이름</th>
-      <th>설정</th>
-    </tr>
-`;
-const stationDeleteBtn = `
-<td><button class="station-delete-button">삭제</button></td>
-`;
-const stationListCloseTag = `
-  </table>
-</div>
-`;
+import {
+  stationInputForm,
+  stationList,
+  stationDeleteBtn,
+} from "./templates.js";
 
-const getStationList = (_stations) => {
-  let stationList = "";
+const stationManagerBtn = document.getElementById("station-manager-button");
+
+const loadStations = () => {
+  return JSON.parse(stationManagerBtn.dataset.stations);
+};
+
+const saveStations = (_stations) => {
+  stationManagerBtn.dataset.stations = JSON.stringify(_stations);
+};
+
+const printLayout = () => {
+  const managerContainer = document.getElementById("manager-container");
+
+  managerContainer.innerHTML = stationInputForm + stationList;
+};
+
+const createStationList = (_stations) => {
+  const stationNames = document.getElementById("station-names");
 
   for (let i = 0; i < _stations.length; i++) {
-    stationList += `<tr><td>${_stations[i]}</td>${stationDeleteBtn}</tr>`;
+    stationNames.innerHTML += `<tr><td>${_stations[i]}</td>${stationDeleteBtn}</tr>`;
   }
+};
 
-  return stationListOpenTag + stationList + stationListCloseTag;
+const updateStationList = (_stations, _newStation) => {
+  createStationList([_newStation]);
+  saveStations([..._stations, _newStation]);
 };
 
 const getStationName = () => {
-  return document.getElementById("station-name-input").value;
-};
+  const stationNameInput = document.getElementById("station-name-input");
+  const stationName = stationNameInput.value;
 
-const printLayout = (_stations) => {
-  const managerContainer = document.getElementById("manager-container");
+  stationNameInput.value = "";
 
-  managerContainer.innerHTML = stationInputForm + getStationList(_stations);
-};
-
-const createStation = (_stations) => {
-  _stations.push(getStationName());
-  printLayout(_stations);
+  return stationName;
 };
 
 export default function StationManager() {
-  const stations = ["인천", "동인천"];
-
-  printLayout(stations);
+  printLayout();
+  createStationList(loadStations());
 
   const stationAddBtn = document.getElementById("station-add-button");
 
   stationAddBtn.addEventListener("click", () => {
-    createStation(stations);
+    updateStationList(loadStations(), getStationName());
   });
 }
---
+++ src/station/templates.js
@@ -0,0 +1,30 @@
+const stationInputForm = `
+<div id="station-input-form">
+  <label for="station-name">역 이름</label>
+  <div>
+    <input 
+      type="text" 
+      id="station-name-input" 
+      name="station-name" 
+      placeholder="역 이름을 입력해주세요.">
+    </input> 
+    <button id="station-add-button">역 추가</button>
+  </div>
+</div>
+`;
+const stationList = `
+<div id="station-list">
+  <h2>🚉 지하철 역 목록</h2>
+  <table id="station-names">
+    <tr>
+      <th>역 이름</th>
+      <th>설정</th>
+    </tr>
+  </table>
+</div>
+`;
+const stationDeleteBtn = `
+<td><button class="station-delete-button">삭제</button></td>
+`;
+
+export { stationInputForm, stationList, stationDeleteBtn };
