0c93f1d0e9e50f72224c065d01de0019783f85aa
sunhpark42 <sunhpark42@gmail.com>
2020-12-12 01:43:24 +0900

    feat: check Line Name is Valid

--- src/LineManger/LineManagerModel.js
+++ src/LineManger/LineManagerModel.js
@@ -1,3 +1,32 @@
 export default class LineManagerModel {
+  static isValidName(line) {
+    return (!this.isDuplicated(line) && !this.isSpace(line) && !this.isEnglish(line)
+              && !this.isSpecialChar(line) && !this.isValidLength(line)
+                && !this.isSingleKoreanAlphabet(line));
+  }
 
+  static isDuplicated(line) {
+    const lines = Object.keys(JSON.parse(localStorage.getItem('Lines')));
+    return lines.indexOf(line) > -1;
+  }
+
+  static isSpace(line) {
+    return line.indexOf(' ') > -1;
+  }
+
+  static isEnglish(line) {
+    return /[a-z]/.test(line);
+  }
+
+  static isSpecialChar(line) {
+    return /[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]/.test(line);
+  }
+
+  static isValidLength(line) {
+    return line.length < 2;
+  }
+
+  static isSingleKoreanAlphabet(line) {
+    return /[^가-힣]/.test(line);
+  }
 }
