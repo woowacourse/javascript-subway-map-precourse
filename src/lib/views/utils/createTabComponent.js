58bc5e5cf06f28fe2370b597300e1f4de0e913bd
loquemedalagana <nadamasquetu012@gmail.com>
2020-12-11 17:40:17 +0900

    test: 자식 컴포넌트 버튼에서 클릭하면 부모 컴포넌트의 버튼이 뜬금없이 호출되는 오류 수정 테스트

--- src/lib/views/utils/createTabComponent.js
+++ src/lib/views/utils/createTabComponent.js
@@ -5,9 +5,14 @@ import Line from "../Line/Line.js";
 import Section from "../Section/Section.js";
 import Map from "../Map/Map.js";
 
+//여기는 생성된 자식 컴포넌트를 그냥 받아만 줘서 유저 엑션에 넘기자
+
 export default (index) => {
   const $tab = new Div(tabs[index].querySelector);
-  const $childNodes = [new Station(), new Line(), new Section(), new Map()];
-  $tab.element.innerHTML = $childNodes[index].render();
+  const $test = document.createElement("p");
+  $test.innerText = tabs[index].title;
+  $test.onclick = () => alert(tabs[index].title);
+  $tab.element.appendChild($test);
+
   return $tab.element;
 };
