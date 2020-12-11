7ebc305357f70bfa713e3d4c425f576691b1abd0
shinhyogeun <tls1gy2rms3@naver.com>
2020-12-11 18:26:27 +0900

    refactor : line.js에서 line이름 검증을 제거함(lineFactory.js에서 실행)

--- src/line.js
+++ src/line.js
@@ -1,47 +1,6 @@
-import { Data } from "./data.js"
-
 export class Line {
     constructor(name, startStation, endStation) {
-        this.checkLineName(name);
         this.name = name;
         this.stationArray = [startStation, endStation];
     }
-
-    getStartStation = () => {
-        return this.stationArray[0]
-    }
-
-    getEndStation = () => {
-        return this.stationArray[this.stationArray.length - 1];
-    }
-
-    checkLineName = (name) => {
-
-        if (this.isLineNameEmpty(name)) {
-            throw "호선 이름을 입력해주세요."
-        }
-
-        if (this.isSameLineNameExite(name)) {
-            throw "같은 이름의 호선이 있습니다."
-        }
-
-    }
-
-    isLineNameEmpty = (name) => {
-        if (name === "") {
-            return true;
-        }
-
-        return false;
-    }
-
-    isSameLineNameExite = (name) => {
-        if (Data.getLineRepository().hasOwnProperty(name)) {
-            return true;
-        }
-
-        return false;
-    }
-
-
 }
