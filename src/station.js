c3652c19d3f1a851c74b8fe3753a17d0fd2ef34a
HyuuunjuKim <gus7wn@gmail.com>
2020-12-11 17:02:19 +0900

    :sparkles: station 객체가 포함된 노선의 속성 및 관련 메소드 추가

--- src/station.js
+++ src/station.js
@@ -1,4 +1,14 @@
 export default function Station(name) {
   this.name = name;
   this.next = null;
+  this.isIncluded = [];
+  this.addIncludedLine = (lineName) => {
+    this.isIncluded.push(lineName);
+  };
+  this.deleteIncludedLine = (lineName) => {
+    const deleteIdx = this.isIncluded.findIndex(
+      (line) => line.name === lineName
+    );
+    this.isIncluded.splice(deleteIdx, 1);
+  };
 }
