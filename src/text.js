47d8312f51795b1be389f99ece2550aa391316c1
shinhyogeun <tls1gy2rms3@naver.com>
2020-12-11 18:16:23 +0900

    feat : 하드코딩을 제거하기 위해 text.js생성

---
+++ src/text.js
@@ -0,0 +1,12 @@
+export const alertText = {
+    SAME_STATION_EXITE: "똑같은 역이 존재합니다.",
+    SHORT_STATION_NAME: "역 이름은 2글자 이상이어야 합니다.",
+    EMPTY_STATION_NAME: "역 이름을 입력해주세요.",
+    SAME_LINE_EXITE: "똑같은 호선이 존재합니다.",
+    EMPTY_LINE_NAME: "호선 이름을 입력해주세요."
+}
+
+export const text = {
+    EMPTY: "",
+    MINIMUM_STATION_LENGTH: 2
+}
