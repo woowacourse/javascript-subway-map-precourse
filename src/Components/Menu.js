dd8c1ed2915393886d104d4f55392e2fea43fd72
2SOOY <1401660@naver.com>
2020-12-11 20:31:49 +0900

    Feat - 모든 메뉴에 대한 컴포넌트 : Menu
    - 4가지 모드에 대한 메뉴 버튼 제공
    - 버튼 클릭시 ManagerContainer의 내부 렌더링

---
+++ src/Components/Menu.js
@@ -0,0 +1,19 @@
+class Menu {
+  constructor({ $target, managerContainer }) {
+    this.$target = $target;
+    this.managerContainer = managerContainer;
+
+    this.bindEvents();
+  }
+
+  bindEvents() {
+    this.$target.addEventListener(`click`, this.onClick.bind(this));
+  }
+
+  onClick({ target }) {
+    if (target.nodeName !== `BUTTON`) return;
+    this.managerContainer.createInnerHTML(target.id);
+  }
+}
+
+export default Menu;
