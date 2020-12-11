1cee6a9f672555cd72d8bb1bf64d7c86ba448c95
dande <7bdande@gmail.com>
2020-12-11 19:50:21 +0900

    feat: [Station] 지하철역 목록 localStorage에서 불러오기
    만약 저장된 지하철역이 없으면 빈 배열을 반환

--- src/modules/Station.js
+++ src/modules/Station.js
@@ -1,9 +1,17 @@
 import { STATION_LIST } from "../constant/constant.js";
 
 export default class Station {
-  constructor() {}
+  constructor() {
+    this._stationList = this._getSavedStationList();
+  }
 
   _setStationList = stationList => {
     localStorage.setItem(STATION_LIST, JSON.stringify(stationList));
+  };
+
+  _getSavedStationList = () => {
+    const savedStationList = localStorage.getItem(STATION_LIST);
+
+    return JSON.parse(savedStationList) || [];
   };
 }
