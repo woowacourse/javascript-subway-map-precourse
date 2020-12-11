359c81721e04d146b6bad2b314c103d9f9bce5f0
권세진 <0307kwon@gmail.com>
2020-12-11 14:43:20 +0900

    feat: 역 관리 html string 작성

--- src/ui/contents-ui/station-manager-ui.js
+++ src/ui/contents-ui/station-manager-ui.js
@@ -10,5 +10,16 @@ export default class StationManagerUI {
 }
 
 const TEMPLATE = `
-station Template 입니다.
+역 이름<br>
+<input type="text" placeholder="역 이름을 입력해주세요." id=""/>
+<button id="">역 추가</button>
+<h2>🚉 지하철 역 목록</h2>
+<table border="1">
+  <th>역 이름</th>
+  <th>설정</th>
+  <tr>
+    <td>첫번째 칸</td>
+    <td>두번째 칸</td>
+  </tr>
+</table>
 `;
