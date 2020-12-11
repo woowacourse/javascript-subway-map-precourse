c94229ca8d29941e8f23bc8d8dfa2e45785157a3
sunhpark42 <sunhpark42@gmail.com>
2020-12-11 16:36:14 +0900

    feat: check Station Name

---
+++ src/StationManger/StationManagerModel.js
@@ -0,0 +1,32 @@
+export default class StationManagerModel {
+  static isValidName(station) {
+    return (!this.isDuplicated(station) && !this.isSpace(station) && !this.isEnglish(station)
+              && !this.isSpecialChar(station) && !this.isValidLength(station)
+                && !this.isSingleKoreanAlphabet(station));
+  }
+
+  static isDuplicated(station) {
+    const stations = localStorage.getItem('Stations').split(',');
+    return stations.indexOf(station) > -1;
+  }
+
+  static isSpace(station) {
+    return station.indexOf(' ') > -1;
+  }
+
+  static isEnglish(station) {
+    return /[a-z]/.test(station);
+  }
+
+  static isSpecialChar(station) {
+    return /[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]/.test(station);
+  }
+
+  static isValidLength(station) {
+    return station.length < 2;
+  }
+
+  static isSingleKoreanAlphabet(station) {
+    return /[^가-힣]/.test(station);
+  }
+}
