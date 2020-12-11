80276151f63253b4b77f1989ffe136367351c462
sunhpark42 <sunhpark42@gmail.com>
2020-12-12 01:32:27 +0900

    feat: add Event Handler

--- src/LineManger/LineManagerController.js
+++ src/LineManger/LineManagerController.js
@@ -1,3 +1,25 @@
-export default LineManagerController {
-  
+import LineManagerView from './LineManagerView.js';
+import LineManagerModel from './LineManagerModel.js';
+
+export default class LineManagerController {
+  static buttonEventController() {
+    document.addEventListener('click', (event) => {
+      const eventId = event.target.id;
+      const eventClassName = event.target.className;
+      if (eventId === 'line-add-button') {
+        this.addButtonClicked();
+      } else if (eventClassName === 'line-delete-button') {
+        const button = event.path[0];
+        this.deleteButtonClicked(button);
+      }
+    });
+  }
+
+  static addButtonClicked() {
+
+  }
+
+  static deleteButtonClicked() {
+    
+  }
 }
