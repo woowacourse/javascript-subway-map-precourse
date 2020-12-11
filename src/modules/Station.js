f859e3e485eca70b4f4e7509988c20dcd18e4179
dande <7bdande@gmail.com>
2020-12-11 19:54:31 +0900

    feat: [Station] 이미 등록된 역인지 확인하기

--- src/modules/Station.js
+++ src/modules/Station.js
@@ -5,6 +5,14 @@ export default class Station {
     this._stationList = this._getSavedStationList();
   }
 
+  _isSavedStation = name => {
+    const savedStation = this._stationList.filter(
+      station => station.name === name
+    );
+
+    return savedStation.length !== 0;
+  };
+
   _setStationList = stationList => {
     localStorage.setItem(STATION_LIST, JSON.stringify(stationList));
   };
