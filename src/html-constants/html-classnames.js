8e3afd13286fa17706703f984696471e404e96d7
Tanney-102 <dhxodms89@gmail.com>
2020-12-12 02:01:58 +0900

    Rename class

--- src/html-constants/html-classnames.js
+++ src/html-constants/html-classnames.js
@@ -4,6 +4,6 @@ export const DELETE_BUTTONS_CLASS = {
   sectoinDeleteButton: "sectoin-delete-button",
 };
 
-export const LINE_SELECT_BUTTONS = "line-select-buttons";
+export const SECTION_LINE_MENU_BUTTON = "section-line-menu-button";
 
 export const LINE_SELECT_HEADER = "line-select-header";
