2173fa9662d1dbbeaa5a3d37f168a3c592ad0eaa
yujo <yujo@student.42seoul.kr>
2020-12-11 13:31:05 +0900

    feat: 입력 받은 역 이름이 올바른지 체크하고 올바르지 않을 경우 alert로 알려주는 기능 구현

--- src/event/stationManagerEvent.js
+++ src/event/stationManagerEvent.js
@@ -1,7 +1,35 @@
-import setState from '../setState.js';
+// import setState from '../setState.js';
+
+const MINIMUN_STATION_LENGTH = 2;
+
+function checkValidStationName(stationName, stations) {
+  if (stationName.length < MINIMUN_STATION_LENGTH) {
+    return alert(`역 이름은 ${MINIMUN_STATION_LENGTH}글자 이상이어야 합니다.`);
+  }
+  if (String(stations).includes(stationName)) {
+    return alert('이미 존재하는 역 이름은 입력할 수 없습니다.');
+  }
+  return true;
+}
+
+function addStation() {
+  const inputStationName = document.querySelector('#station-name-input').value;
+  const stations = JSON.parse(localStorage.getItem('stations'));
+
+  if (!checkValidStationName(inputStationName, stations)) {
+    return false;
+  }
+  if (!stations) {
+    return localStorage.setItem('stations', JSON.stringify(inputStationName));
+  }
+  return localStorage.setItem(
+    'stations',
+    JSON.stringify(`${stations} ${inputStationName}`),
+  );
+}
 
 export default function stationManagerEvent() {
   const $stationAddButton = document.querySelector('#station-add-button');
 
-  $stationAddButton.addEventListener('click', () => setState('addStation'));
+  $stationAddButton.addEventListener('click', addStation);
 }
