c8d5b49891a196265204227099b4d9c138216047
Tanney-102 <dhxodms89@gmail.com>
2020-12-12 03:29:59 +0900

    Change selecte-line-manager HTML to static element except select elemeent

--- src/handlers/section-manager-click-event.js
+++ src/handlers/section-manager-click-event.js
@@ -3,51 +3,29 @@ import LineManager from "../line-manager.js";
 import {
   SECTION_MANAGER_CONTAINERS_ID,
   SELECTORS_ID,
-  INPUT_FORM_ID,
-  ADD_BUTTONS_ID,
 } from "../html-constants/html-id-values.js";
-import {
-  LINE_SELECT_HEADER,
-  SECTION_LINE_MENU_BUTTON,
-} from "../html-constants/html-classnames.js";
+import { SECTION_LINE_MENU_BUTTON } from "../html-constants/html-classnames.js";
 import { getChildById } from "./custom-dom-handler.js";
 
-const getStationSelectorOptions = (lineIndex) => {
+const renderStationSelectorOptions = ($select, lineIndex) => {
   const stationList = new StationManager().stationList;
   const section = new LineManager().lineList[lineIndex].section;
-  let optionsHTML = "<option value='.'>--선택--</option>";
-  optionsHTML += stationList
+  $select.innerHTML = "<option value='.'>--선택--</option>";
+  $select.innerHTML += stationList
     .map((_station) => {
       if (!section.find((_inSection) => _inSection === _station)) {
         return `<option value=${_station}>${_station}</option>`;
       }
     })
     .join("\n");
-
-  return optionsHTML;
 };
 
-const renderSectionRegisterForm = ($selectedLineManager, lineIndex) => {
-  const selectedLine = new LineManager().lineList[lineIndex];
-  $selectedLineManager.innerHTML = `
-    <h3 class=${LINE_SELECT_HEADER}>${selectedLine.name} 관리</h3>
-    <h4>구간 등록</h4>
-    <select id=${SELECTORS_ID.sectionStationSelector}>
-      ${getStationSelectorOptions(lineIndex)}
-    </select>
-    <input 
-      type="text" id=${INPUT_FORM_ID.sectionOrderInput} placeholder="순서" />
-    <button id=${ADD_BUTTONS_ID.sectionAddButton}>등록</button>
-  `;
-};
-
-const renderSelectedLineManager = ($selectedLineManager, lineIndex) => {
-  renderSectionRegisterForm($selectedLineManager, lineIndex);
-  document
-    .getElementById("section-station-selector")
-    .addEventListener("focus", () => {
-      console.log(document.getElementById("section-station-selector"));
-    });
+const showSelectedLineManager = ($selectedLineManager, lineIndex) => {
+  $selectedLineManager.style.display = "block";
+  renderStationSelectorOptions(
+    getChildById($selectedLineManager, SELECTORS_ID.sectionStationSelector),
+    lineIndex
+  );
 };
 
 const selectedLineManagerHandler = (e) => {
@@ -55,7 +33,7 @@ const selectedLineManagerHandler = (e) => {
     e.target.parentElement.parentElement,
     SECTION_MANAGER_CONTAINERS_ID.selectedLineManager
   );
-  renderSelectedLineManager(
+  showSelectedLineManager(
     $selectedLineManager,
     e.target.dataset.selectedLineIndex
   );
