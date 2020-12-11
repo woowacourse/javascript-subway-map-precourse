4d249526107fcf1a3c6f40fb689624d5534f3267
2SOOY <1401660@naver.com>
2020-12-11 20:34:52 +0900

    Feat - App컴포넌트 내 Menu, ManagerContainer 적용
    - 동작 메뉴 렌더링
    - 메뉴에 따라 적절한 렌더링 진행되도록 설계

--- src/Components/App.js
+++ src/Components/App.js
@@ -5,11 +5,15 @@ import {
 } from "../utils/templates/app.js";
 import { clearInnerHTML } from "../utils/domUtil.js";
 
+import Menu from "./Menu.js";
+import ManagerContainer from "./ManagerContainer.js";
+
 class App {
   constructor($target) {
     this.$target = $target;
     this.initHTML();
     this.mountDOM();
+    this.mountComponent();
 
     console.log(this);
   }
@@ -22,6 +26,17 @@ class App {
   mountDOM() {
     this.$menu = this.$target.querySelector(`#menu`);
     this.$managerContainer = this.$target.querySelector(`#manager-container`);
+  }
+
+  mountComponent() {
+    this.managerContainer = new ManagerContainer({
+      $target: this.$managerContainer,
+    });
+
+    this.menu = new Menu({
+      $target: this.$menu,
+      managerContainer: this.managerContainer,
+    });
   }
 }
 
