9dda8035e70b7783b29dd46fc3c9b19baaac207d
unknown <dudtjr913@naver.com>
2020-12-11 21:18:21 +0900

    Refactor : 유효성 검사 함수 분리
    <description>
    유효성을 검사하는 함수의 조건문을 각각의 함수로 분리했다.

--- src/Controller/valid.js
+++ src/Controller/valid.js
@@ -1,28 +1,68 @@
 export const isStationInputVaild = (station, exStation) => {
-  if (station.length < 2) {
-    return alert('2글자 이상으로 입력해주세요.');
+  if (!isLengthRight(station)) {
+    return false;
   }
-  if (station.match(/[^가-힣]/)) {
-    return alert('띄어쓰기 없이 한글만 입력해주세요.');
+  if (!isMatched(station)) {
+    return false;
   }
-  if (exStation && exStation.includes(station)) {
-    return alert('중복된 역이 존재합니다.');
+  if (!isAllDifferentStation(station, exStation)) {
+    return false;
   }
 
   return true;
 };
 
 export const isLineInputValid = (userLine, exLine) => {
-  if (userLine.lineName.length < 2) {
+  if (!isLengthRight(userLine.lineName)) {
+    return false;
+  }
+  if (!isMatched(userLine.lineName)) {
+    return false;
+  }
+  if (!isAllDifferentLine(userLine.lineName, exLine)) {
+    return false;
+  }
+  if (!isUpDownStreamDifferent(userLine.upStream, userLine.downStream)) {
+    return false;
+  }
+
+  return true;
+};
+
+const isLengthRight = (value) => {
+  if (value.length < 2) {
     return alert('2글자 이상으로 입력해주세요.');
   }
-  if (userLine.lineName.match(/[^가-힣1-9]/)) {
+
+  return true;
+};
+
+const isMatched = (value) => {
+  if (value.match(/[^가-힣1-9]/)) {
     return alert('띄어쓰기 없이 한글과 숫자만 입력해주세요.');
   }
-  if (exLine && exLine.find((line) => line.lineName === userLine.lineName)) {
-    return alert('이미 동일한 노선 이름이 존재합니다.');
+
+  return true;
+};
+
+const isAllDifferentStation = (station, allStations) => {
+  if (allStations && allStations.includes(station)) {
+    return alert('같은 이름의 역이 존재합니다.');
   }
-  if (userLine.upStream === userLine.downStream) {
+
+  return true;
+};
+
+const isAllDifferentLine = (userLine, allLines) => {
+  if (allLines.find((line) => line.lineName === userLine)) {
+    return alert('같은 이름의 노선이 존재합니다.');
+  }
+
+  return true;
+};
+
+const isUpDownStreamDifferent = (upStream, downStream) => {
+  if (upStream === downStream) {
     return alert('상행과 하행이 같은 역이 될 수 없습니다.');
   }
 
