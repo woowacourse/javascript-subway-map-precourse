8bbb6012db71988098d49009c3e337086bfe988e
unknown <dudtjr913@naver.com>
2020-12-11 11:10:39 +0900

    Feat : 버튼 클릭 시 역 추가 기능 구현 중
    <description>
    버튼을 클릭하면 input의 유효성을 검사한 후 localStorage에 저장하는데,
    저장할 때 배열로 저장하기 위해서 기존의 localStorage를 가져와서 저장하는
    방식이 필요했다.
    추가로 구현해야 할 것은 dataset을 활용하기 위해 추가된 역의 삭제
    버튼에 dataset을 등록해 삭제할 때 dataset을 활용하는 것이고, input의
    유효성을 검사하는 것, 화면 랜더링하는 것이 추가로 구현되어야 한다.

--- src/View/input.js
+++ src/View/input.js
@@ -1,11 +1,19 @@
-import {onChangeScreen} from '../index.js';
+import {onChangeScreen, onAddStation} from '../index.js';
 
 export const $screenButton = document.body.querySelectorAll('#app > button');
 export const $stationContainer = document.body.querySelector('#station');
 export const $lineContainer = document.body.querySelector('#line');
 export const $sectionContainer = document.body.querySelector('#subway-section');
 export const $mapContainer = document.body.querySelector('#map-section');
+export const $stationAddInput = document.body.querySelector(
+  '#station-name-input',
+);
+export const $stationAddButton = document.body.querySelector(
+  '#station-add-button',
+);
 
 $screenButton.forEach((button) =>
   button.addEventListener('click', onChangeScreen),
 );
+
+$stationAddButton.addEventListener('click', onAddStation);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,30 @@
 import {hideScreen} from './View/hide-screen.js';
 import {showScreen} from './View/show-screen.js';
+import {$stationAddInput} from './View/input.js';
 
 export function onChangeScreen(e) {
   hideScreen();
   showScreen(e);
 }
+
+export function onAddStation() {
+  setLocalStorage('station');
+}
+
+const setLocalStorage = (key) => {
+  const localStorageValue = getLocalStorage(key);
+  if (localStorageValue === null) {
+    return localStorage.setItem(key, JSON.stringify([$stationAddInput.value]));
+  }
+
+  return localStorage.setItem(
+    key,
+    JSON.stringify([...localStorageValue, $stationAddInput.value]),
+  );
+};
+
+const getLocalStorage = (key) => {
+  const localStorageValue = JSON.parse(localStorage.getItem(key));
+
+  return localStorageValue;
+};
