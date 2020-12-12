0e7246a1e46c826483a117963a6f5a61adf93440
2SOOY <1401660@naver.com>
2020-12-12 15:29:14 +0900

    Feat - 커스텀 DOM 객체를 생성하는 유틸 : makeElement
    - 한 번에 DOM객체 생성과 id나 className과 같은 프로퍼티도 바로 지정할 수 있는 유틸

--- src/utils/domUtil.js
+++ src/utils/domUtil.js
@@ -1,3 +1,16 @@
 export const clearInnerHTML = $target => {
   $target.innerHTML = "";
 };
+
+export const makeElement = (type, props = {}, text = "") => {
+  const $element = document.createElement(type);
+
+  Object.keys(props).forEach(prop => {
+    $element[prop] = props[prop];
+  });
+
+  const textNode = document.createTextNode(text);
+  $element.appendChild(textNode);
+
+  return $element;
+};
