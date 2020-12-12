107409ec9771c2c60beaafcd7811f39345cdddb5
dawit jeong <wjdekdnlt1215@gmail.com>
2020-12-12 19:40:49 +0900

    update state in State

--- src/state.js
+++ src/state.js
@@ -2,8 +2,8 @@ import { getLocalStorageDataBy } from "./local_storage.js";
 
 const State = function () {
   this.state = {
-    stations: getLocalStorageDataBy("stations"),
-    lines: getLocalStorageDataBy("lines"),
+    stations: getLocalStorageDataBy("stations") || "",
+    lines: getLocalStorageDataBy("lines") || "",
     currentPage: "0",
   }; // "0": 초기 상태, "1": 역 관리, "2": 노선 관리, "3": 구간 관리, "4": 지하철 노선도 관리
 
