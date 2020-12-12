660eb6c57deedd784e26e1a2ae3887c9deac3f97
jehong <jehong@student.42seoul.com>
2020-12-12 13:49:43 +0900

    feat: split and complete initial table creating functions

--- src/table_utils.js
+++ src/table_utils.js
@@ -20,42 +20,34 @@ export default class TableUtils {
 
   createTable(toIdName) {
     const table = document.createElement('table');
-    table.setAttribute('id', `${toIdName}Table`);
-    // this._tableType[toIdName].forEach(col => {
-    //   console.log(col)
-    // })
-    const row1 = table.insertRow(0);
-    const row2 = table.insertRow(1);
 
-    const row1cell1 = row1.insertCell(0);
-    const row1cell2 = row1.insertCell(1);
-
-    row1cell1.innerHTML = '역 이름';
-    row1cell2.innerHTML = '설정';
-
-
-    const row2cell1 = row2.insertCell(0);
-    const row2cell2 = row2.insertCell(1);
-
-    row2cell1.innerHTML = '신촌';
-    const row2button = document.createElement('button');
-    row2button.innerHTML = '삭제';
-    row2cell2.appendChild(row2button);
-
-    table.style.border = '1px solid black';
-    row1cell2.style.textAlign = 'center';
-
-    row1cell1.style.fontWeight = 'bold';
-    row1cell2.style.fontWeight = 'bold';
-
-    row1cell1.style.border = '1px solid black';
-    row1cell2.style.border = '1px solid black';
-    row2cell1.style.border = '1px solid black';
-    row2cell2.style.border = '1px solid black';
+    this._privateDomUtils.setAttribute(table, `${toIdName}Table`);
+    this.addTableStyle(table);
+    this.createTitleRow(table, toIdName);
     this._privateDomUtils.appendTo(toIdName, table);
   }
 
-  addRow() {
+  addTableStyle(table) {
+    table.style.border = '1px solid black';
+  }
 
+  createTitleRow(table, tableType) {
+    const titleRow = table.insertRow(0);
+
+    this._tableType[tableType].forEach((text, i) => {
+      const cell = this.addCell(titleRow, i);
+      this.addTitleCellStyle(cell, text);
+   })
+  }
+
+  addCell(titleRow, i) {
+    return titleRow.insertCell(i);
+  }
+
+  addTitleCellStyle(cell, text) {
+    cell.innerHTML = text;
+    cell.style.border = '1px solid black';
+    cell.style.fontWeight = 'bold';
+    cell.style.textAlign = 'center';
   }
 }
