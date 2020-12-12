d00025d59fdada16aa61f6312088c9f55af04506
2SOOY <1401660@naver.com>
2020-12-12 21:04:08 +0900

    Feat - DOM 생성 유틸 함수 변경 : makeElement
    - 변경 전 textNode만 취급
    - 변경 후 innerHTML 자체를 넣을 수 있도록 설계
    - DOM 생성시 템플릿을 더욱 자유롭게 사용 가능

--- src/utils/domUtil.js
+++ src/utils/domUtil.js
@@ -2,15 +2,14 @@ export const clearInnerHTML = $target => {
   $target.innerHTML = "";
 };
 
-export const makeElement = (type, props = {}, text = "") => {
+export const makeElement = (type, props = {}, innerHTML = ``) => {
   const $element = document.createElement(type);
 
   Object.keys(props).forEach(prop => {
     $element[prop] = props[prop];
   });
 
-  const textNode = document.createTextNode(text);
-  $element.appendChild(textNode);
+  $element.innerHTML = innerHTML;
 
   return $element;
 };
