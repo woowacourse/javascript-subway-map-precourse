d32926d12882bb2be09599246558a4658bd2cc01
ChayanRin <k852012@naver.com>
2020-12-12 15:27:39 +0900

    feat: implement showMenuButton

--- src/section.js
+++ src/section.js
@@ -1,5 +1,21 @@
+import Line from "./line.js";
 class Section {
-  constructor() {}
+  constructor() {
+    this.lines = Line.lines;
+    this.showMenuButton();
+  }
+
+  showMenuButton = () => {
+    const sectionMenuContainer = document.getElementById("sect-menus");
+    for (let i = 0; i < Object.keys(this.lines).length; i++) {
+      const button = document.createElement("button");
+      button.innerHTML = Object.keys(this.lines)[i];
+      button.className = "section-line-menu-button";
+      sectionMenuContainer.appendChild(button);
+    }
+
+    this.handleMenuButton();
+  };
 }
 
 export default new Section();
