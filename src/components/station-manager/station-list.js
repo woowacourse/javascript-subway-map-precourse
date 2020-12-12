d5dae2a9af5373bcca3152799e65b2fa3ace9684
jho2301 <jho2301@gmail.com>
2020-12-12 16:21:14 +0900

    feat: 역 목록을 조회한다

--- src/components/station-manager/station-list.js
+++ src/components/station-manager/station-list.js
@@ -3,13 +3,28 @@ import Component from '../../library/core/component.js';
 class StationList extends Component {
   constructor($target, props) {
     super($target, props);
+    this._props.stations.subscribe(this.render);
     this.render();
   }
 
   mountTemplate() {
-    this._$target.innerHTML = `
-			<h2>🚉 지하철 역 목록</h2>
-		`;
+    this._$target.innerHTML = `<h2>🚉 지하철 역 목록</h2>`;
+    this._$target.innerHTML += this.createTableTemplate();
+  }
+
+  createTableTemplate() {
+    let tableTemplate = `<table>
+                            <tr>
+                              <th>역 이름</th>
+                              <th>설정</th>
+                            <tr>`;
+    this._props.stations?.value.forEach(station => {
+      tableTemplate += `<tr>
+                          <td>${station}</td>
+                          <td><button>삭제</button></td>
+                        </tr>`;
+    });
+    return tableTemplate + `</table>`;
   }
 }
 
