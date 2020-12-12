59f41a85f348ef1b7745d33a93a92b18cca9bb71
joyfuI <jong970105@gmail.com>
2020-12-12 21:45:14 +0900

    지하철 노선에 역 추가 기능 구현

--- src/subway_line.js
+++ src/subway_line.js
@@ -3,4 +3,9 @@ export default class SubwayLine {
     this.name = name;
     this.line = [upTerminus, downTerminus];
   }
+
+  // 노선에 역 추가. index를 지정하지 않으면 하행 종점 앞에 추가됨
+  addStation(station, index = -1) {
+    this.line.splice(index, 0, station);
+  }
 }
