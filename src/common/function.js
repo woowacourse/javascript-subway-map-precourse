3264232ab3d4a9c3bc68e63f8f8c0e3751f71a95
dawit jeong <wjdekdnlt1215@gmail.com>
2020-12-12 20:04:40 +0900

    add generateArrayFromString, generateArrayFilledByObjectsFromString

--- src/common/function.js
+++ src/common/function.js
@@ -1,1 +1,12 @@
-const Function = function () {};
+const Function = function () {
+  this.generateArrayFromString = (string, separator) => string.split(separator);
+
+  this.generateArrayFilledByObjectsFromString = (string) => {
+    const lineArray = this.generateArrayFromString(string, "},");
+    return lineArray.map((line, index) => {
+      const object = index !== lineArray.length - 1 ? line + "}" : line;
+      return JSON.parse(object);
+    });
+  };
+};
+export const { generateArrayFilledByObjectsFromString } = new Function();
