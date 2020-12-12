aae8e5ccfc7842ea00026495f4d8ea8223118003
Tanney-102 <dhxodms89@gmail.com>
2020-12-12 17:09:17 +0900

    Rename module 'section-manager-click-event.js => section-manager-click-handler.js'

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import menuButtonHandler from "./handlers/menu-button-handler.js";
 import stationManagerClickHandler from "./handlers/station-manager-click-handler.js";
 import lineManagerClickHandler from "./handlers/line-manager-click-handler.js";
-import sectionManagerClickHandler from "./handlers/section-manager-click-event.js";
+import sectionManagerClickHandler from "./handlers/section-manager-click-handler.js";
 
 document.addEventListener("click", menuButtonHandler);
 document.addEventListener("click", stationManagerClickHandler);
