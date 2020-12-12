9b2b5354889b514f9d3a4abdacb15f34fce358f3
권세진 <0307kwon@gmail.com>
2020-12-12 19:11:18 +0900

    feat: 구간 관리 html string 작성

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import HTMLManager from "./html-manager.js";
 import StationINFOManager from "./station-info-manager.js";
 
-const BODY_ID = "app";
-
 const stationINFOManager = new StationINFOManager();
 
 new HTMLManager(stationINFOManager);
--- src/ui/contents-ui/line-manager-ui.js
+++ src/ui/contents-ui/line-manager-ui.js
@@ -11,5 +11,31 @@ export default class LineManagerUI {
 }
 
 const TEMPLATE = `
-line Template 입니다.
+<h2>구간을 수정할 노선을 선택해주세요.</h2>
+<div>
+  <button class="section-line-menu-button">1호선</button>
+</div>
+
+<div>
+  <h2>1호선 관리</h2>
+  <h3>구간 등록</h3>
+  <p>
+    <select id="section-station-selector">
+    </select>
+    <input type="text" id="section-order-input" placeholder="순서" />
+    <button id="section-add-button">등록</button>
+  </p>
+  <table border="1">
+    <th>순서</th>
+    <th>이름</th>
+    <th>설정</th>
+    <tr>
+      <td>1</td>
+      <td>2</td>
+      <td>
+        <button class="section-delete-button">노선에서 제거</button>
+      </td>
+  </table>
+
+<div>
 `;
