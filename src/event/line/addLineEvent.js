cf74d0ce079cb4071e4c7c83eb4bfa3c1b2d20f6
yujo <yujo@student.42seoul.kr>
2020-12-12 20:18:27 +0900

    feat: 노선의 이름이 올바른지 검증하는 기능, 상행과 하행 종점이 같은지 검증하는 기능 구현

--- src/event/line/addLineEvent.js
+++ src/event/line/addLineEvent.js
@@ -1,26 +1,73 @@
-function addLine() {
-  const inputLineName = document.querySelector('#line-name-input').value;
-  const startStation = document.querySelector('#line-start-station-selector')
-    .value;
-  const endStation = document.querySelector('#line-end-station-selector').value;
-  const lines = JSON.parse(localStorage.getItem('lines'));
+import render from '../../render/render.js';
 
+const MINIMUN_LINE_LENGTH = 2;
+
+function checkStartEndStation() {
+  const startStation = document.querySelector('#line-start-station-selector').value;
+  const endStation = document.querySelector('#line-end-station-selector').value;
+
+  if (startStation === endStation) {
+    return false;
+  }
+  return true;
+}
+
+function checkDuplicateLineName(lines, inputLineName) {
   if (lines === null) {
+    return true;
+  }
+
+  const lineNames = lines.split(',').map((line) => line.split(' ')[0]);
+  if (lineNames.includes(inputLineName)) {
+    return false;
+  }
+  return true;
+}
+
+function checkInputLineName(inputLineName) {
+  if (inputLineName.length < MINIMUN_LINE_LENGTH) {
+    return false;
+  }
+  return true;
+}
+
+function addLine(lines, inputLineName) {
+  const startStation = document.querySelector('#line-start-station-selector').value;
+  const endStation = document.querySelector('#line-end-station-selector').value;
+
+  if (!lines) {
     return localStorage.setItem(
       'lines',
-      JSON.stringify(`${inputLineName} : [${startStation} ${endStation}]`),
+      JSON.stringify(`${inputLineName} ${startStation} ${endStation}`),
     );
   }
   return localStorage.setItem(
     'lines',
-    JSON.stringify(
-      `${lines}, ${inputLineName} : [${startStation} ${endStation}]`,
-    ),
+    JSON.stringify(`${lines},${inputLineName} ${startStation} ${endStation}`),
   );
+}
+
+function checkValidInput() {
+  const lines = JSON.parse(localStorage.getItem('lines'));
+  const inputLineName = document.querySelector('#line-name-input').value;
+
+  if (!checkInputLineName(inputLineName)) {
+    return alert(`역 이름은 ${MINIMUN_LINE_LENGTH}글자 이상이어야 합니다.`);
+  }
+  if (!checkDuplicateLineName(lines, inputLineName)) {
+    return alert('이미 존재하는 노선 이름은 입력할 수 없습니다.');
+  }
+  if (!checkStartEndStation()) {
+    return alert('상행 종점과 하행 종점은 다른 역을 선택해야 합니다.');
+  }
+  return addLine(lines, inputLineName);
 }
 
 export default function addLineEvent() {
   const $lineAddButton = document.querySelector('#line-add-button');
 
-  $lineAddButton.addEventListener('click', addLine);
+  $lineAddButton.addEventListener('click', () => {
+    checkValidInput();
+    render();
+  });
 }
