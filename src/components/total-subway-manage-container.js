ff734924457072b07f3fd0551df4802a5a7267c4
ddongule <mnk918p@gmail.com>
2020-12-12 22:45:23 +0900

    refactor: 파일에 필요한 constant 이동

--- src/components/total-subway-manage-container.js
+++ src/components/total-subway-manage-container.js
@@ -1,16 +1,24 @@
-import { menuIDs, menus, menuIndexArray } from "../constants/global.js";
 import showElements from "../utils/display/show-elements.js";
 import hideElements from "../utils/display/hide-elements.js";
 
+const menuIDs = {
+  0: "manage-station",
+  1: "manage-lines",
+  2: "manage-sections",
+  3: "manage-map-print",
+};
+const menus = document.querySelectorAll("#menu > button");
+const menuIndexArray = [...Array(menus.length).keys()];
+
 export default function totalSubwayManageContainer() {
   for (const [index, child] of menus.entries()) {
-    const elementToHide = menuIndexArray.filter(x => {
+    const elementToHide = menuIndexArray.filter((x) => {
       return x !== index;
     });
 
     child.addEventListener("click", () => {
       showElements(menuIDs[index]);
-      elementToHide.forEach(el => {
+      elementToHide.forEach((el) => {
         hideElements(menuIDs[el]);
       });
     });
