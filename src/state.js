4ff7e7db27f727fe0e26065e6e3c7d15db4293bf
dawit jeong <wjdekdnlt1215@gmail.com>
2020-12-13 01:05:59 +0900

    change state in State

--- src/state.js
+++ src/state.js
@@ -5,7 +5,9 @@ const State = function () {
     stations: getLocalStorageDataBy("stations") || "",
     lines: getLocalStorageDataBy("lines") || "",
     currentPage: "0",
-  }; // "0": 초기 상태, "1": 역 관리, "2": 노선 관리, "3": 구간 관리, "4": 지하철 노선도 관리
+    selectedLineIndex: 0,
+  };
+  // currentPage "0": 초기 상태, "1": 역 관리, "2": 노선 관리, "3": 구간 관리, "4": 지하철 노선도 관리
 
   this.isEmptyPage = () => this.state.currentPage === "0";
   this.isAlreadyRenderedPage = (page) => this.state.currentPage === page;
