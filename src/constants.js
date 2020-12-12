68aba9448a81cc70efc17b6111efc46032a0fbae
kiminseob <kis6473@naver.com>
2020-12-12 15:47:27 +0900

    상수 관련 파일 추가

---
+++ src/constants.js
@@ -0,0 +1,45 @@
+const CONSTANTS = {
+  'MENU': {
+    'ID': 'menu',
+    'BUTTON': {
+      'STATION': {
+        'ID': 'station-manager-button',
+      },
+      'LINE': {
+        'ID': 'line-manager-button',
+      },
+      'SECTION': {
+        'ID': 'section-manager-button',
+      },
+      'MAP': {
+        'ID': 'map-print-manager-button',
+      },
+    },
+  },
+  'REGISTER': {
+    'ID': 'register',
+  },
+  'RESULT': {
+    'ID': 'result',
+  },
+  'STATION': {
+    'ALERT': {
+      'DUPLICATION': '역 이름이 중복되지 않게 입력해주세요',
+      'LENGTH': '역 이름은 2글자 이상으로 입력해주세요',
+      'DELETE': '정말로 삭제하시겠습니까?',
+    },
+    'INPUT': {
+      'ID': 'station-name-input',
+    },
+    'BUTTON': {
+      'ADD': {
+        'ID': 'station-add-button',
+      },
+      'DELETE': {
+        'CLASS': 'station-delete-button',
+      },
+    },
+  },
+};
+
+export const {MENU, REGISTER, RESULT, STATION} = CONSTANTS;
