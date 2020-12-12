2a00f1006a35d67cb4751c9c831ffca2275a94cd
seojihwan <seojihwan213@naver.com>
2020-12-12 15:58:40 +0900

    fix: 구간 관련 템플릿 수정

--- src/templates/subway_section.js
+++ src/templates/subway_section.js
@@ -2,8 +2,6 @@ export const sectionMenuContainer = lines => {
   return `<div>
     <h3>구간을 수정할 노선을 선택해주세요.</h3>
     ${lines.map(line => sectionLineMenuButton(line.name))}
-    ${sectionNameInput}
-    ${sectionAddButton}
   </div>`;
 };
 
@@ -11,34 +9,34 @@ const sectionLineMenuButton = name => {
   `<button class="section-line-menu-button">${name}</button>`;
 };
 
-export const sectionHeading = name => {
-  return `<h3>${name}관리</h3>`;
-};
-
-export const sectionAddContainer = () => {
+export const sectionAddContainer = name => {
   return `<div>
+    ${sectionHeading(name)}
     <h4>구간 등록</h4>
     ${sectionStationSelector}
     ${sectionOrderInput}
     ${sectionAddButton}
-  </div>`;
+    </div>`;
 };
 
+const sectionHeading = name => {
+  return `<h3>${name}관리</h3>`;
+};
 const sectionStationSelector =
   '<select id="section-station-selector"></select>';
 const sectionOrderInput = '<input id="section-order-input"></input>';
 const sectionAddButton = '<button id="section-add-button">등록</button>';
 
 export const sectionTable = sections => {
-  `<table${sectionTableHeader}
-    ${sections.map(
-      (section, idx) =>
-        `<tr>
-          ${sectionOrder(idx)}
-          ${sectionName(section.name)}
-          ${sectionDeleteButton}
-        </tr>`
-    )}
+  return `<table>
+    ${sectionTableHeader}
+    ${sections.map((section, idx) => {
+      return `<tr>
+        ${sectionOrder(idx)}
+        ${sectionName(section.name)}
+        ${sectionDeleteButton}
+      </tr>`;
+    })}
   </table>`;
 };
 
