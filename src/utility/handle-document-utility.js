fcb160a2d429d1c3fc046fbdbe6c53d59ff6b41d
권세진 <0307kwon@gmail.com>
2020-12-12 15:29:27 +0900

    feat: 역, 노선 입력값을 목록에 담기 전에 입력값의 앞뒤 공백을 제거

--- src/utility/handle-document-utility.js
+++ src/utility/handle-document-utility.js
@@ -1,7 +1,6 @@
 export function getInputTextByID(id) {
   return document.getElementById(id).value.trim();
 }
-
 export function getAllElementsByClass(className) {
   return document.querySelectorAll("." + className);
 }
