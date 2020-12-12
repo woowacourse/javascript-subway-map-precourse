9596b0f8938859998d3873ca6c0401d33840dcdb
HyuuunjuKim <gus7wn@gmail.com>
2020-12-13 01:39:35 +0900

    :hammer: section-manager.js 리팩토링

--- src/section-manager.js
+++ src/section-manager.js
@@ -2,7 +2,7 @@ import { makeStationOption } from "./make-selector-list.js";
 import { manager } from "./manager.js";
 import Station from "./station.js";
 
-export const isPossibleDeleteIdx = () => {
+export const isCorrectDeleteIdx = () => {
   if (manager.getSelectedLine().length <= 2) {
     alert("더 이상 지하철 역을 제거 할 수 없습니다.");
 
@@ -11,17 +11,17 @@ export const isPossibleDeleteIdx = () => {
   return true;
 };
 export const deleteSectionInList = (idx) => {
-  if (isPossibleDeleteIdx()) {
+  if (isCorrectDeleteIdx()) {
     const stationInSelecteLine = document.getElementById(
       "station-in-selected-line"
     );
     stationInSelecteLine.innerHTML = "";
     manager.getSelectedLine().deleteStationInIdx(idx);
     manager.setSelectedLine(manager.selectedLine);
-    showStationList(manager.getSelectedLine().getAllStationName());
+    showSectionList(manager.getSelectedLine().getAllStationName());
   }
 };
-export const makeStationBox = (station, idx) => {
+export const makeSectionUI = (station, idx) => {
   const oneStation = document.createElement("tr");
   oneStation.setAttribute("id", `${idx}`);
   const stationIdx = document.createElement("td");
@@ -37,17 +37,13 @@ export const makeStationBox = (station, idx) => {
 
   return [stationIdx, stationName, deleteButton];
 };
-export const showStationList = (allStationName) => {
+export const showSectionList = (allStationName) => {
   const table = document.getElementById("station-in-selected-line-list-table");
   allStationName.forEach((station, idx) => {
-    const stationIdx = makeStationBox(station, idx)[0];
-    const stationName = makeStationBox(station, idx)[1];
-    const deleteButton = makeStationBox(station, idx)[2];
+    const [stationIdx, stationName, deleteButton] = makeSectionUI(station, idx);
     const oneStation = document.createElement("tr");
     oneStation.setAttribute("id", `${idx}`);
-    oneStation.appendChild(stationIdx);
-    oneStation.appendChild(stationName);
-    oneStation.appendChild(deleteButton);
+    oneStation.append(stationIdx, stationName, deleteButton);
 
     table.children[1].appendChild(oneStation);
   });
@@ -58,14 +54,13 @@ export const onClickedLine = (lineName) => {
   selectedLineBox.style.display = "Block";
   const sectionManagerTitle = document.getElementById("section-line-name");
   sectionManagerTitle.innerHTML = `${selectedLineName} 관리`;
-  const stationListBox = document.getElementById("station-in-selected-line");
-  stationListBox.innerHTML = ""; // 선택 노선 변경 시 역 계속 나열 안되게 초기화
-  makeStationOption("section-station-selector");
+  resetSectionInput();
+  resetSectionList();
   const selectedLine = manager.lineList.find(
     (line) => line.name === selectedLineName
   );
   manager.setSelectedLine(selectedLine);
-  showStationList(manager.getSelectedLine().getAllStationName());
+  showSectionList(manager.getSelectedLine().getAllStationName());
 };
 export const makeLineButton = (line) => {
   const lineMenuButton = document.createElement("button");
@@ -86,7 +81,14 @@ export const showLineMenuInSectionManager = (lineList) => {
     lineMenu.appendChild(lineMenuButton);
   });
 };
-export const isCorrectAddIdx = (idx, addSectionIdx) => {
+export const resetSectionInput = () => {
+  document.getElementById("section-order-input").value = "";
+  makeStationOption("section-station-selector");
+};
+export const resetSectionList = () => {
+  document.getElementById("station-in-selected-line").innerHTML = "";
+};
+export const isCorrectAddIdx = (idx) => {
   if (
     idx > 0 &&
     idx < manager.getSelectedLine().length &&
@@ -96,17 +98,15 @@ export const isCorrectAddIdx = (idx, addSectionIdx) => {
     return true;
   } else {
     alert("올바른 구간 번호를 입력하세요");
-    addSectionIdx.value = "";
-    makeStationOption("section-station-selector");
+    resetSectionInput();
 
     return false;
   }
 };
 const btnAddSection = document.getElementById("section-add-button");
 btnAddSection.onclick = () => {
-  const addSectionIdx = document.getElementById("section-order-input");
-  const inputIdx = addSectionIdx.value;
-  if (isCorrectAddIdx(inputIdx, addSectionIdx)) {
+  const addSectionIdx = document.getElementById("section-order-input").value;
+  if (isCorrectAddIdx(addSectionIdx)) {
     const addStationName = document.getElementById("section-station-selector")
       .value;
     const selectedLine = manager.getSelectedLine();
@@ -114,11 +114,8 @@ btnAddSection.onclick = () => {
     inputStation.addIncludedLine(selectedLine.name);
     manager.setStationInManager(inputStation);
     selectedLine.addStationInIdx(inputStation, addSectionIdx);
-
-    const stationList = document.getElementById("station-in-selected-line");
-    stationList.innerHTML = "";
-    showStationList(manager.getSelectedLine().getAllStationName());
-    addSectionIdx.value = "";
-    makeStationOption("section-station-selector");
+    resetSectionList();
+    showSectionList(manager.getSelectedLine().getAllStationName());
+    resetSectionInput();
   }
 };
