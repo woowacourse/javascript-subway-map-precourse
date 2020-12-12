a2f693c869865f4e8d0b876c60aa6eb0cb72150c
dande <7bdande@gmail.com>
2020-12-12 21:26:10 +0900

    fix: 상수 추가 및 상수 이름 변경
    상수 추가 및 이름 변경

--- src/constant/message.js
+++ src/constant/message.js
@@ -1,9 +1,12 @@
 export const SAVED_STATION_ALERT = "이미 등록된 지하철역 입니다.";
 export const UNDEFINED_STATION_ALERT = "등록된 지하철역이 없습니다.";
-export const DUPLICATED_LINE_NAME_ALERT =
+export const SAVED_LINE_ALERT =
   "이미 등록된 노선입니다. 다른 이름을 입력해주세요.";
 export const STATION_IN_LINE_ALERT = "노선에 등록된 역은 삭제할 수 없습니다.";
 export const INVALID_NAME_LENGTH_ALERT = "두 글자 이상 입력해주세요.";
+export const INVAILD_LINE_NAME_LENGTH_ALERT = "한 글자 이상 입력해주세요.";
+export const INVALID_LINE_STATION =
+  "상행 종점과 하행 종점은 서로 다른 역으로 선택해주세요.";
 
 export const DELETE_CONFIRM = "정말로 삭제하시겠습니까?";
 export const DELETE_FROM_LINE_CONFIRM = "정말로 노선에서 제거하시겠습니까?";
