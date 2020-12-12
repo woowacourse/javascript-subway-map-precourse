34a0c0157009cab22af85431c5fa9da99a1f5966
lulu-kang <dalkang.hy@gmail.com>
2020-12-12 20:59:11 +0900

    feat: 지하철 역 등록 함수 구현

--- src/station.js
+++ src/station.js
@@ -1,13 +1,21 @@
 import { isSpecialCharacter, isValidLength } from './check.js'
 
 export default function Station() {
+  this.addStation = function(stationName) {
+    const key = stationName;
+    const value = false;
+    let stationList = document.querySelector("#station-list")
+    localStorage.setItem(key, value);
+    stationList.innerHTML += `<tr id="${key}"><td>${key}</td><td><button data-name="${key}" class="station-delete-button">삭제</button></td></tr>`;
+  }
+
   this.getStationName = function() {
     const stationAddButton = document.querySelector("#station-add-button");
     stationAddButton.addEventListener("click", () => {
       const stationName = document.querySelector("#station-name-input").value;
       const alertText = "2글자 이상의 단어를 입력해 주세요."
       if (!isSpecialCharacter(stationName) && isValidLength(stationName)) {
-        console.log(stationName);
+        this.addStation(stationName);
       } else {
         alert(alertText)
       }
