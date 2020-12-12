b6f40f732ab61721cdde0b4e1a41af5a210f68ef
ddongule <mnk918p@gmail.com>
2020-12-12 22:43:16 +0900

    feat: localstorage에 있는 데이터를 가져와 state에 저장하는 기능 구현

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import totalSubwayManageContainer from "./components/total-subway-manage-container.js";
+import stationManageContainer from "./components/station-manage-container.js";
+import { STATION_ARRAY_KEY } from "./global/constant.js";
 
-new totalSubwayManageContainer();
+const state = {
+  stationArray: [],
+};
+
+function init() {
+  if (localStorage.getItem(STATION_ARRAY_KEY)) {
+    state.stationArray = JSON.parse(localStorage.getItem(STATION_ARRAY_KEY));
+  }
+}
+
+init();
+totalSubwayManageContainer();
+stationManageContainer(state);
