87567584965218ba0791fad57eaac22a82c6691a
ChayanRin <k852012@naver.com>
2020-12-12 13:56:04 +0900

    feat: implement addLine

--- src/line.js
+++ src/line.js
@@ -4,7 +4,9 @@ import { createSelect } from "./table.js";
 class Line {
   constructor() {
     this.stations = Station.stations;
+    this.lines = {};
     this.showStationSelect();
+    this.handleAddLineClick();
   }
 
   showStationSelect = () => {
@@ -12,6 +14,19 @@ class Line {
     const downLineSelect = document.getElementById("line-end-station-selector");
     createSelect(upLineSelect, this.stations);
     createSelect(downLineSelect, this.stations);
+  };
+
+  addLine = () => {
+    const lineName = document.getElementById("line-name-input").value;
+    const upStation = document.getElementById("line-start-station-selector")
+      .value;
+    const downStation = document.getElementById("line-end-station-selector")
+      .value;
+    if (this.checkLineVaild(lineName)) {
+      this.lines[lineName] = [upStation, downStation];
+    } else {
+      alert("중복된 노선 이름입니다");
+    }
   };
 
   handleAddLineClick = () => {
