ed790a620630da2ae0bd3bdbddb05514f1e28241
Hyun Cheol <mlch1603@naver.com>
2020-12-13 01:45:27 +0900

    refactor: 상수화, 메소드 이동 - DOM, dataset 관련 문자열들 상수화 - DOM을 다루는 메소드들은 doms.js 모듈로 이동

--- src/doms.js
+++ src/doms.js
@@ -1,13 +1,178 @@
+import { dataStrings } from './index.js';
+
 export const DOMs = {
-  stationManagerButton: document.getElementById('station-manager-button'),
-  lineManagerButton: document.getElementById('line-manager-button'),
-  sectionManagerButton: document.getElementById('section-manager-button'),
-  mapPrintManagerButton: document.getElementById('map-print-manager-button'),
-  managerContainer: document.getElementById('manager-container'),
+  STATION_MANAGER_BUTTON: document.getElementById('station-manager-button'),
+  LINE_MANAGER_BUTTON: document.getElementById('line-manager-button'),
+  SECTION_MANAGER_BUTTON: document.getElementById('section-manager-button'),
+  MAP_PRINT_MANAGER_BUTTON: document.getElementById('map-print-manager-button'),
+  MANAGER_CONTAINER: document.getElementById('manager-container'),
+};
+
+export const DOMStrings = {
+  // station manager
+  STATION_MANAGER: 'station-manager',
+  STATION_NAME_INPUT: 'station-name-input',
+  STATION_ADD_BUTTON: 'station-add-button',
+  STATION_LIST_TABLE: 'station-list',
+  STATION_DELETE_BUTTON: 'station-delete-button',
+
+  // line manager
+  LINE_MANAGER: 'line-manager',
+  LINE_NAME_INPUT: 'line-name-input',
+  LINE_START_STATION_SELECTOR: 'line-start-station-selector',
+  LINE_END_STATION_SELECTOR: 'line-end-station-selector',
+  LINE_ADD_BUTTON: 'line-add-button',
+  LINE_LIST_TABLE: 'line-list',
+  LINE_DELETE_BUTTON: 'line-delete-button',
+
+  // section manager
+  SECTION_CONTAINER: 'section-container',
+  SECTION_LINE_MENU_BUTTON: 'section-line-menu-button',
+  SECTION_MANAGER: 'section-manager',
+  SECTION_STATION_SELECTOR: 'section-station-selector',
+  SECTION_ORDER_INPUT: 'section-order-input',
+  SECTION_ADD_BUTTON: 'section-add-button',
+  SECTION_LIST_TABLE: 'sections',
+  SECTION_DELETE_BUTTON: 'section-delete-button',
+
+  // map print manager
+  MAP_PRINT_MANAGER: 'map',
+};
+
+const strings = {
+  STATION_NAME: '역 이름',
+  STATION_PLACEHOLDER: '역 이름을 입력해주세요.',
+  STATION_ADD: '역 추가',
+  STATION_LIST_TITLE: '🚉 지하철 역 목록',
+  LINE_NAME: '노선 이름',
+  LINE_PLACEHOLDER: '노선 이름을 입력해주세요.',
+  LINE_START: '상행 종점',
+  LINE_END: '하행 종점',
+  LINE_START_STATION: '상행 종점역',
+  LINE_END_STATION: '하행 종점역',
+  LINE_ADD: '노선 추가',
+  LINE_LIST_TITLE: '🚉 지하철 노선 목록',
+  SECTION_SELECT_TITLE: '구간을 수정할 노선을 선택해주세요.',
+  SECTION_ADD_TITLE: '구간 등록',
+  SECTION_DELETE: '노선에서 제거',
+  SETTING: '설정',
+  ORDER: '순서',
+  DELETE: '삭제',
+  MANAGE: '관리',
+  ADD: '등록',
+  NAME: '이름',
 };
 
 export const DOMCtrl = {
   clearManagerContainer() {
-    DOMs.managerContainer.innerHTML = '';
+    DOMs.MANAGER_CONTAINER.innerHTML = '';
+  },
+
+  openStationManager() {
+    const stationManager = `
+      <div id="${DOMStrings.STATION_MANAGER}"><br><span>${strings.STATION_NAME}</span>
+      <br><input type="text" id="${DOMStrings.STATION_NAME_INPUT}" 
+      placeholder="${strings.STATION_PLACEHOLDER}"/><button id="${DOMStrings.STATION_ADD_BUTTON}">
+      ${strings.STATION_ADD}</button><h1>${strings.STATION_LIST_TITLE}</h1>
+      <table id="${DOMStrings.STATION_LIST_TABLE}"><tr><th><b>${strings.STATION_NAME}</b></th>
+      <th><b>${strings.SETTING}</b></th></tr>${this.stations
+      .map(
+        station =>
+          `<tr><td>${station}</td><td><button class="${DOMStrings.STATION_DELETE_BUTTON}" 
+              data-${dataStrings.DATA_STATION}="${station}">${strings.DELETE}</button></td></tr>`
+      )
+      .join('')}</table></div>
+    `;
+    DOMCtrl.clearManagerContainer();
+    DOMs.MANAGER_CONTAINER.innerHTML = stationManager;
+  },
+
+  openLineManager() {
+    const lineManager = `
+      <div id="${DOMStrings.LINE_MANAGER}"><br><span>${strings.LINE_NAME}</span><br>
+      <input type="text" id="${DOMStrings.LINE_NAME_INPUT}" 
+      placeholder="${strings.LINE_PLACEHOLDER}" /><br><br><span>${strings.LINE_START} 
+      <select id="${DOMStrings.LINE_START_STATION_SELECTOR}">
+      ${this.stations.map(station => `<option>${station}</option>`).join('')}</select></span>
+      <br><span>${strings.LINE_END} <select id="${DOMStrings.LINE_END_STATION_SELECTOR}">
+      ${this.stations.map(station => `<option>${station}</option>`).join('')}</select></span>
+      <br><br><button id="${DOMStrings.LINE_ADD_BUTTON}">${strings.LINE_ADD}</button>
+      <h1>${strings.LINE_LIST_TITLE}</h1><table id="${DOMStrings.LINE_LIST_TABLE}"><tr>
+      <th><b>${strings.LINE_NAME}</b></th><th><b>${strings.LINE_START_STATION}</b></th>
+      <th><b>${strings.LINE_END_STATION}</b></th><th><b>${strings.SETTING}</b></th></tr>
+      ${this.lines
+        .map(
+          line =>
+            `<tr><td>${line.lineName}</td><td>${line.start}</td>
+          <td>${line.end}</td><td><button class="${DOMStrings.LINE_DELETE_BUTTON}" 
+          data-${dataStrings.DATA_LINE}="${line.lineName}">${strings.DELETE}</button></td></tr>`
+        )
+        .join('')}</table></div>
+    `;
+    DOMCtrl.clearManagerContainer();
+    DOMs.MANAGER_CONTAINER.innerHTML = lineManager;
+  },
+
+  openSectionManager() {
+    const sectionContainer = `
+      <div id="${DOMStrings.SECTION_CONTAINER}"><h2>${strings.SECTION_SELECT_TITLE}</h2>
+        ${this.lines
+          .map(
+            line =>
+              `<button class="${DOMStrings.SECTION_LINE_MENU_BUTTON}" 
+              data-${dataStrings.DATA_LINE}="${line.lineName}">${line.lineName}</button>`
+          )
+          .join(' ')}
+        <div id="${DOMStrings.SECTION_MANAGER}"></div></div>
+    `;
+    DOMCtrl.clearManagerContainer();
+    DOMs.MANAGER_CONTAINER.innerHTML = sectionContainer;
+  },
+
+  openMapPrintManager() {
+    const mapManager = `<div class="${DOMStrings.MAP_PRINT_MANAGER}">
+      ${this.lines
+        .map(
+          line => `
+            <h3>${line.lineName}</h3><ul>${line.stations
+            .map(station => `<li>${station}</li>`)
+            .join('')}</ul>`
+        )
+        .join('')}
+    </div>`;
+    DOMCtrl.clearManagerContainer();
+    DOMs.MANAGER_CONTAINER.innerHTML = mapManager;
+  },
+
+  selectLine(event) {
+    const {
+      target: { className },
+    } = event;
+    if (className === DOMStrings.SECTION_LINE_MENU_BUTTON) {
+      const lineSelect = event.target.dataset['line'];
+      const lineIndex = this.lines.findIndex(line => line.lineName === lineSelect);
+      DOMCtrl.openSection.bind(this)(lineSelect, lineIndex);
+    }
+  },
+
+  openSection(lineSelect, lineIndex) {
+    const sectionManager = `
+      <h2 data-${dataStrings.DATA_TARGET}="${lineSelect}">${lineSelect} ${strings.MANAGE}</h2><h3>
+      ${strings.SECTION_ADD_TITLE}</h3><select id="${DOMStrings.SECTION_STATION_SELECTOR}">
+      ${this.stations.map(station => `<option>${station}</option>`).join('')}</select>
+      <input type="number" id="${DOMStrings.SECTION_ORDER_INPUT}" 
+      placeholder="${strings.ORDER}">&nbsp;<button id="${DOMStrings.SECTION_ADD_BUTTON}">
+      ${strings.ADD}</button>
+      <br><br><br><table id="${DOMStrings.SECTION_LIST_TABLE}"><tr><th><b>${strings.ORDER}</b>
+      </th><th><b>${strings.NAME}</b></th><th><b>${strings.SETTING}</b></th></tr>
+      ${this.lines[lineIndex].stations
+        .map(
+          (station, index) => `<tr><td>${index}</td><td>${station}</td>
+          <td><button class="${DOMStrings.SECTION_DELETE_BUTTON}" data-${dataStrings.DATA_INDEX}="${index}">
+          ${strings.SECTION_DELETE}</button></td></tr>`
+        )
+        .join('')}</table>
+    `;
+    document.getElementById(DOMStrings.SECTION_MANAGER).innerHTML = sectionManager;
   },
 };
--- src/index.js
+++ src/index.js
@@ -1,144 +1,38 @@
 import SubwayLine from './line.js';
-import { DOMs, DOMCtrl } from './doms.js';
+import { DOMs, DOMCtrl, DOMStrings } from './doms.js';
 import { isValidStationName, isValidLineName } from './valid.js';
 
 export default class SubwayManager {
   constructor() {
-    this.stations = JSON.parse(localStorage.getItem('stations')) || [];
-    this.lines = JSON.parse(localStorage.getItem('lines')) || [];
+    this.stations = JSON.parse(localStorage.getItem(dataStrings.DATA_STATIONS)) || [];
+    this.lines = JSON.parse(localStorage.getItem(dataStrings.DATA_LINES)) || [];
 
     this.setEventListeners();
   }
 
   setEventListeners() {
-    DOMs.stationManagerButton.addEventListener('click', this.openStationManager.bind(this));
-    DOMs.lineManagerButton.addEventListener('click', this.openLineManager.bind(this));
-    DOMs.sectionManagerButton.addEventListener('click', this.openSectionManager.bind(this));
-    DOMs.mapPrintManagerButton.addEventListener('click', this.openMapPrintManager.bind(this));
-    DOMs.managerContainer.addEventListener('click', this.addStation.bind(this));
-    DOMs.managerContainer.addEventListener('click', this.deleteStation.bind(this));
-    DOMs.managerContainer.addEventListener('click', this.addLine.bind(this));
-    DOMs.managerContainer.addEventListener('click', this.deleteLine.bind(this));
-    DOMs.managerContainer.addEventListener('click', this.selectLine.bind(this));
-    DOMs.managerContainer.addEventListener('click', this.addSection.bind(this));
-    DOMs.managerContainer.addEventListener('click', this.deleteSection.bind(this));
-  }
-
-  openStationManager() {
-    const stationManager = `
-      <div id="station-manager"><br><span>역 이름</span><br><input type="text" 
-      id="station-name-input" placeholder="역 이름을 입력해주세요."/>
-      <button id="station-add-button">역 추가</button><h1>🚉 지하철 역 목록</h1>
-      <table id="station-list"><tr><th><b>역 이름</b></th><th><b>설정</b></th></tr>
-        ${this.stations
-          .map(
-            station =>
-              `<tr><td>${station}</td><td><button class="station-delete-button" 
-              data-station="${station}">삭제</button></td></tr>`
-          )
-          .join('')}</table></div>
-    `;
-    DOMCtrl.clearManagerContainer();
-    DOMs.managerContainer.innerHTML = stationManager;
-  }
-
-  openLineManager() {
-    const lineManager = `
-      <div id="line-manager"><br><span>노선 이름</span><br>
-      <input type="text" id="line-name-input" placeholder="노선 이름을 입력해주세요." />
-      <br><br><span>상행 종점<select id="line-start-station-selector">
-        ${this.stations.map(station => `<option>${station}</option>`).join('')}
-      </select></span><br><span>하행 종점<select id="line-end-station-selector">
-        ${this.stations.map(station => `<option>${station}</option>`).join('')}
-      </select></span><br><br><button id="line-add-button">노선 추가</button>
-      <h1>🚉 지하철 노선 목록</h1><table id="line-list"><tr><th><b>노선 이름</b></th>
-      <th><b>상행 종점역</b></th><th><b>하행 종점역</b></th><th><b>설정</b></th></tr>
-      ${this.lines
-        .map(
-          line =>
-            `<tr><td>${line.lineName}</td><td>${line.start}</td>
-          <td>${line.end}</td><td><button class="line-delete-button" 
-          data-line="${line.lineName}">삭제</button></td></tr>`
-        )
-        .join('')}</table></div>
-    `;
-    DOMCtrl.clearManagerContainer();
-    DOMs.managerContainer.innerHTML = lineManager;
-  }
-
-  openSectionManager() {
-    const sectionContainer = `
-      <div id="section-container">
-        <h2>구간을 수정할 노선을 선택해주세요.</h2>
-        ${this.lines
-          .map(
-            line =>
-              `<button class="section-line-menu-button" data-line="${line.lineName}">${line.lineName}</button>`
-          )
-          .join(' ')}
-        <div id="section-manager"></div>
-      </div>
-    `;
-    DOMCtrl.clearManagerContainer();
-    DOMs.managerContainer.innerHTML = sectionContainer;
-  }
-
-  selectLine(event) {
-    const {
-      target: { className },
-    } = event;
-    if (className === 'section-line-menu-button') {
-      const lineSelect = event.target.dataset['line'];
-      const lineIndex = this.lines.findIndex(line => line.lineName === lineSelect);
-      this.openSection(lineSelect, lineIndex);
-    }
-  }
-
-  openSection(lineSelect, lineIndex) {
-    const sectionManager = `
-      <h2 data-target="${lineSelect}">${lineSelect} 관리</h2><h3>구간 등록</h3>
-      <select id="section-station-selector">${this.stations
-        .map(station => `<option>${station}</option>`)
-        .join('')}</select>
-      <input type="number" id="section-order-input" placeholder="순서">&nbsp;
-      <button id="section-add-button">등록</button><br><br><br><table id="sections"><tr><th><b>순서</b>
-      </th><th><b>이름</b></th><th><b>설정</b></th></tr>
-      ${this.lines[lineIndex].stations
-        .map(
-          (station, index) => `<tr><td>${index}</td><td>${station}</td>
-        <td><button class="section-delete-button" data-index="${index}">노선에서 제거</button></td></tr>`
-        )
-        .join('')}
-      </table>
-    `;
-    document.getElementById('section-manager').innerHTML = sectionManager;
-  }
-
-  openMapPrintManager() {
-    const map = `<div class="map">
-      ${this.lines
-        .map(
-          line => `
-        <h3>${line.lineName}</h3><ul>${line.stations
-            .map(station => `<li>${station}</li>`)
-            .join('')}</ul>
-      `
-        )
-        .join('')}
-    </div>`;
-    DOMCtrl.clearManagerContainer();
-    DOMs.managerContainer.innerHTML = map;
+    DOMs.STATION_MANAGER_BUTTON.addEventListener('click', DOMCtrl.openStationManager.bind(this));
+    DOMs.LINE_MANAGER_BUTTON.addEventListener('click', DOMCtrl.openLineManager.bind(this));
+    DOMs.SECTION_MANAGER_BUTTON.addEventListener('click', DOMCtrl.openSectionManager.bind(this));
+    DOMs.MAP_PRINT_MANAGER_BUTTON.addEventListener('click', DOMCtrl.openMapPrintManager.bind(this));
+    DOMs.MANAGER_CONTAINER.addEventListener('click', this.addStation.bind(this));
+    DOMs.MANAGER_CONTAINER.addEventListener('click', this.deleteStation.bind(this));
+    DOMs.MANAGER_CONTAINER.addEventListener('click', this.addLine.bind(this));
+    DOMs.MANAGER_CONTAINER.addEventListener('click', this.deleteLine.bind(this));
+    DOMs.MANAGER_CONTAINER.addEventListener('click', DOMCtrl.selectLine.bind(this));
+    DOMs.MANAGER_CONTAINER.addEventListener('click', this.addSection.bind(this));
+    DOMs.MANAGER_CONTAINER.addEventListener('click', this.deleteSection.bind(this));
   }
 
   addStation(event) {
     const {
       target: { id },
     } = event;
-    if (id === 'station-add-button') {
-      const station = document.getElementById('station-name-input').value;
+    if (id === DOMStrings.STATION_ADD_BUTTON) {
+      const station = document.getElementById(DOMStrings.STATION_NAME_INPUT).value;
       if (isValidStationName(this.stations, station)) {
         this.stations.push(station);
-        localStorage.setItem('stations', JSON.stringify(this.stations));
+        localStorage.setItem(dataStrings.DATA_STATIONS, JSON.stringify(this.stations));
         this.openStationManager();
       }
     }
@@ -149,11 +43,11 @@ export default class SubwayManager {
     const {
       target: { className },
     } = event;
-    if (className === 'station-delete-button') {
-      const targetStationName = event.target.dataset['station'];
+    if (className === DOMStrings.STATION_DELETE_BUTTON) {
+      const targetStationName = event.target.dataset[dataStrings.DATA_STATION];
       const index = this.stations.indexOf(targetStationName);
       this.stations.splice(index, 1);
-      localStorage.setItem('stations', JSON.stringify(this.stations));
+      localStorage.setItem(dataStrings.DATA_STATIONS, JSON.stringify(this.stations));
       this.openStationManager();
     }
   }
@@ -162,13 +56,13 @@ export default class SubwayManager {
     const {
       target: { id },
     } = event;
-    if (id === 'line-add-button') {
-      const lineName = document.getElementById('line-name-input').value;
+    if (id === DOMStrings.LINE_ADD_BUTTON) {
+      const lineName = document.getElementById(DOMStrings.LINE_NAME_INPUT).value;
       if (isValidLineName(this.lines, lineName)) {
-        const startStation = document.getElementById('line-start-station-selector').value;
-        const endStation = document.getElementById('line-end-station-selector').value;
+        const startStation = document.getElementById(DOMStrings.LINE_START_STATION_SELECTOR).value;
+        const endStation = document.getElementById(DOMStrings.LINE_END_STATION_SELECTOR).value;
         this.lines.push(new SubwayLine(lineName, startStation, endStation));
-        localStorage.setItem('lines', JSON.stringify(this.lines));
+        localStorage.setItem(dataStrings.DATA_LINES, JSON.stringify(this.lines));
         this.openLineManager();
       }
     }
@@ -178,11 +72,11 @@ export default class SubwayManager {
     const {
       target: { className },
     } = event;
-    if (className === 'line-delete-button') {
-      const targetLine = event.target.dataset['line'];
+    if (className === DOMStrings.LINE_DELETE_BUTTON) {
+      const targetLine = event.target.dataset[dataStrings.DATA_LINE];
       const index = this.lines.findIndex(line => line.lineName === targetLine);
       this.lines.splice(index, 1);
-      localStorage.setItem('lines', JSON.stringify(this.lines));
+      localStorage.setItem(dataStrings.DATA_LINES, JSON.stringify(this.lines));
       this.openLineManager();
     }
   }
@@ -191,17 +85,16 @@ export default class SubwayManager {
     const {
       target: { id },
     } = event;
-    if (id === 'section-add-button') {
-      const targetLine = document.getElementById('section-manager').querySelector('h2').dataset[
-        'target'
-      ];
+    if (id === DOMStrings.SECTION_ADD_BUTTON) {
+      const targetLine = document.getElementById(DOMStrings.SECTION_MANAGER).querySelector('h2')
+        .dataset[dataStrings.DATA_TARGET];
       const targetLineIndex = this.lines.findIndex(line => line.lineName === targetLine);
-      const stationOrder = document.getElementById('section-order-input').value;
-      const stationName = document.getElementById('section-station-selector').value;
+      const stationOrder = document.getElementById(DOMStrings.SECTION_ORDER_INPUT).value;
+      const stationName = document.getElementById(DOMStrings.SECTION_STATION_SELECTOR).value;
       this.lines[targetLineIndex].stations = this.lines[targetLineIndex].stations
         .slice(0, stationOrder)
         .concat(stationName, this.lines[targetLineIndex].stations.slice(stationOrder));
-      localStorage.setItem('lines', JSON.stringify(this.lines));
+      localStorage.setItem(dataStrings.DATA_LINES, JSON.stringify(this.lines));
       this.openSectionManager();
       this.openSection(targetLine, targetLineIndex);
     }
@@ -211,18 +104,26 @@ export default class SubwayManager {
     const {
       target: { className },
     } = event;
-    if (className === 'section-delete-button') {
-      const targetLine = document.getElementById('section-manager').querySelector('h2').dataset[
-        'target'
-      ];
+    if (className === DOMStrings.SECTION_DELETE_BUTTON) {
+      const targetLine = document.getElementById(DOMStrings.SECTION_MANAGER).querySelector('h2')
+        .dataset[dataStrings.DATA_TARGET];
       const targetLineIndex = this.lines.findIndex(line => line.lineName === targetLine);
-      const targetSectionIndex = event.target.dataset['index'];
+      const targetSectionIndex = event.target.dataset[dataStrings.DATA_INDEX];
       this.lines[targetLineIndex].stations.splice(targetSectionIndex, 1);
-      localStorage.setItem('lines', JSON.stringify(this.lines));
+      localStorage.setItem(dataStrings.DATA_LINES, JSON.stringify(this.lines));
       this.openSectionManager();
       this.openSection(targetLine, targetLineIndex);
     }
   }
 }
+
+export const dataStrings = {
+  DATA_STATIONS: 'stations',
+  DATA_STATION: 'station',
+  DATA_LINES: 'lines',
+  DATA_LINE: 'line',
+  DATA_TARGET: 'target',
+  DATA_INDEX: 'index',
+};
 
 new SubwayManager();
