0aff04697afc816e595a8a78fa7c037213c5684b
Tanney-102 <dhxodms89@gmail.com>
2020-12-12 17:54:52 +0900

    Add handler that show section table upon click line menu button

--- src/handlers/section-manager-click-handler.js
+++ src/handlers/section-manager-click-handler.js
@@ -3,8 +3,12 @@ import LineManager from "../line-manager.js";
 import {
   SECTION_MANAGER_CONTAINERS_ID,
   SELECTORS_ID,
+  TBODY_ID,
 } from "../html-constants/html-id-values.js";
-import { SECTION_LINE_MENU_BUTTON } from "../html-constants/html-classnames.js";
+import {
+  SECTION_LINE_MENU_BUTTON,
+  DELETE_BUTTONS_CLASS,
+} from "../html-constants/html-classnames.js";
 import { getChildById } from "./custom-dom-handler.js";
 
 const renderSelectedLineManagerHeader = ($selectedLineManager, lineIndex) => {
@@ -29,6 +33,23 @@ const renderStationSelectorOptions = ($select, lineIndex) => {
     .join("\n");
 };
 
+const renderLineSectionTbody = (currentLine) => {
+  const $tbody = document.getElementById(TBODY_ID.lineSectionTbody);
+  $tbody.innerHTML = currentLine.section
+    .map((_station, _index) => {
+      return `
+        <tr data-line-name=${currentLine.name}>
+          <td>${_index}</td>
+          <td>${_station}</td>
+          <td><button 
+            class=${DELETE_BUTTONS_CLASS.sectoinDeleteButton}
+            data-section-index=${_index}>노선에서 제거</button>
+        </tr>
+      `;
+    })
+    .join("\n");
+};
+
 const showSelectedLineManager = ($selectedLineManager, lineIndex) => {
   $selectedLineManager.style.display = "block";
   renderSelectedLineManagerHeader($selectedLineManager, lineIndex);
@@ -36,6 +57,7 @@ const showSelectedLineManager = ($selectedLineManager, lineIndex) => {
     getChildById($selectedLineManager, SELECTORS_ID.sectionStationSelector),
     lineIndex
   );
+  renderLineSectionTbody(new LineManager().lineList[lineIndex]);
 };
 
 const selectedLineManagerHandler = (e) => {
