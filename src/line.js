f95f2aea8fecaeb107a0ba7c0f80e2c3493f3f0c
ChayanRin <k852012@naver.com>
2020-12-12 13:35:34 +0900

    feat: implement showStationSelect

--- src/line.js
+++ src/line.js
@@ -1,9 +1,18 @@
 import Station from "./station.js";
+import { createSelect } from "./table.js";
 
 class Line {
   constructor() {
-    this.station = Station.stations;
+    this.stations = Station.stations;
+    this.showStationSelect();
   }
+
+  showStationSelect = () => {
+    const upLineSelect = document.getElementById("line-start-station-selector");
+    const downLineSelect = document.getElementById("line-end-station-selector");
+    createSelect(upLineSelect, this.stations);
+    createSelect(downLineSelect, this.stations);
+  };
 }
 
 export default new Line();
