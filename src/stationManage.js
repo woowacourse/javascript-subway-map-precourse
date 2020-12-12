f2ee0bf5ce10ca5060ded060ec3b8ea3f10bb1ad
zigsong <wldms5764@gmail.com>
2020-12-12 12:20:41 +0900

    refactor: separate functions adding station name

--- src/stationManage.js
+++ src/stationManage.js
@@ -1,38 +1,46 @@
 const app = document.getElementById('app');
-const nameInputArea = document.createElement('p');
-const nameLabel = document.createElement('b');
-nameLabel.innerHTML = '역 이름';
-
-const nameInput = document.createElement('input');
-nameInput.setAttribute('id', 'station-name-input');
-nameInput.setAttribute('placeholder', '역 이름을 입력해주세요');
-
-const nameSubmit = document.createElement('button');
-nameSubmit.setAttribute('id', 'station-add-button');
-nameSubmit.textContent = '역 추가';
-
-nameInputArea.appendChild(nameLabel);
-nameInputArea.appendChild(document.createElement('br'));
-nameInputArea.appendChild(nameInput);
-nameInputArea.appendChild(nameSubmit);
 
 export const initStationManage = () => {
+  createInputArea();
+  createResultArea();
+};
+
+const createInputArea = () => {
+  const nameInputArea = document.createElement('p');
+  const nameLabel = document.createElement('b');
+  nameLabel.innerHTML = '역 이름';
+
+  const nameInput = document.createElement('input');
+  nameInput.setAttribute('id', 'station-name-input');
+  nameInput.setAttribute('placeholder', '역 이름을 입력해주세요');
+
+  const nameSubmit = document.createElement('button');
+  nameSubmit.setAttribute('id', 'station-add-button');
+  nameSubmit.innerHTML = '역 추가';
+
+  nameInputArea.appendChild(nameLabel);
+  nameInputArea.appendChild(document.createElement('br'));
+  nameInputArea.appendChild(nameInput);
+  nameInputArea.appendChild(nameSubmit);
+
   app.appendChild(nameInputArea);
 
   nameInput.addEventListener('keypress', e => {
     let name = nameInput.value;
     if (e.key === 'Enter') {
-      addStation(name);
+      addStation(name, nameInput);
     }
   });
 
   nameSubmit.addEventListener('click', () => {
     let name = nameInput.value;
-    addStation(name);
+    addStation(name, nameInput);
   });
 };
 
-const addStation = name => {
+const addStation = (name, nameInput) => {
   nameInput.value = '';
   console.log(name);
 };
+
+const createResultArea = () => {};
