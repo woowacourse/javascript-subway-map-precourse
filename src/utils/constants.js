c730a4dd6356f30c38b24861b0ecc199385c95a3
intae92 <kangit2013@gmail.com>
2020-12-13 12:55:27 +0900

    style: class, id 선택자 값을 변수 상수화

---
+++ src/utils/constants.js
@@ -0,0 +1,34 @@
+export const DOM_MENU = {
+  STATION_MANAGER_BUTTON_ID: "station-manager-button",
+  LINE_MANAGER_BUTTON_ID: "line-manager-button",
+  SECTION_MANAGER_BUTTON_ID: "section-manager-button",
+  MAP_PRINT_MANAGER_BUTTON_ID: "map-print-manager-button",
+};
+
+export const DOM_STATION = {
+  STATION_NAME_INPUT_ID: "station-name-input",
+  STATION_ADD_BUTTON_ID: "station-add-button",
+  STATION_DELETE_BUTTON_CLASS: "station-delete-button",
+  STATION_FORM_ID: "station-form",
+  STATION_LIST_TBODY_ID: "station-list-tbody",
+};
+
+export const DOM_LINE = {
+  LINE_NAME_INPUT_ID: "line-name-input",
+  LINE_START_STATION_SELECTOR_ID: "line-start-station-selector",
+  LINE_END_STATION_SELECTOR_ID: "line-end-station-selector",
+  LINE_ADD_BUTTON_ID: "line-add-button",
+  LINE_DELETE_BUTTON_CLASS: "line-delete-button",
+};
+
+export const DOM_SECTION = {
+  SECTION_LINE_MENU_BUTTON_CLASS: "section-line-menu-button",
+  SECTION_STATION_SELECTOR_ID: "section-station-selector",
+  SECTION_ORDER_INPUT_ID: "section-order-input",
+  SECTION_ADD_BUTTON_ID: "section-add-button",
+  SECTION_DELETE_BUTTON_CLASS: "section-delete-button",
+};
+
+export const DOM_ID = {
+  ID: "app",
+};
