84b1d8faa456a94b41236e489450e6544984b709
2SOOY <1401660@naver.com>
2020-12-13 14:56:39 +0900

    Feat - 역 관리 컴포넌트 이벤트 등록 및 렌더링 메소드 : Components/StationManager
    - mountDOMs : 역 등록/제거를 위해 필요한 DOM 마운트
    - bindEvents : 역 등록/제거를 위한 클릭 이벤트 등록
    - render : 역 목록 상태에 따라 브라우저에 렌더링하는 메소드

--- src/Components/StationManager.js
+++ src/Components/StationManager.js
@@ -1,18 +1,45 @@
-import { StationManagerHTML } from "../utils/templates/stationManager.js";
+import subwayStore from "../models/SubwayStore.js";
+import {
+  TableHeaderHTML,
+  TableRow,
+} from "../utils/templates/stationManager.js";
 
 class StationManager {
   constructor({ $target }) {
     this.$target = $target;
-    this.innerHTML = StationManagerHTML();
 
+    this.mountDOMs();
     this.bindEvents();
-    this.render();
+    this.render(subwayStore.getStations());
   }
 
-  bindEvents() {}
-  render() {
-    this.$target.innerHTML = this.innerHTML;
+  mountDOMs() {
+    this.$input = this.$target.querySelector(`#station-name-input`);
+    this.$addButton = this.$target.querySelector(`#station-add-button`);
+    this.$table = this.$target.querySelector(`#station-table`);
+  }
+
+  bindEvents() {
+    this.$addButton.addEventListener("click", this.onClickAddButton.bind(this));
+    this.$table.addEventListener("click", this.onClickDeleteButton.bind(this));
+  }
+
+  onClickAddButton({ target }) {}
+
+  onClickDeleteButton({ target }) {}
+
+  render(stations) {
+    this.$table.innerHTML = stations.reduce((html, station) => {
+      html += TableRow(station.name);
+      return html;
+    }, TableHeaderHTML());
   }
 }
 
 export default StationManager;
+
+/*
+  STATION_NAME_INPUT: `station-name-input`,
+  STATION_ADD_BUTTON: `station-add-button`,
+  STATION_DELETE_BUTTON: `station-delete-button`,
+*/
