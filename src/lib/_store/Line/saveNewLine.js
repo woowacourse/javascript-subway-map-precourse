2514b21e28f1d0f1dcc413b7ae3073c57be29e2f
loquemedalagana <nadamasquetu012@gmail.com>
2020-12-13 15:16:10 +0900

    노선 추가 시 UI 업데이트 안되는 버그 수정

--- src/lib/_store/Line/saveNewLine.js
+++ src/lib/_store/Line/saveNewLine.js
@@ -1,8 +1,20 @@
 import { lineSelector } from "../selectors.js";
 import { lineReducer } from "../reducers.js";
+import {LINE_LIST} from "../../common/IdAndClassNames.js";
+
+import getNewLineDataRowSet from "../../views/Line/getNewLineDataRowSet.js";
+import convertTableRowDataToDOM from "../../views/components/subComponents/convertTableRowDataToDOM.js";
+
+const updateUI = (newLineInfo) => {
+  const $dataTable = document.querySelector(LINE_LIST).querySelector("tbody");
+  const [newLineDataRow] = getNewLineDataRowSet([newLineInfo]);
+  const $newLineDataRow = convertTableRowDataToDOM(newLineDataRow);
+  $dataTable.appendChild($newLineDataRow);
+}
 
 export default (newLineInfo) => {
   const updatedLineList = lineSelector();
   updatedLineList.push(newLineInfo);
   lineReducer(updatedLineList);
+  updateUI(newLineInfo);
 };
