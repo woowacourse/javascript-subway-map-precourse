66a8e23e3a44605abcad8e7f03539658e7119b41
ddongule <mnk918p@gmail.com>
2020-12-13 16:26:55 +0900

    feat: 지하철 역 중복 확인 기능 구현

---
+++ src/global/messages.js
@@ -0,0 +1,12 @@
+export const ALERT_MESSAGES = {
+  ERROR_STATION_INPUT_DUPLICATED: "이미 존재하는 역 이름입니다.",
+  ERROR_STATION_INPUT_LENGTH_UNDER: "2글자 이상을 입력하셔야 합니다!",
+  ERROR_STATION_INPUT_SPECIAL_CHARACTER:
+    "역 이름에는 특수문자가 입력되지 않습니다!",
+  ERROR_STATION_INPUT_NOT_COMPLETE: "역 이름을 바르게 작성해 주세요!",
+  ERROR_STATION_INPUT_MULTIPLE_SPACES: "1개 이하의 공백만 입력이 가능합니다!",
+};
+
+export const CONFIRM_MESSAGES = {
+  CONFIRM_DELETE: "정말로 삭제하시겠습니까?",
+};
--- src/utils/inputs/validator/station-name-validator.js
+++ src/utils/inputs/validator/station-name-validator.js
@@ -1,13 +1,28 @@
-import { ALERT_MESSAGES } from "../../../global/alert-messages.js";
+import { ALERT_MESSAGES } from "../../../global/messages.js";
+import { state } from "../../../index.js";
 
 const checkSpecialCharacter = /[\{\}\[\]\/?.,;:|\)*~`!^\-_+₩\<\>@\#$%&\\\=\(\'\"]/;
 const checkInvalidKorean = /[|ㄱ-ㅎ|ㅏ-ㅣ]/;
 const checkMultipleSpaces = /\s{2,}/;
+const STATION_INPUT_MINIMUM_LENGTH = 2;
+
+export function isDuplicatedStationName(stationName) {
+  let isDuplicated = false;
+
+  for (const station of state.stationArray) {
+    if (station.stationName === stationName) {
+      isDuplicated = true;
+    }
+  }
+
+  return isDuplicated;
+}
 
 export default function inputStationValidator(stationNameInputValue) {
   let isValid = false;
-
-  if (stationNameInputValue.length < 2) {
+  if (isDuplicatedStationName(stationNameInputValue)) {
+    alert(ALERT_MESSAGES.ERROR_STATION_INPUT_DUPLICATED);
+  } else if (stationNameInputValue.length < STATION_INPUT_MINIMUM_LENGTH) {
     alert(ALERT_MESSAGES.ERROR_STATION_INPUT_LENGTH_UNDER);
   } else if (checkSpecialCharacter.test(stationNameInputValue)) {
     alert(ALERT_MESSAGES.ERROR_STATION_INPUT_SPECIAL_CHARACTER);
