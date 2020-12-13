c2705a27ae7eb8d985284f6ffbe2485e452e13f8
ddongule <mnk918p@gmail.com>
2020-12-13 18:16:14 +0900

    feat: 지하철 역을 삭제하기 전 경고창 띄우는 기능 구현

--- src/utils/display/make-elements.js
+++ src/utils/display/make-elements.js
@@ -1,5 +1,6 @@
 import { state, saveToLocalStorage } from "../../index.js";
 import { STATION_ARRAY_KEY } from "../../global/constant.js";
+import { CONFIRM_MESSAGES } from "../../global/messages.js";
 
 export function makeTdElement(elementToMake) {
   const tdContent = document.createElement("td");
@@ -12,22 +13,27 @@ export function makeTdElement(elementToMake) {
 export function makeTdDeleteBtn(objectToMake, tr) {
   const tdDeleteBtn = document.createElement("td");
   const deleteBtn = document.createElement("button");
+  const CONFIRM_DELETE = CONFIRM_MESSAGES.CONFIRM_DELETE;
 
   deleteBtn.innerHTML = "삭제";
   deleteBtn.addEventListener("click", () => {
-    const deleteStation = state.stationArray.filter((station) => {
-      return station.id !== objectToMake.id;
-    });
-    tr.remove();
-    state.stationArray = deleteStation;
-    saveToLocalStorage(STATION_ARRAY_KEY, JSON.stringify(state.stationArray));
+    const confirmDelete = confirm(CONFIRM_DELETE);
+
+    if (confirmDelete) {
+      const deleteStation = state.stationArray.filter((station) => {
+        return station.id !== objectToMake.id;
+      });
+      tr.remove();
+      state.stationArray = deleteStation;
+      saveToLocalStorage(STATION_ARRAY_KEY, JSON.stringify(state.stationArray));
+    }
   });
   tdDeleteBtn.appendChild(deleteBtn);
 
   return tdDeleteBtn;
 }
 
-export default function makeOneRowWithDeleteBtn(objectToMake, ...args) {
+export default function makeOneRowWithDeleteBtn(objectToMake) {
   const tr = document.createElement("tr");
   const deleteBtn = makeTdDeleteBtn(objectToMake, tr);
 
