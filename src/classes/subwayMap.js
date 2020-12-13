89f5f3b62161fecf1a88fd2e2ce06238afe882a6
swon3210 <swon3210@gmail.com>
2020-12-13 21:57:36 +0900

    feat: delete subway station

--- src/classes/subwayMap.js
+++ src/classes/subwayMap.js
@@ -4,4 +4,13 @@ export default class SubwayMap {
   addStation(station) {
     this.#allStations.push(station);
   }
+
+  deleteStation(stationName) {
+    const deletingStationIndex = this.#allStations.findIndex(
+      (registeredStation) => registeredStation.name === stationName
+    );
+    if(deletingStationIndex !== -1) {
+      this.#allStations.splice(deletingStationIndex, 1);
+    }
+  }
 }
