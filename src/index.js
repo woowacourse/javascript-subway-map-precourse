9439931e26ce92bfa62bedcb72e81e26b4b3cfa6
Shim MunSeong <Puterism.K@gmail.com>
2020-12-14 01:50:22 +0900

    feat: 노선의 상행 종점부터 하행 종점까지 연결된 순서대로 역 목록을 출력하는 기능

--- src/index.js
+++ src/index.js
@@ -501,9 +501,21 @@ class SubwayMap {
   }
 
   showMapPrintManager() {
-    this.elements.managerContainer.innerHTML = `
-      <h2>지하철 노선도 출력</h2>
-    `;
+    this.elements.managerContainer.innerHTML = `<div class="map" />`;
+    const mapElement = document.querySelector('.map');
+
+    mapElement.innerHTML = this.lineList
+      .map((line) => {
+        let generatedDOM = `
+          <h3>${line.name}</h3>
+          <ul>
+        `;
+        generatedDOM += line.sectionList.map((section) => `<li>${section.name}</li>`).join('');
+        generatedDOM += `</ul>`;
+
+        return generatedDOM;
+      })
+      .join('');
   }
 }
 
