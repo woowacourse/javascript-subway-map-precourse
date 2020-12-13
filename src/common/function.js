6d252fb91a4f54aa2852abf069858421ca56ede7
dawit jeong <wjdekdnlt1215@gmail.com>
2020-12-14 05:11:34 +0900

    move funcions in state to function.js

--- src/common/function.js
+++ src/common/function.js
@@ -1,1 +1,14 @@
-const Function = function () {};
+import { setLocalStorageDataOf } from "../local_storage.js";
+import { resultDIV, setState, state } from "../state.js";
+
+const Function = function () {
+  this.isEmptyPage = () => state.currentPage === "0";
+  this.isAlreadyRenderedPage = (page) => state.currentPage === page;
+
+  this.getFormattedStations = () => JSON.parse(state.stations);
+  this.getFormattedLines = () => JSON.parse(state.lines);
+
+  this.clearResultDIV = () => {
+    resultDIV.innerHTML = "";
+  };
+};
--- src/state.js
+++ src/state.js
@@ -5,26 +5,15 @@ const State = function () {
     stations: getLocalStorageDataBy("stations") || "[]",
     lines: getLocalStorageDataBy("lines") || "[]",
     currentPage: "0",
-    selectedLineIndex: 0,
+    selectedLineIndex: null,
     alreadySelectionButtonClicked: false,
   };
   // currentPage "0": 초기 상태, "1": 역 관리, "2": 노선 관리, "3": 구간 관리, "4": 지하철 노선도 관리
-
-  this.isEmptyPage = () => this.state.currentPage === "0";
-  this.isAlreadyRenderedPage = (page) => this.state.currentPage === page;
-
-  this.getFormattedStations = () => JSON.parse(this.state.stations);
-  this.getFormattedLines = () => JSON.parse(this.state.lines);
-
   this.setState = (dataName, nextState) => {
     this.state[dataName] = nextState;
   };
 
   this.resultDIV = document.getElementById("result");
-  this.clearResultDIV = () => {
-    resultDIV.innerHTML = "";
-  };
-
   this.VALUE_IN_ARRAY = "valueInArray";
   this.INDEX_OF_DATA = "indexOfData";
 };
