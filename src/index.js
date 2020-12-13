bfec86bb6a83510ebcfa7314677db360dd9808ce
joyfuI <jong970105@gmail.com>
2020-12-13 17:19:11 +0900

    역 관리 뷰 구현

--- src/index.js
+++ src/index.js
@@ -66,6 +66,11 @@ const station = document.getElementById('station');
 const line = document.getElementById('line');
 const section = document.getElementById('section');
 const mapPrint = document.getElementById('map-print');
+const stationNameInput = document.getElementById('station-name-input');
+const stationAddButton = document.getElementById('station-add-button');
+const stationResult = document.getElementById('station-result');
+
+const subwayMap = new SubwayMap();
 
 stationManagerButton.addEventListener('click', showStationManager);
 lineManagerButton.addEventListener('click', showLineManager);
@@ -77,6 +82,8 @@ function showStationManager() {
   "use strict";
 
   changeTab(station);
+  stationNameInput.value = '';
+  showStationList();
 }
 
 // 노선 관리 탭
@@ -109,4 +116,18 @@ function changeTab(dom) {
   section.classList.add('d-none');
   mapPrint.classList.add('d-none');
   dom.classList.remove('d-none');
+}
+
+// 지하철 역 목록 출력
+function showStationList() {
+  "use strict";
+
+  const stationList = subwayMap.getStationList();
+  const stationDeleteButtonHTML = '<button class="station-delete-button">';
+
+  let html = '';
+  for (const station of stationList) {
+    html += `<tr><td>${station}</td><td>${stationDeleteButtonHTML}</td></tr>`;
+  }
+  stationResult.innerHTML = html;
 }
