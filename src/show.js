c21215a67a8aa39d29aa0e75a431d90657a8aa74
ChayanRin <k852012@naver.com>
2020-12-13 16:42:13 +0900

    feat: implement showLines

--- src/show.js
+++ src/show.js
@@ -1,9 +1,25 @@
 import Line from "./line.js";
+import { createCustomElement } from "./table.js";
 
 class Show {
   constructor() {
     this.lines = Line.lines;
+    this.showLines();
   }
+
+  showLines = () => {
+    const previewContainer = document.getElementById("station-line");
+    for (let i = 0; i < Object.keys(this.lines).length; i++) {
+      const name = Object.keys(this.lines)[i];
+      previewContainer.appendChild(
+        createCustomElement({
+          tag: "h2",
+          innerHTML: name,
+        })
+      );
+      previewContainer.appendChild(this.showLine(name));
+    }
+  };
 }
 
 export default new Show();
