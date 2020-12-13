d42be0c0e0b7e8e3c76b47059cbd5ef650b1faed
intae92 <kangit2013@gmail.com>
2020-12-13 12:58:10 +0900

    feat: menu nav 버튼4개 렌더링 구현

--- src/components/MenuComponentRender.js
+++ src/components/MenuComponentRender.js
@@ -1,5 +1,23 @@
+import { DOM_MENU, DOM_ID } from "../utils/constants.js";
+
 export default class MenuComponentRender {
   constructor() {
     console.log("--MenuComponentRender--");
+    this._app = document.getElementById(DOM_ID.ID);
+    this.render();
+  }
+
+  render() {
+    const menuNav = document.createElement("div");
+    menuNav.setAttribute("id", "menu-nav");
+    const menuInnerHTML = `
+        <button id=${DOM_MENU.STATION_MANAGER_BUTTON_ID}>1. 역 관리</button>
+        <button id=${DOM_MENU.LINE_MANAGER_BUTTON_ID}>2. 노선 관리</button>
+        <button id=${DOM_MENU.SECTION_MANAGER_BUTTON_ID}>3. 구간 관리</button>
+        <button id=${DOM_MENU.MAP_PRINT_MANAGER_BUTTON_ID}>4. 지하철 노선도 출력</button>
+    `;
+
+    menuNav.innerHTML = menuInnerHTML;
+    this._app.appendChild(menuNav);
   }
 }
