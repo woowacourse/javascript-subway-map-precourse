129179779105851b5a2faf453714fcae90332056
swimme <2sy100@likelion.org>
2020-12-14 06:06:25 +0900

    fix: DOM 문법 오류 수정

--- src/controller/line-manager.controller.js
+++ src/controller/line-manager.controller.js
@@ -1,6 +1,7 @@
 import line from "../service/line.service.js";
 import { createLineTableRowHTML, lineManagerViewHTML } from "../common/template.js";
-
+import { errorMessage } from "../common/error-message.js";
+const { INVALID_LENGTH_LINE_NAME, DUPLICATE_LINE_NAME, INVALID_START_END_STATION } = errorMessage;
 export default class LineManager {
   constructor() {
     this.line = line;
@@ -26,21 +27,21 @@ export default class LineManager {
   }
 
   getLineNameInput() {
-    const lineNameInputField = this.document.getElementById("line-name-input");
+    const lineNameInputField = document.getElementById("line-name-input");
     const lineName = lineNameInputField.value;
 
     return lineName;
   }
 
   getLineStartStationInput() {
-    const startStationSelector = this.document.getElementById("line-name-input");
+    const startStationSelector = document.getElementById("line-name-input");
     const startStationInput = startStationSelector.value;
 
     return startStationInput;
   }
 
   getLineEndStationInput() {
-    const endStationSelector = this.document.getElementById("line-name-input");
+    const endStationSelector = document.getElementById("line-name-input");
     const endStationInput = endStationSelector.value;
 
     return endStationInput;
