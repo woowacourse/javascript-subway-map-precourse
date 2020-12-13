97cd77198235d95460bc7482b19b8035f1b0584b
swon3210 <swon3210@gmail.com>
2020-12-13 22:09:23 +0900

    feat: delete subway line

--- src/classes/subwayMap.js
+++ src/classes/subwayMap.js
@@ -14,16 +14,25 @@ export default class SubwayMap {
     return isDuplicatedStationName;
   }
 
-  addLine(line) {
-    this.#allLines.push(line);
-  }
-
   deleteStation(stationName) {
     const deletingStationIndex = this.#allStations.findIndex(
       (registeredStation) => registeredStation.name === stationName
     );
     if (deletingStationIndex !== -1) {
       this.#allStations.splice(deletingStationIndex, 1);
+    }
+  }
+
+  addLine(line) {
+    this.#allLines.push(line);
+  }
+
+  deleteLine(lineName) {
+    const deletingLineIndex = this.#allLines.findIndex(
+      (registerdLine) => registerdLine.name === lineName
+    );
+    if (deletingLineIndex !== -1) {
+      this.#allLines.splice(deletingLineIndex, 1);
     }
   }
 }
