acf9a3014dd1c2fd35b9c3787517e179292b1597
Shim MunSeong <Puterism.K@gmail.com>
2020-12-13 21:14:35 +0900

    feat: 지하철 노선을 등록하는 기능 - README.md에서 이전 커밋의 `지하철 역 데이터가 새로고침 후에도 유지되는 기능` 체크 누락 수정

---
+++ src/Line.js
@@ -0,0 +1,9 @@
+class Line {
+  constructor(name, startStation, endStation) {
+    this.name = name;
+    this.startStation = startStation;
+    this.endStation = endStation;
+  }
+}
+
+export default Line;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import Station from './Station.js';
+import Line from './Line.js';
 
 const MINIMUM_INPUT_LENGTH = 2;
+const MINIMUM_STATION_COUNT = 2;
 
 class SubwayMap {
   constructor() {
+    this.elements = {};
     this.stationList = [];
+    this.lineList = [];
+
     this.setMenuElements();
     this.setMenuEventListener();
+
+    this.loadStationList();
   }
 
   setMenuElements() {
@@ -26,12 +33,21 @@ class SubwayMap {
     this.elements.mapPrintManagerButton.addEventListener('click', this.showMapPrintManager.bind(this));
   }
 
+  getStation(name) {
+    return this.stationList.find((station) => station.name === name);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  isValidNameLength(name) {
+    return name.length >= MINIMUM_INPUT_LENGTH;
+  }
+
   isExistStation(name) {
     return this.stationList.some((station) => station.name === name);
   }
 
-  hasValidStationName(name) {
-    if (name.length < MINIMUM_INPUT_LENGTH) {
+  isValidStationName(name) {
+    if (!this.isValidNameLength(name)) {
       alert(`${MINIMUM_INPUT_LENGTH}글자 이상 입력해주세요`);
       return false;
     }
@@ -67,7 +83,7 @@ class SubwayMap {
     e.preventDefault();
 
     const name = this.elements.stationNameInput.value;
-    if (!this.hasValidStationName(name)) return;
+    if (!this.isValidStationName(name)) return;
 
     const station = new Station(name);
     this.stationList.push(station);
@@ -152,11 +168,82 @@ class SubwayMap {
     this.elements.stationForm.addEventListener('submit', this.handleSubmitStationAdd.bind(this));
     this.elements.stationList.addEventListener('click', this.handleClickStationDelete.bind(this));
 
-    this.loadStationList();
     this.showStationElementsAll();
   }
 
+  getStationSelectorOptions() {
+    return this.stationList.map((station) => `<option value="${station.name}">${station.name}</options>`).join('');
+  }
+
+  isExistLine(name) {
+    return this.lineList.some((line) => line.name === name);
+  }
+
+  isValidLine(name, startStation, endStation) {
+    if (!this.isValidNameLength(name)) {
+      alert(`${MINIMUM_INPUT_LENGTH}글자 이상 입력해주세요`);
+      return false;
+    }
+
+    if (this.isExistLine(name)) {
+      alert(`이미 존재하는 노선 이름입니다`);
+      return false;
+    }
+
+    if (!startStation || !endStation) {
+      alert('종점역을 올바르게 지정해주세요');
+      return false;
+    }
+
+    if (startStation === endStation) {
+      alert('종점역은 서로 다르게 지정해주세요');
+      return false;
+    }
+
+    return true;
+  }
+
+  addLineListItemElement(line) {
+    this.elements.lineListTableBody.innerHTML += `
+      <tr data-name="${line.name}">
+        <td>${line.name}</td>
+        <td>${line.startStation.name}</td>
+        <td>${line.endStation.name}</td>
+        <td>
+          <button class="line-delete-button" data-name="${line.name}">
+            삭제
+          </button>
+        </td>
+      </tr>
+    `;
+  }
+
+  handleSubmitLineAdd(e) {
+    e.preventDefault();
+
+    const name = this.elements.lineNameInput.value;
+    const startStation = this.getStation(this.elements.lineStartStationSelector.value);
+    const endStation = this.getStation(this.elements.lineEndStationSelector.value);
+
+    if (!this.isValidLine(name, startStation, endStation)) return;
+
+    const line = new Line(name, startStation, endStation);
+    this.lineList.push(line);
+    this.addLineListItemElement(line);
+
+    this.elements.lineNameInput.value = '';
+  }
+
+  isValidLineManager() {
+    return this.stationList.length >= MINIMUM_STATION_COUNT;
+  }
+
   showLineManager() {
+    if (!this.isValidLineManager()) {
+      alert('등록된 역이 2개 이상이어야 노선 관리 메뉴에 진입할 수 있습니다.');
+      return;
+    }
+
     this.elements.managerContainer.innerHTML = `
       <form id="line-form">
         <label for="line-name-input">노선 이름</label>
@@ -181,8 +268,25 @@ class SubwayMap {
             <th>설정</th>
           </tr>
         </thead>
+        <tbody>
+        </tbody>
       </table>
     `;
+
+    this.elements = {
+      ...this.elements,
+      lineForm: document.querySelector('#line-form'),
+      lineNameInput: document.querySelector('#line-name-input'),
+      lineStartStationSelector: document.querySelector('#line-start-station-selector'),
+      lineEndStationSelector: document.querySelector('#line-end-station-selector'),
+      lineList: document.querySelector('#line-list'),
+      lineListTableBody: document.querySelector('#line-list tbody'),
+    };
+
+    this.elements.lineStartStationSelector.innerHTML = this.getStationSelectorOptions();
+    this.elements.lineEndStationSelector.innerHTML = this.getStationSelectorOptions();
+
+    this.elements.lineForm.addEventListener('submit', this.handleSubmitLineAdd.bind(this));
   }
 
   showSectionManager() {
