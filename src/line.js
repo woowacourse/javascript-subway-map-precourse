94fa352c5fa72e95ca8159e06b5c3fdbb5da980e
lulu-kang <dalkang.hy@gmail.com>
2020-12-13 22:50:47 +0900

    feat: 노선 이름 받는 함수 구현

--- src/line.js
+++ src/line.js
@@ -1,4 +1,19 @@
+import { isSpecialCharacter, isDuplicated } from './check.js'
+
 export default function Line() {
+  this.getLineName = function() {
+    const lineNameInput = document.querySelector("#line-name-input");
+    lineNameInput.addEventListener("change", () => {
+      const lineName = document.querySelector("#line-name-input").value;
+      const alertText = "노선 목록에 없는 단어를 입력해 주세요."
+      if (!isSpecialCharacter(lineName) && !isDuplicated(lineName)) {
+        console.log(lineName);
+      } else {
+        alert(alertText);
+      }
+    })
+  }
+
   this.addOption = function(startStationSelector, endStationSelector) {
     let i;
 
@@ -17,6 +32,7 @@ export default function Line() {
     const startStationSelector = document.querySelector("#line-start-station-selector");
     const endStationSelector = document.querySelector("#line-end-station-selector");
     this.addOption(startStationSelector, endStationSelector);
+    this.getLineName();
   }
 
   this.init();
