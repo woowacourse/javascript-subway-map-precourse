4ba519780f8fd910a7d4e1363a6629db57cef526
intae92 <kangit2013@gmail.com>
2020-12-13 15:20:28 +0900

    feat: 역이름과 노선을 저장할 초기 localStorage 생성 - 최상위 부모 Component.js 에서 localStorage 생성

--- src/js/Component.js
+++ src/js/Component.js
@@ -4,5 +4,35 @@ export default class Component {
   constructor() {
     console.log("-----Component------");
     this._app = document.getElementById(DOM_ID.ID);
+
+    this.stations = [];
+    this.lines = [];
+
+    this.initLocalStorage();
+  }
+
+  initLocalStorage() {
+    this.stationStorage();
+    this.lineStorage();
+  }
+
+  stationStorage() {
+    const storageKey = "STATION";
+    const loadedStorage = localStorage.getItem(storageKey);
+
+    if (loadedStorage === null) {
+      localStorage.setItem(storageKey, JSON.stringify(this.stations));
+    } else {
+    }
+  }
+
+  lineStorage() {
+    const storageKey = "LINE";
+    const loadedStorage = localStorage.getItem(storageKey);
+
+    if (loadedStorage === null) {
+      localStorage.setItem(storageKey, JSON.stringify(this.stations));
+    } else {
+    }
   }
 }
