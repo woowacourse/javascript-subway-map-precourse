4bff7a38b3d974d5f815866bf911c73e5f0142db
shinhyogeun <tls1gy2rms3@naver.com>
2020-12-13 15:44:34 +0900

    refactor : 버튼에 이벤트를 추가하는 것을 모두 stationManager.js에서 실행하도록 변경

--- src/stationManager/stationManager.js
+++ src/stationManager/stationManager.js
@@ -4,15 +4,17 @@ import {
     addStationNameToTable,
     stationNameInputClear,
     removeStationManagerHTML
-} from "./stationManagerView.js"
-import { makeStationIfPossible } from "../stationFactory.js"
+} from "./stationView.js"
+import { makeStationIfPossible, checkStationInLine } from "./stationController.js"
+import { alertText } from "../text.js"
 import { Data } from "../data.js"
 
 export class StationManager {
 
     static show = () => {
         addResultToBody(makeStationHTML(Data.getStationRepository()));
-        addEventToAddStationButton()
+        addEventToAddStationButton();
+        addEventToDeleteButton();
     }
 
     static hide = () => {
@@ -30,9 +32,27 @@ const addEventToAddStationButton = () => {
             const station = makeStationIfPossible(addStaitonInput.value);
             Data.addStation(station);
             addStationNameToTable(station.name);
+            addEventToDeleteButton();
             stationNameInputClear();
         } catch (error) {
             alert(error)
         }
     });
+}
+
+const addEventToDeleteButton = () => {
+    Array.prototype.forEach.call(document.querySelectorAll(".station-delete-button"), (button) => {
+        button.addEventListener("click", function (button) {
+            const stationName = button.target.parentNode.parentNode.dataset.stationName
+            try {
+                checkStationInLine(stationName);
+                if (confirm(alertText.CONFIRM_MESSAGE)) {
+                    Data.removeStation(stationName);
+                    document.querySelector("#station-name-table tbody").removeChild(this.parentElement.parentElement);
+                }
+            } catch (error) {
+                alert(error);
+            }
+        })
+    })
 }
