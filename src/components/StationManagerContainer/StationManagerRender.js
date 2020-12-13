453ff0583d286caca5a609f36b01384363e28bb2
intae92 <kangit2013@gmail.com>
2020-12-13 14:37:42 +0900

    feat: 역 이름 입력 요소 렌더링 구현

--- src/components/StationManagerContainer/StationManagerRender.js
+++ src/components/StationManagerContainer/StationManagerRender.js
@@ -1,8 +1,38 @@
 import Component from "../../js/Component.js";
+import { DOM_STATION } from "../../utils/constants.js";
 
 export default class StationManagerRender extends Component {
   constructor() {
     super();
     console.log("--StationManagerRender--");
+    this.initDOM();
+    this.render();
+    this.createDOM();
+  }
+
+  initDOM() {
+    this.stationNameInputElement = document.createElement("div");
+    this.stationNameForm = "";
+    console.log(this.stationNameForm, "=-=-=-=-");
+  }
+
+  render() {
+    this.stationNameInputElement.innerHTML = this.stationNameInputRender();
+
+    this._app.append(this.stationNameInputElement);
+  }
+
+  createDOM() {
+    this.stationNameForm = document.getElementById(DOM_STATION.STATION_FORM_ID);
+  }
+
+  stationNameInputRender() {
+    return `  
+      <label for=${DOM_STATION.STATION_NAME_INPUT_ID}>역 이름</label>
+      <form id=${DOM_STATION.STATION_FORM_ID}>
+        <input type="text" id=${DOM_STATION.STATION_NAME_INPUT_ID} placeholder="역 이름을 입력해 주세요" />
+        <button id=${DOM_STATION.STATION_ADD_BUTTON_ID}>역 추가</button>
+      </form>
+    `;
   }
 }
--- src/js/Component.js
+++ src/js/Component.js
@@ -1,5 +1,8 @@
+import { DOM_ID } from "../utils/constants.js";
+
 export default class Component {
   constructor() {
     console.log("-----Component------");
+    this._app = document.getElementById(DOM_ID.ID);
   }
 }
