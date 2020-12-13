a3b11e2e05f034cde1488b5ed9a32497e9c66c97
권세진 <0307kwon@gmail.com>
2020-12-13 17:07:58 +0900

    feat: 노선도 정보를 받아와 출력함

--- src/ui/contents-ui/map-print-manager-ui.js
+++ src/ui/contents-ui/map-print-manager-ui.js
@@ -3,28 +3,36 @@ import { contentsUI } from "./contents-ui.js";
 export default class MapPrintManager extends contentsUI {
   constructor(contentsID, stationINFOManager) {
     super(contentsID, stationINFOManager);
-    this.setContentsHTML(INITIAL_TEMPLATE);
+    this.setContentsHTML("");
   }
 
   setContentsHTML(initialTemplate) {
+    const lines = this.stationINFOManager_.getLineINFOs();
+    lines.forEach((line) => {
+      initialTemplate += this.makeLineINFOHTML_(line);
+    });
     super.setContentsHTML(initialTemplate);
+  }
+
+  makeLineINFOHTML_(line) {
+    const stationListHTML = this.makeStationListHTML_(line);
+    const infoHTML = `
+    <div class="${MAP_CLASS}">
+      <h2>${line.name}</h2>
+      <ul>
+        ${stationListHTML}
+      </ul>
+    </div>
+    `;
+    return infoHTML;
+  }
+  makeStationListHTML_(line) {
+    let listHTML = "";
+    line.stationsOfLine.forEach((station) => {
+      listHTML += `<li>${station.name}</li>`;
+    });
+    return listHTML;
   }
 }
 
-const INITIAL_TEMPLATE = `
-<h2>1호선</h2>
-<ul>
-  <li>인천</li>
-  <li>소요산</li>
-</ul>
-<h2>2호선</h2>
-<ul>
-  <li>인천</li>
-  <li>소요산</li>
-</ul>
-<h2>3호선</h2>
-<ul>
-  <li>인천</li>
-  <li>소요산</li>
-</ul>
-`;
+const MAP_CLASS = "map";
