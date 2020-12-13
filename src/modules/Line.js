6eb0db4f545981be7c95f495a904f9545887147f
dande <7bdande@gmail.com>
2020-12-13 14:02:33 +0900

    feat: [Line] 새로운 노선 등록

--- src/modules/Line.js
+++ src/modules/Line.js
@@ -1,8 +1,15 @@
+import Station from "./Station.js";
 import { LINE_LIST } from "../constant/constant.js";
+import {
+  INVAILD_LINE_NAME_LENGTH_ALERT,
+  INVALID_LINE_STATION,
+  SAVED_LINE_ALERT,
+} from "../constant/message.js";
 
 export default class Line {
   constructor() {
     this._lineList = this._getSavedLineList();
+    this._station = new Station();
   }
 
   _isSavedLine = name => {
@@ -19,5 +26,40 @@ export default class Line {
     const savedLineList = localStorage.getItem(LINE_LIST);
 
     return JSON.parse(savedLineList) || [];
+  };
+
+  _getLineObject = (name, startStation, endStation) => {
+    return { name, list: [startStation, endStation] };
+  };
+
+  _failToSaveNewStation = (isValidLength, isSavedLine, isValidStation) => {
+    if (!isValidLength) {
+      alert(INVAILD_LINE_NAME_LENGTH_ALERT);
+    }
+
+    if (isSavedLine) {
+      alert(SAVED_LINE_ALERT);
+    }
+
+    if (!isValidStation) {
+      alert(INVALID_LINE_STATION);
+    }
+  };
+
+  saveNewLine = (name, startStation, endStation) => {
+    const isValidLength = name.length > 0;
+    const isSavedLine = this._isSavedLine(name);
+    const isValidStation = startStation !== endStation;
+
+    if (isValidLength && !isSavedLine && isValidStation) {
+      this._lineList.push(this._getLineObject(name, startStation, endStation));
+      this._setLineList(this._lineList);
+      this._station.saveLineToStation(name, startStation);
+      this._station.saveLineToStation(name, endStation);
+
+      return;
+    }
+
+    this._failToSaveNewStation(isValidLength, isSavedLine, isValidStation);
   };
 }
