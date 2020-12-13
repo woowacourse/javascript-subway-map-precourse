4c0c6ee9d1263cb7ec1821135e05514025caa2ad
dande <7bdande@gmail.com>
2020-12-13 16:05:51 +0900

    feat: [LineManager] 등록된 노선 목록 출력

--- src/modules/LineManager.js
+++ src/modules/LineManager.js
@@ -1,5 +1,6 @@
 import Station from "./Station.js";
 import Line from "./Line.js";
+import { DELETE_BUTTON } from "../constant/constant.js";
 
 export default class LineManager {
   constructor() {
@@ -18,7 +19,51 @@ export default class LineManager {
     this._render();
   }
 
+  _createNameTd = name => {
+    const nameTd = document.createElement("td");
+    nameTd.textContent = name;
+
+    return nameTd;
+  };
+
+  _createDeleteButtonTd = name => {
+    const deleteButtonTd = document.createElement("td");
+    const deleteButton = document.createElement("button");
+
+    deleteButton.classList.add("line-delete-button");
+    deleteButton.dataset.name = name;
+    deleteButton.textContent = DELETE_BUTTON;
+    deleteButton.addEventListener("click", this._deleteLine);
+    deleteButtonTd.appendChild(deleteButton);
+
+    return deleteButtonTd;
+  };
+
+  _setTableElements = (name, list) => {
+    const tr = document.createElement("tr");
+    const lineNameTd = this._createNameTd(name);
+    const startStationTd = this._createNameTd(list[0]);
+    const endStationTd = this._createNameTd(list[list.length - 1]);
+    const deleteButtonTd = this._createDeleteButtonTd(name);
+
+    tr.appendChild(lineNameTd);
+    tr.appendChild(startStationTd);
+    tr.appendChild(endStationTd);
+    tr.appendChild(deleteButtonTd);
+    this._tableBody.appendChild(tr);
+  };
+
+  _printLineList = () => {
+    const lineList = this._line.getLineList();
+    this._tableBody.innerHTML = "";
+
+    lineList.forEach(({ name, list }) => {
+      this._setTableElements(name, list);
+    });
+  };
+
   _render = () => {
+    this._printLineList();
     this._station.setStationOptions(this._startStationSelector);
     this._station.setStationOptions(this._endStationSelector);
   };
