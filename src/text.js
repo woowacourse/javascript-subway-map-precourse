7c54c6cdc79483b6135e3dcaf859a8c6548ac76f
shinhyogeun <tls1gy2rms3@naver.com>
2020-12-13 15:36:21 +0900

    feat : 하드코딩을 피하기위해 새로운 상수들 생성

--- src/text.js
+++ src/text.js
@@ -3,7 +3,9 @@ export const alertText = {
     SHORT_STATION_NAME: "역 이름은 2글자 이상이어야 합니다.",
     EMPTY_STATION_NAME: "역 이름을 입력해주세요.",
     SAME_LINE_EXITE: "똑같은 호선이 존재합니다.",
-    EMPTY_LINE_NAME: "호선 이름을 입력해주세요."
+    EMPTY_LINE_NAME: "호선 이름을 입력해주세요.",
+    STATION_ALREADY_IN_LINE: "호선에 등록되어 있는 역은 삭제할 수 없습니다.",
+    CONFIRM_MESSAGE: "정말로 삭제하시겠습니까?"
 }
 
 export const text = {
