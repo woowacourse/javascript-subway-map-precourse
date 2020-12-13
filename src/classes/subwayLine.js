108f1fb00d9b9c8c9587d50671e9bed0e4e3e324
swon3210 <swon3210@gmail.com>
2020-12-14 00:19:17 +0900

    feat: get all subway stations in subway line

--- src/classes/subwayLine.js
+++ src/classes/subwayLine.js
@@ -11,4 +11,23 @@ export default class SubwayLine {
     this.upTerminatingStation = upTerminatingStation;
     this.downTerminatingStation = downTerminatingStation;
   }
+
+  #getNextStationInLine(station) {
+    const nextStationInLine = station.nextStations.find((nextStation) =>
+      nextStation.belongingLineNames.some((lineName) => lineName === this.name)
+    );
+
+    return nextStationInLine;
+  }
+
+  get allStations() {
+    const allStations = [];
+    let nextStation = this.#getNextStationInLine(this.upTerminatingStation);
+    while (nextStation) {
+      allStations.push(allStations);
+      nextStation = this.#getNextStationInLine(nextStation);
+    }
+
+    return allStations;
+  }
 }
