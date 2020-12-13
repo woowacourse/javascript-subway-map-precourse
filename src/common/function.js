5e24f7d915400c905cbf05002336fc1cb836fa06
dawit jeong <wjdekdnlt1215@gmail.com>
2020-12-14 05:17:02 +0900

    add setStateAndLocalSrorage

--- src/common/function.js
+++ src/common/function.js
@@ -2,6 +2,12 @@ import { setLocalStorageDataOf } from "../local_storage.js";
 import { resultDIV, setState, state } from "../state.js";
 
 const Function = function () {
+  this.setStateAndLocalSrorage = (stationsOrLines, data) => {
+    const stringData = JSON.stringify(data);
+    setState(stationsOrLines, stringData);
+    setLocalStorageDataOf(stationsOrLines, stringData);
+  };
+
   this.isEmptyPage = () => state.currentPage === "0";
   this.isAlreadyRenderedPage = (page) => state.currentPage === page;
 
