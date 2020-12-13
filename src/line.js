7af7ab424977e3f048d4ac0376a21d1e658d322b
lulu-kang <dalkang.hy@gmail.com>
2020-12-13 22:19:22 +0900

    feat: select의 option에 등록된 지하철 역 띄우는 함수 구현

--- src/line.js
+++ src/line.js
@@ -1,5 +1,25 @@
 export default function Line() {
+  this.addOption = function(startStationSelector, endStationSelector) {
+    let i;
 
+    for (i = 0; i < localStorage.length; i++) {
+      const key = localStorage.key(i);
+      const parsedStationObject = JSON.parse(localStorage.getItem(key));
+
+      if (parsedStationObject[0] === "station") {
+        startStationSelector.innerHTML += `<option>${key}</option>`;
+        endStationSelector.innerHTML += `<option>${key}</option>`;
+      }
+    }
+  }
+
+  this.init = function() {
+    const startStationSelector = document.querySelector("#line-start-station-selector");
+    const endStationSelector = document.querySelector("#line-end-station-selector");
+    this.addOption(startStationSelector, endStationSelector);
+  }
+
+  this.init();
 }
 
 new Line();
