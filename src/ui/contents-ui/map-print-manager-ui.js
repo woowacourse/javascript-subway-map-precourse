2e7dc93e42782398214ccaed0e436df750f2a2f2
권세진 <0307kwon@gmail.com>
2020-12-13 16:44:32 +0900

    feat: 노선도 출력 html string 작성

--- src/ui/contents-ui/map-print-manager-ui.js
+++ src/ui/contents-ui/map-print-manager-ui.js
@@ -1,15 +1,30 @@
-export default class MapPrintManager {
+import { contentsUI } from "./contents-ui.js";
+
+export default class MapPrintManager extends contentsUI {
   constructor(contentsID, stationINFOManager) {
-    this.contentsID_ = contentsID;
-    this.stationINFOManager_ = stationINFOManager;
-    this.setContentsHTML();
+    super(contentsID, stationINFOManager);
+    this.setContentsHTML(INITIAL_TEMPLATE);
   }
 
-  setContentsHTML() {
-    document.getElementById(this.contentsID_).innerHTML = TEMPLATE;
+  setContentsHTML(initialTemplate) {
+    super.setContentsHTML(initialTemplate);
   }
 }
 
-const TEMPLATE = `
-mapPrintManager Template 입니다.
+const INITIAL_TEMPLATE = `
+<h2>1호선</h2>
+<ul>
+  <li>인천</li>
+  <li>소요산</li>
+</ul>
+<h2>2호선</h2>
+<ul>
+  <li>인천</li>
+  <li>소요산</li>
+</ul>
+<h2>3호선</h2>
+<ul>
+  <li>인천</li>
+  <li>소요산</li>
+</ul>
 `;
