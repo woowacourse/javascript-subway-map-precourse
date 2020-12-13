becc35829964d390691ea1a76dc43c10b1fd59ee
2SOOY <1401660@naver.com>
2020-12-13 17:14:06 +0900

    Feat - 역 이름 추가를 위한 검증 유틸 함수 : isValidStationName
    - 빈 값 여부 체크
    - 이름 2글자 이상인지 확인
    - 중복된 이름이 존재하지 않은지 확인

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,4 +1,29 @@
-export const isValidNameLength = (name, limit) => name.length >= limit;
+import { MESSAGE } from "../constants/message.js";
+import { NUM } from "../constants/number.js";
+import { alertMessage } from "./domUtil.js";
 
-export const isDuplicatedName = (name, nameList) =>
-  nameList.every(target => target !== name);
+const isValidNameLength = (name, limit) => name.length >= limit;
+
+const isDuplicatedName = (name, nameList) =>
+  nameList.some(target => target === name);
+
+const isEmptyValue = value => !value;
+
+export const isVaildStationName = ($input, nameList, name) => {
+  if (isEmptyValue(name)) {
+    alertMessage($input, MESSAGE.EMPTY_NAME_ERROR);
+    return false;
+  }
+
+  if (!isValidNameLength(name, NUM.STATION_NAME_LIMIT)) {
+    alertMessage($input, MESSAGE.STATION_NAME_LIMIT_ERROR);
+    return false;
+  }
+
+  if (isDuplicatedName(name, nameList)) {
+    alertMessage($input, MESSAGE.DUPLICATED_NAME_ERROR);
+    return false;
+  }
+
+  return true;
+};
