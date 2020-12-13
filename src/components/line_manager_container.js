ac0c19fdf16c56c4b65d1eacea9b70a016e39cce
seojihwan <seojihwan213@naver.com>
2020-12-13 18:37:28 +0900

    feat: 잘못된 노선입력 alert
    1.이름 공백인 경우
    2.지하철 노선 이름 중복된 경우
    3.상행역 하행역 같은 경우

--- src/components/line_manager_container.js
+++ src/components/line_manager_container.js
@@ -1,4 +1,5 @@
 import { lineAddContainer, lineList } from '../templates/index.js';
+import { isValidLine, isValidLineName } from '../utils/index.js';
 
 export default function LineManagerContainer({
   addLine,
@@ -14,10 +15,17 @@ export default function LineManagerContainer({
   };
 
   this.addLine = () => {
-    const name = document.getElementById('line-name-input').value;
+    const name = document
+      .getElementById('line-name-input')
+      .value.trim('')
+      .replace(/\s{2,}/g, ' ');
     const start = document.getElementById('line-start-station-selector').value;
     const end = document.getElementById('line-end-station-selector').value;
-    addLine({ name, start, end });
+    const lines = getLines();
+    const newLine = { name, start, end };
+    if (isValidLineName(lines, newLine) && isValidLine(newLine)) {
+      addLine(newLine);
+    }
   };
 
   this.render = () => {
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -11,3 +11,26 @@ export const isValidStationName = (stations, name) => {
 
   return true;
 };
+
+export const isValidLineName = (lines, newLine) => {
+  if (newLine.name.length < 1) {
+    alert('노선은 1글자 이상이어야 합니다.');
+    return false;
+  }
+
+  if (lines.map(line => line.name).includes(newLine.name)) {
+    alert('중복된 노선 이름입니다.');
+    return false;
+  }
+
+  return true;
+};
+
+export const isValidLine = newLine => {
+  if (newLine.start === newLine.end) {
+    alert('상행과 하행역은 달라야 합니다');
+    return false;
+  }
+
+  return true;
+};
