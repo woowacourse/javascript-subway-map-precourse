b900879fcfd594f98db8b19c70e34891bcc109ee
joyfuI <jong970105@gmail.com>
2020-12-13 14:18:58 +0900

    버튼 탭 부분 구현

--- src/index.js
+++ src/index.js
@@ -57,3 +57,57 @@ export default class SubwayMap {
     return true;
   }
 }
+
+const stationManagerButton = document.getElementById('station-manager-button');
+const lineManagerButton = document.getElementById('line-manager-button');
+const sectionManagerButton = document.getElementById('section-manager-button');
+const mapPrintManagerButton = document.getElementById('map-print-manager-button');
+const station = document.getElementById('station');
+const line = document.getElementById('line');
+const section = document.getElementById('section');
+const mapPrint = document.getElementById('map-print');
+
+stationManagerButton.addEventListener('click', showStationManager);
+lineManagerButton.addEventListener('click', showLineManager);
+sectionManagerButton.addEventListener('click', showSectionManager);
+mapPrintManagerButton.addEventListener('click', showMapPrintManager);
+
+// 역 관리 탭
+function showStationManager() {
+  "use strict";
+
+  station.classList.remove('d-none');
+  line.classList.add('d-none');
+  section.classList.add('d-none');
+  mapPrint.classList.add('d-none');
+}
+
+// 노선 관리 탭
+function showLineManager() {
+  "use strict";
+
+  station.classList.add('d-none');
+  line.classList.remove('d-none');
+  section.classList.add('d-none');
+  mapPrint.classList.add('d-none');
+}
+
+// 구간 관리 탭
+function showSectionManager() {
+  "use strict";
+
+  station.classList.add('d-none');
+  line.classList.add('d-none');
+  section.classList.remove('d-none');
+  mapPrint.classList.add('d-none');
+}
+
+// 지하철 노선도 출력 탭
+function showMapPrintManager() {
+  "use strict";
+
+  station.classList.add('d-none');
+  line.classList.add('d-none');
+  section.classList.add('d-none');
+  mapPrint.classList.remove('d-none');
+}
