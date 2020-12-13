4de46d74169b4a44f3145f47284f95e26acef2b4
yungo1846 <yungo1468@naver.com>
2020-12-13 19:10:43 +0900

    docs: 불필요한 Manager 폴더 삭제

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-import Station from "./manager/station.js";
-import Line from "./manager/line.js";
-import Section from "./manager/section.js";
-import MapPrint from "./manager/mapPrint.js";
 import menuEvent from "./event/menuEvent.js";
 
 export default function SubwayMap() {
