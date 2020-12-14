2f7e3e7dd0de4adb8ef6030fbe1a3583691e0deb
minkyung <mnk918p@gmail.com>
2020-12-14 17:06:20 +0900

    feat: 지하철 구간 관리 - 역 삭제/등록시 구간 등록 표에 반영하는 기능 구현

--- src/components/section-manage-container.js
+++ src/components/section-manage-container.js
@@ -1,5 +1,15 @@
 import { state, saveToLocalStorage } from "../index.js";
 import { LINE_ARRAY_KEY } from "../global/constant.js";
+import { getStationByName } from "../utils/global-utils.js";
+
+const SECTION_TAGS = {
+  PARENT_SECTION_ID: "manage-sections",
+  LINE_MENU_BUTTON_CLASS: "section-line-menu-button",
+  STATION_SELECTOR_ID: "section-station-selector",
+  ORDER_INPUT_ID: "section-order-input",
+  ADD_BUTTON_ID: "section-add-button",
+  DELETE_BUTTON_CLASS: "section-delete-button"
+};
 
 function removeAllChild(element) {
   while (element.firstChild) {
@@ -8,11 +18,12 @@ function removeAllChild(element) {
 }
 
 function loadEditSectionLines() {
-  const parentDiv = document.getElementById("manage-sections");
+  const parentDiv = document.getElementById(SECTION_TAGS.PARENT_SECTION_ID);
 
   for (const line of state.subwayLines) {
     const lineManageBtn = newElementWithInnerHtml("button", line.lineName);
-    lineManageBtn.id = `manage${line.id}`;
+    lineManageBtn.classList.add(SECTION_TAGS.LINE_MENU_BUTTON_CLASS);
+    lineManageBtn.dataset.id = `manage-${line.id}`;
 
     parentDiv.append(lineManageBtn);
   }
@@ -20,13 +31,12 @@ function loadEditSectionLines() {
 
 export default function sectionManageContainer() {
   loadEditSectionLines();
-  const parentDiv = document.getElementById("manage-sections");
+  const parentDiv = document.getElementById(SECTION_TAGS.PARENT_SECTION_ID);
   const lineWrapperDiv = document.createElement("div");
   lineWrapperDiv.id = "manage-wrapper";
 
   for (const line of state.subwayLines) {
-    const manage = document.getElementById(`manage${line.id}`);
-
+    const manage = document.querySelector(`[data-id="manage-${line.id}"]`);
     manage.addEventListener("click", () => {
       removeAllChild(lineWrapperDiv);
 
@@ -38,8 +48,8 @@ export default function sectionManageContainer() {
       `;
       appendHTML(lineWrapperDiv, manageHTML);
 
-      // select stations
       const selectWrapper = document.createElement("select");
+      selectWrapper.id = SECTION_TAGS.STATION_SELECTOR_ID;
 
       for (const station of state.stationArray) {
         const option = `<option value="${station.stationName}">${station.stationName}</option>`;
@@ -47,14 +57,12 @@ export default function sectionManageContainer() {
       }
       lineWrapperDiv.append(selectWrapper);
 
-      // input
       const sectionInputHTML = `
-          <input placeholder="순서" />
-          <button type="submit">등록</button>
+          <input id=${SECTION_TAGS.ORDER_INPUT_ID} placeholder="순서" />
+          <button type="submit" id=${SECTION_TAGS.ADD_BUTTON_ID}>등록</button>
       `;
       appendHTML(lineWrapperDiv, sectionInputHTML);
 
-      // table
       const sectionTableHTML = `
         <table border="1" id="section-table">
           <thead>
@@ -71,33 +79,108 @@ export default function sectionManageContainer() {
 
       parentDiv.append(lineWrapperDiv);
 
-      // table items
       const table = document.getElementById("section-table");
       const tableBody = document.getElementById("section-tbody");
 
-      for (const [index, station] of Object.entries(line.stations)) {
+      const sectionInputBtn = document.getElementById(SECTION_TAGS.ADD_BUTTON_ID);
+      const sectionInput = document.getElementById(SECTION_TAGS.ORDER_INPUT_ID);
+      const selectedOption = document.getElementById(SECTION_TAGS.STATION_SELECTOR_ID);
+
+      sectionInputBtn.addEventListener("click", () => {
         const tr = document.createElement("tr");
-        const tdIndex = newElementWithInnerHtml("td", index);
-        const tdStationName = newElementWithInnerHtml("td", station.stationName);
+        tr.dataset.stationId = getStationByName(selectedOption.value).id;
+
+        let indexOfNewElement = line.stations.length;
+        if (sectionInput.value < line.stations.length) {
+          indexOfNewElement = sectionInput.value;
+        }
+        tr.dataset.trId = indexOfNewElement;
+
+        const tdIndex = newElementWithInnerHtml("td", indexOfNewElement);
+        const tdStationName = newElementWithInnerHtml(
+          "td",
+          getStationByName(selectedOption.value).stationName
+        );
+        line.stations.splice(
+          parseInt(sectionInput.value),
+          0,
+          getStationByName(selectedOption.value)
+        );
 
         const tdDeleteBtn = newElementWithInnerHtml("button", "노선에서 삭제");
-        tdDeleteBtn.id = station.id;
-        const tdDelete = newTdWithElement(tdDeleteBtn);
+        tdDeleteBtn.class = SECTION_TAGS.DELETE_BUTTON_CLASS;
+        tdDeleteBtn.dataset.id = getStationByName(selectedOption.value).id;
 
-        tr.append(...[tdIndex, tdStationName, tdDelete]);
-        tableBody.append(tr);
+        const tdDelete = newTdWithElement(tdDeleteBtn);
 
         tdDeleteBtn.addEventListener("click", () => {
           if (confirm("정말로 삭제하시겠습니까?")) {
             const deleteStationFromLine = line.stations.filter(station => {
-              return parseInt(tdDeleteBtn.id) !== parseInt(station.id);
+              return parseInt(tdDeleteBtn.dataset.id) !== parseInt(station.id);
             });
             tr.remove();
 
             line.stations = deleteStationFromLine;
+            for (
+              let index = parseInt(tdIndex.innerHTML);
+              index < tableBody.children.length;
+              index++
+            ) {
+              tableBody.children[index].firstChild.innerHTML =
+                parseInt(tableBody.children[index].firstChild.innerHTML) - 1;
+            }
           }
           saveToLocalStorage(LINE_ARRAY_KEY, JSON.stringify(state.subwayLines));
         });
+
+        for (let index = parseInt(indexOfNewElement); index < tableBody.children.length; index++) {
+          tableBody.children[index].firstChild.innerHTML =
+            parseInt(tableBody.children[index].firstChild.innerHTML) + 1;
+        }
+
+        tr.append(...[tdIndex, tdStationName, tdDelete]);
+        tableBody.insertBefore(tr, tableBody.children[indexOfNewElement]);
+
+        saveToLocalStorage(LINE_ARRAY_KEY, JSON.stringify(state.subwayLines));
+      });
+
+      for (const [index, station] of Object.entries(line.stations)) {
+        const tr = document.createElement("tr");
+        tr.dataset.stationId = station.id;
+        tr.dataset.trId = index;
+
+        const tdIndex = newElementWithInnerHtml("td", index);
+        const tdStationName = newElementWithInnerHtml("td", station.stationName);
+
+        const tdDeleteBtn = newElementWithInnerHtml("button", "노선에서 삭제");
+        tdDeleteBtn.class = SECTION_TAGS.DELETE_BUTTON_CLASS;
+        tdDeleteBtn.dataset.id = station.id;
+
+        const tdDelete = newTdWithElement(tdDeleteBtn);
+
+        tdDeleteBtn.addEventListener("click", () => {
+          if (confirm("정말로 삭제하시겠습니까?")) {
+            const deleteStationFromLine = line.stations.filter(station => {
+              return parseInt(tdDeleteBtn.dataset.id) !== parseInt(station.id);
+            });
+            tr.remove();
+
+            line.stations = deleteStationFromLine;
+
+            for (
+              let index = parseInt(tdIndex.innerHTML);
+              index < tableBody.children.length;
+              index++
+            ) {
+              tableBody.children[index].firstChild.innerHTML =
+                parseInt(tableBody.children[index].firstChild.innerHTML) - 1;
+            }
+          }
+          saveToLocalStorage(LINE_ARRAY_KEY, JSON.stringify(state.subwayLines));
+        });
+
+        tr.append(...[tdIndex, tdStationName, tdDelete]);
+        tableBody.append(tr);
       }
       table.append(tableBody);
       lineWrapperDiv.append(table);
@@ -109,12 +192,14 @@ export default function sectionManageContainer() {
 function newElementWithInnerHtml(elementType, innerHTML) {
   const element = document.createElement(elementType);
   element.innerHTML = innerHTML;
+
   return element;
 }
 
 function newTdWithElement(element) {
   const td = document.createElement("td");
   td.append(element);
+
   return td;
 }
 
