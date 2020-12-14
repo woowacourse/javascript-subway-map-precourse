3c253e2b0ab8d7039663b1397ac4e7effa643f8b
365kim <365listener@gmail.com>
2020-12-14 15:16:57 +0900

    fix: 갓 추가된 구간은 검사대상에서 빠져 또 다시 추가할 수 있었던 문제 해결

--- src/managers/section/adder.js
+++ src/managers/section/adder.js
@@ -1,13 +1,19 @@
 import { Line } from '../../classes/line.js';
 import { createSectionManagerUI } from './launcher.js';
-import { addSubItemToLocalStroage } from '../../util/util-local-storage.js';
+import {
+  getItemFromLocalStorage,
+  addSubItemToLocalStroage,
+} from '../../util/util-local-storage.js';
 import { emptyElement } from '../../util/util-ui.js';
 import { EXCEPTION_MESSAGE } from '../../configuration.js';
 
 // 3. 구간 관리 - 신규 구간 추가 요청
 export const requestToAddSection = (menu, lineSelected) => {
   const subContainer = document.getElementById('sub-container');
-  const line = new Line(lineSelected.name, lineSelected.stationList);
+  const line = new Line(
+    lineSelected.name,
+    getItemFromLocalStorage('line', lineSelected.name).stationList
+  );
   const stationSelector = document.getElementById(`${menu}-station-selector`);
   const orderInput = document.getElementById(`${menu}-order-input`);
   const exception = line.unableToAddSection(
