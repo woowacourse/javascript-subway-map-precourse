4ecaed0d7d4d5abe97cd2bcb382c174bd8864f81
lulu-kang <dalkang.hy@gmail.com>
2020-12-15 04:48:05 +0900

    feat: 테이블에 상행 종점과 하행 종점 띄우기 함수 구현

--- src/section-manager.js
+++ src/section-manager.js
@@ -1,5 +1,27 @@
 export default function SectionManager() {
-  this.addShowEvent = function(key) {
+  this.getLineStations = function(lineName) {
+    let objects = JSON.parse(localStorage.getItem("line"));
+    let i;
+    for (i = 0; i < objects.length; i++) {
+      if (objects[i].name === lineName) {
+        return objects[i].line
+      }
+    }
+  }
+
+  this.addTable = function(lineName) {
+    const table = document.querySelector("#section-list")
+    const lineStations = this.getLineStations(lineName);
+    let i;
+    table.innerHTML = `<tr><th scope="row">순서</th><th scope="row">이름</th><th scope="row">설정</th></tr>`;
+    table.style.display = "table"
+    for (i = 0; i < lineStations.length; i++) {
+      const index = lineStations.indexOf(lineStations[i])
+      table.innerHTML += `<tr id="${lineStations[i]}"><td id="index">${index}</td><td>${lineStations[i]}</td><td><button>설정</button></td></tr>`
+    }
+  }
+
+  this.addShowEvent = function() {
     const sectionLineMenuButton = document.getElementsByClassName("section-line-menu-button");
     let i;
     for (i = 0; i < sectionLineMenuButton.length; i++) {
@@ -9,6 +31,7 @@ export default function SectionManager() {
         const lineText = document.querySelector("#line-text")
         section.style.display = "block"
         lineText.innerText = `${lineName} 관리`
+        this.addTable(lineName);
       })
     }
   }
@@ -19,14 +42,14 @@ export default function SectionManager() {
     for (i = 0; i < lines.length; i++) {
       const key = lines[i].name
       const select = document.querySelector("#select")
-      select.innerHTML += `<button type="button" data-button-id="${key}" class="section-line-menu-button">${key}</button>`
-      this.addShowEvent(key);
+      select.innerHTML += `<button type="button" id="select-button" data-button-id="${key}" class="section-line-menu-button">${key}</button>`
+      this.addShowEvent();
     }
   }
 
   this.init = function() {
    const section = document.querySelector("#section");
-   const table = document.querySelector("#table");
+   const table = document.querySelector("#section-list");
    section.style.display = "none";
    table.style.display = "none";
    this.addSelectButton();
