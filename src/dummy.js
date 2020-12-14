b94a4a1687e7b2069f7883cc320c4349264391e7
365kim <365listener@gmail.com>
2020-12-14 17:01:43 +0900

    test: 테스트용 데이터 및 코드 추가

--- src/dummy.js
+++ src/dummy.js
@@ -1,28 +1,48 @@
-export const setStationDummy = () => {
-  const stationList = [
-    {
-      name: '잠실',
-      lineList: ['2호선', '8호선'],
-    },
-    {
-      name: '잠실새내',
-      lineList: ['2호선'],
-    },
-    {
-      name: '잠실나루',
-      lineList: ['2호선'],
-    },
-  ];
-
-  localStorage.setItem('station', JSON.stringify(stationList));
-  const listAgain = JSON.parse(localStorage.getItem('station'));
-  console.log(listAgain);
-  listAgain.push(new Station('운동장'));
-  console.log(listAgain);
-  localStorage.setItem('station', JSON.stringify(stationList));
-};
-
-export const lineListDummy = [
-  { name: '2호선', stationList: ['잠실새내', '잠실', '잠실나루'] },
-  { name: '8호선', stationList: ['석촌', '잠실', '몽촌토성'] },
+// 콘솔 창에 전체 복사 붙여넣기
+const stationDummy = [
+  {
+    name: '몽촌토성',
+    lineList: ['8호선'],
+  },
+  {
+    name: '잠실나루',
+    lineList: ['2호선'],
+  },
+  {
+    name: '잠실',
+    lineList: ['2호선', '8호선'],
+  },
+  {
+    name: '잠실새내',
+    lineList: ['2호선'],
+  },
+  {
+    name: '종합운동장',
+    lineList: ['2호선', '9호선'],
+  },
+  {
+    name: '삼성',
+    lineList: ['2호선'],
+  },
+  {
+    name: '봉은사',
+    lineList: ['9호선'],
+  },
 ];
+
+const lineDummy = [
+  {
+    name: '2호선',
+    stationList: ['잠실나루', '잠실', '잠실새내', '종합운동장', '삼성'],
+  },
+  { name: '8호선', stationList: ['몽촌토성', '잠실'] },
+  { name: '9호선', stationList: ['종합운동장', '봉은사'] },
+];
+
+localStorage.clear();
+localStorage.setItem('station', JSON.stringify(stationDummy));
+localStorage.setItem('line', JSON.stringify(lineDummy));
+
+// 저장된 local storage 내용 확인
+JSON.parse(localStorage.line);
+JSON.parse(localStorage.station);
