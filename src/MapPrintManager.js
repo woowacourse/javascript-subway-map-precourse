be6cc75eb9e325b0e3a08a0192a2a7bfcb0ce680
bigsaigon333 <likepeppermint@gmail.com>
2020-12-14 11:05:41 +0900

    feat: 지하철 노선도 출력 기능 구현

--- src/MapPrintManager.js
+++ src/MapPrintManager.js
@@ -1,9 +1,31 @@
 import Component from "./Component.js";
+import { retrieveLineInfo } from "./util.js";
 
 export default class MapPrintManager extends Component {
   constructor({ $parent }) {
     super({ $parent });
-    this.$component.innerHTML = "MapPrintManager";
+    this.$component.className = "map";
+
+    this.initializeVariables();
+    
+    this.render();
   }
 
+  initializeVariables() {
+    this.lineInfo = retrieveLineInfo()
+      .sort(({ lineName: aLineName }, { lineName: bLineName }) => {
+        return aLineName < bLineName ? -1 : 1;
+      });
+  }
+
+  render() {
+    this.$component.innerHTML = this.lineInfo.map(({ lineName, stations }) => {
+      return `
+        <div>${lineName}</div>
+        <ul>
+          ${stations.map(stationName => `<li>${stationName}</li>`).join("")}
+        </ul>
+      `;
+    }).join("");
+  }
 }
