64b9dc08c005c01d22c27357288fc4072feb630e
song <johyesong8686@gmail.com>
2020-12-14 20:13:25 +0900

    feat: Add Functions about Add & Remove & Print Station

--- src/utils/controlView.js
+++ src/utils/controlView.js
@@ -1,4 +1,5 @@
 import { STATION_DIV } from "../constant.js";
+import { removeStationHandler } from "./eventHandler.js";
 export const cleanView = () => {
   const children = document.getElementById("app").children;
   for (let i = STATION_DIV; i < children.length; i += 1) {
@@ -12,9 +13,42 @@ export const cleanPreView = (num) => {
     if (i !== num) children[i].style.display = "none";
   }
 };
-
 export const controlDisplay = (child) => {
   child.style.display === "none"
     ? (child.style.display = "")
     : (child.style.display = "none");
 };
+
+export function tableButton(buttonNodes) {
+  Array.prototype.forEach.call(
+    buttonNodes,
+    function (button) {
+      button.addEventListener("click", removeStationHandler.bind(this));
+    }.bind(this)
+  );
+}
+
+export function makeTableStation(table) {
+  table.innerHTML += `<thead>
+    <th>역 이름 </th> <th>설정</th>
+  </thead>`;
+  this.station.map((v) => {
+    let row = `<tr data-id=${v.id}>
+    <td>${v.name}</td>
+    <td> <button class="station-delete-button">삭제</button></td>
+  </tr>`;
+    table.innerHTML += row;
+  });
+  tableButton.call(this, document.querySelectorAll(".station-delete-button"));
+}
+
+export function printStation() {
+  let table;
+  if (!document.querySelector("table")) {
+    table = document.createElement("table");
+    document.getElementById("app").children[STATION_DIV].append(table);
+  }
+  table = document.querySelector("table");
+  table.innerHTML = "";
+  makeTableStation.call(this, table);
+}
--- src/utils/eventHandler.js
+++ src/utils/eventHandler.js
@@ -1,7 +1,27 @@
 import { STATION_DIV, LINE_DIV, SELECTION_DIV } from "../constant.js";
-import { controlDisplay, cleanPreView, cleanView } from "./controlView.js";
+import {
+  controlDisplay,
+  cleanPreView,
+  cleanView,
+  printStation,
+} from "./controlView.js";
 import { getDataFromLocalStorage } from "./getDataFromLocalStorage.js";
 import { ERR_MESSAGE_STATION } from "../constant.js";
+
+export function stationAddHandler() {
+  const stationNameInput = document.getElementById("station-name-input");
+  if (this.isValidStation(stationNameInput.value)) {
+    this.station.push({
+      id: Math.random().toString(36).substr(2, 16),
+      name: stationNameInput.value,
+    });
+    printStation.call(this);
+    stationNameInput.value = "";
+  } else {
+    alert(ERR_MESSAGE_STATION);
+    stationNameInput.value = "";
+  }
+}
 
 export function stationEventHandler(e) {
   getDataFromLocalStorage(this);
@@ -25,12 +45,10 @@ export function mapPrintHandler() {
   cleanView();
 }
 
-export function stationAddHandler() {
-  const stationNameInput = document.getElementById("station-name-input");
-  if (this.isValidStation(stationNameInput.value)) {
-    this.station.push({ id: 1, name: stationNameInput.value });
-    console.log(this.station);
-  } else {
-    alert(ERR_MESSAGE_STATION);
-  }
+export function removeStationHandler(e) {
+  let tr = e.target.parentNode.parentNode;
+  const clearStation = this.station.filter((v) => v.id !== tr.dataset.id);
+  this.station = clearStation;
+  console.log(clearStation);
+  printStation.call(this);
 }
