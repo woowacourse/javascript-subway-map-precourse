b8b25830586a3a750e937a6877f81db10301fdbd
SEONBIN <tjsqls2003@gmail.com>
2020-12-14 14:15:19 +0900

    feat(index.js): add load/save data in localStorage

--- src/index.js
+++ src/index.js
@@ -8,11 +8,27 @@ const lineManagerBtn = document.getElementById("line-manager-button");
 const sectionManagerBtn = document.getElementById("section-manager-button");
 const mapPrintManagerBtn = document.getElementById("map-print-manager-button");
 
+const loadLocalStorage = () => {
+  stationManagerBtn.dataset.stations = localStorage.getItem("station")
+    ? localStorage.getItem("station")
+    : "[]";
+  lineManagerBtn.dataset.lines = localStorage.getItem("lines")
+    ? localStorage.getItem("lines")
+    : "[]";
+};
+
+const saveLocalStorage = () => {
+  localStorage.setItem("station", stationManagerBtn.dataset.stations);
+  localStorage.setItem("lines", lineManagerBtn.dataset.lines);
+};
+
 export default function SubwayManager() {
+  window.addEventListener("load", loadLocalStorage);
   stationManagerBtn.addEventListener("click", StationManager);
   lineManagerBtn.addEventListener("click", LineManager);
   sectionManagerBtn.addEventListener("click", SectionManager);
   mapPrintManagerBtn.addEventListener("click", MapPrintManager);
+  window.addEventListener("beforeunload", saveLocalStorage);
 }
 
 new SubwayManager();
