c602b6f6d9fd55149e58da28c1513f03850d4229
Sunmon <blackmilk274@gmail.com>
2020-12-14 23:20:46 +0900

    refactor: 공통 유틸리티 클래스(CommonUtils) 구현
    - 모든 클래스에서 공통으로 사용할 수 있는 유틸리티 클래스 구현
    - isEmpty 메소드 추가

---
+++ src/common/utils.js
@@ -0,0 +1,20 @@
+/**
+ * 모든 파일에서 참고하여 사용할 수 있는 모듈
+ */
+export default class CommonUtils {
+  static isEmpty(something) {
+    if (something === undefined || something === null) {
+      return true;
+    }
+    if (something === '' || something === '') {
+      return true;
+    }
+    if (something.constructor === Object) {
+      return Object.keys(something).length === 0;
+    }
+    if (something.constructor === Array) {
+      return something.length === 0;
+    }
+    return false;
+  }
+}
