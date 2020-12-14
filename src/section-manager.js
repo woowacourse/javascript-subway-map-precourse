a9c21f304b9f30c80275a3ce6aac50272240cdc5
lulu-kang <dalkang.hy@gmail.com>
2020-12-14 22:37:58 +0900

    feat: 등록된 노선 버튼으로 띄우기 함수 구현

--- src/section-manager.js
+++ src/section-manager.js
@@ -1,9 +1,22 @@
 export default function SectionManager() {
+  this.addSelectButton = function() {
+    let i;
+    for (i = 0; i < localStorage.length; i++) {
+      const key = localStorage.key(i);
+      const parsedObject = JSON.parse(localStorage.getItem(key));
+      if (parsedObject.position === "line") {
+        const select = document.querySelector("#select")
+        select.innerHTML += `<button type="button" class="section-line-menu-button">${localStorage.key(i)}</button>`
+      }
+    }
+  }
+
   this.init = function() {
    const section = document.querySelector("#section");
    const table = document.querySelector("#table");
    section.style.display = "none";
    table.style.display = "none";
+   this.addSelectButton();
   }
   this.init();
 }
