e1885cfdeb451dd5c7240e4e541d292e1a8b7770
lulu-kang <dalkang.hy@gmail.com>
2020-12-15 03:58:40 +0900

    refacotr: 지하철 구간 추가 기능 리팩토링

--- src/section-manager.js
+++ src/section-manager.js
@@ -1,5 +1,5 @@
 export default function SectionManager() {
-  this.addShowEvent = function() {
+  this.addShowEvent = function(key) {
     const sectionLineMenuButton = document.getElementsByClassName("section-line-menu-button");
     let i;
     for (i = 0; i < sectionLineMenuButton.length; i++) {
@@ -7,23 +7,19 @@ export default function SectionManager() {
       sectionLineMenuButton[i].addEventListener("click", () => {
         const section = document.querySelector("#section")
         const lineText = document.querySelector("#line-text")
-        const table = document.querySelector("#table")
         section.style.display = "block"
         lineText.innerText = `${lineName} 관리`
-        table.style.display = "block"; 
       })
     }
   }
 
   this.addSelectButton = function() {
+    const lines = JSON.parse(localStorage.getItem("line"))
     let i;
-    for (i = 0; i < localStorage.length; i++) {
-      const key = localStorage.key(i);
-      const parsedObject = JSON.parse(localStorage.getItem(key));
-      if (parsedObject.position === "line") {
-        const select = document.querySelector("#select")
-        select.innerHTML += `<button type="button" data-button-id="${key}" class="section-line-menu-button">${localStorage.key(i)}</button>`
-      }
+    for (i = 0; i < lines.length; i++) {
+      const key = lines[i].name
+      const select = document.querySelector("#select")
+      select.innerHTML += `<button type="button" data-button-id="${key}" class="section-line-menu-button">${key}</button>`
       this.addShowEvent(key);
     }
   }
