96f6c16cdd7acd8ac58bb49225e3648c82087957
2SOOY <1401660@naver.com>
2020-12-15 06:09:06 +0900

    Fix - 입력 구간에 대해 검증하는 함수 수정
    - 0도 허용해야하기 때문에 insNaN 함수를 사용
    - isNaN을 사용하면 빈 값은 걸러주고 0은 통과시킴

--- src/utils/validations/sectionValidation.js
+++ src/utils/validations/sectionValidation.js
@@ -1,9 +1,9 @@
 import { MESSAGE } from "../constants/message.js";
 import { alertMessage } from "../domUtil.js";
-import { isDuplicatedValue, isEmptyValue, isPossibleIndex } from "./index.js";
+import { isDuplicatedValue, isPossibleIndex } from "./index.js";
 
 export const isValidSectionNumber = ($input, nameList, name, index) => {
-  if (isEmptyValue(index)) {
+  if (isNaN(index)) {
     alertMessage($input, MESSAGE.EMPTY_NUMBER_ERROR);
     return false;
   }
