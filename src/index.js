5ffa85de1b312794f90f979a1c64233b6aac8b2a
dande <7bdande@gmail.com>
2020-12-14 12:17:51 +0900

    feat: SectionManager 인스턴스 생성

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import StationManager from "./modules/StationManager.js";
 import LineManager from "./modules/LineManager.js";
+import SectionManager from "./modules/SectionManager.js";
 
 const managerButtons = document.querySelectorAll(
   ".manager-button-group button"
 );
 const stationManagerButton = document.querySelector("#station-manager-button");
 const lineManagerButton = document.querySelector("#line-manager-button");
+const sectionManagerButton = document.querySelector("#section-manager-button");
 
 function activeSelectedContainer(container, isSelected) {
   if (isSelected) {
     container.classList.add("active");
   }
+}
+
+function hideContainer(container) {
+  container.classList.remove("active");
 }
 
 function checkSelectedContainer(selectedButton) {
@@ -24,8 +30,10 @@ function checkSelectedContainer(selectedButton) {
 }
 
 function handleManagerButton(e) {
+  const addSectionContainer = document.querySelector("#add-section-container");
   const selectedButton = e.target.dataset.title;
   checkSelectedContainer(selectedButton);
+  hideContainer(addSectionContainer);
 }
 
 managerButtons.forEach(managerButton =>
@@ -34,3 +42,4 @@ managerButtons.forEach(managerButton =>
 
 stationManagerButton.addEventListener("click", () => new StationManager());
 lineManagerButton.addEventListener("click", () => new LineManager());
+sectionManagerButton.addEventListener("click", () => new SectionManager());
