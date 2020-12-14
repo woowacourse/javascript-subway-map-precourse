7eced0bd0c6069c2462a6bac79b8f46a755ffe4b
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2020-12-15 03:41:22 +0900

    refactor : 코드 간결하게 수정

--- src/utils/validator/lineValidator.js
+++ src/utils/validator/lineValidator.js
@@ -1,31 +1,23 @@
 import lineStorage from '../lineStorage.js';
 
 const isEqualName = (lines, lineName) => {
-  if (lines.some((line) => line.name === lineName)) {
-    return alert('중복된 노선 이름입니다.');
-  }
-  return true;
+  return lines.some((line) => line.name === lineName) ? alert('중복된 노선 이름입니다.') : true;
 };
 
 const isNameNotNull = (lineName) => {
-  if (!lineName) {
-    return alert('노선 이름을 입력해주세요');
-  }
-  return true;
+  return !lineName ? alert('노선 이름을 입력해주세요') : true;
 };
 
 const isCorrectName = (lineName) => {
-  if (lineName.split('')[lineName.length - 1] !== '선') {
-    return alert("노선의 이름은 '선'으로 끝나야 합니다");
-  }
-  return true;
+  return lineName.split('')[lineName.length - 1] !== '선'
+    ? alert("노선의 이름은 '선'으로 끝나야 합니다")
+    : true;
 };
 
 const isEqualPoints = (startStationName, endStationName) => {
-  if (startStationName === endStationName) {
-    return alert('상행 종점과 하행 종점은 서로 달라야 합니다');
-  }
-  return true;
+  return startStationName === endStationName
+    ? alert('상행 종점과 하행 종점은 서로 달라야 합니다')
+    : true;
 };
 
 const isEqualLine = (startStationName, endStationName) => {
