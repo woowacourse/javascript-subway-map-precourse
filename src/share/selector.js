6959eb2a210d1203526bad0e11fdbb459e46d109
nxxc <nomad.nxxc@gmail.com>
2020-12-14 15:05:45 +0900

    [feat] 프로젝트 기본 셋팅 & HTML 마크업
    - css selector 기반으로 HTML 마크업을 작성하였다.
    - 프로젝트의 기본 폴더를 구성하였다.

---
+++ src/share/selector.js
@@ -0,0 +1,52 @@
+export const MENU = Object.freeze({
+  MENU_CONTAINER_ID: 'menu',
+  STATION_MANGER_BUTTON_ID: 'station-manager-button',
+  LINE_MANAGER_BUTTON_ID: 'line-manager-button',
+  SECTION_MANAGER_BUTTON_ID: 'section-manager-button',
+  MAP_PRINT_MANAGER_BUTTON_ID: 'map-print-manager-button',
+});
+
+export const STATION = Object.freeze({
+  STATION_MANAGER_CONTAINER_ID: 'station-manager',
+  STATION_FORM_ID: 'station-form',
+  STATION_RESULT_CONTAINER_ID: 'station-result',
+  STATION_TABLE: 'station-table',
+  STATION_TABLE_BODY: 'station-table-body',
+  STATION_NAME_INPUT_ID: 'station-name-input',
+  STATION_ADD_BUTTON_ID: 'station-add-button',
+  STATION_DELETE_BUTTON_CLASS: 'station-delete-button',
+});
+
+export const LINE = Object.freeze({
+  LINE_MANAGER_CONTAINER_ID: 'line-manager',
+  LINE_FORM_ID: 'line-form',
+  LINE_NAME_INPUT_ID: 'line-name-input',
+  LINE_START_STATION_SELECTOR_ID: 'line-start-station-selector',
+  LINE_END_STATION_SELECTOR: 'line-end-station-selector',
+  LINE_ADD_BUTTON_ID: 'line-add-button',
+  LINE_DELETE_BUTTON_CLASS: 'line-delete-button',
+  LINE_RESULT_CONTAINER_ID: 'line-result',
+  LINE_TABLE: 'line-table',
+  LINE_TABLE_BODY: 'line-table-body',
+});
+
+export const SECTION = Object.freeze({
+  SECTION_MANAGER_CONTAINER_ID: 'section-manager',
+  SECTION_LINE_MENU_ID: 'section-line-menu',
+  SECTION_LINE_MENU_BUTTON_CLASS: 'section-line-menu-button',
+  SECTION_DETAIL_CONTAINER_ID: 'section-detail',
+  SECTION_FORM_ID: 'section-form',
+  SECTION_FORM_HEADER_ID: 'section-form-header',
+  SECTION_STATION_SELECTOR_ID: 'section-station-selector',
+  SECTION_ORDER_INPUT_ID: 'section-order-input',
+  SECTION_ADD_BUTTON_ID: 'section-add-button',
+  SECTION_DELETE_BUTTON: 'section-delete-button',
+  SECTION_RESULT_CONTAINER_ID: 'section-result',
+  SECTION_TABLE: 'section-table',
+  SECTION_TABLE_BODY: 'section-table-body',
+});
+
+export const MAP = Object.freeze({
+  MAP_PRINT_MANAGER_CONTAINER_ID: 'map-print-manager',
+  MAP_CLASS: 'map',
+});
