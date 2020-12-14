30fda651792ca44e94d49371c4c27c6f1fcaa8df
365kim <365listener@gmail.com>
2020-12-14 14:53:54 +0900

    feat: '지하철 노선도 출력' 메뉴선택 시 전체 노선도 출력

--- src/managers/map-print/launcher.js
+++ src/managers/map-print/launcher.js
@@ -1,4 +1,22 @@
-// 3. 지하철 노선도 출력
+import { getArrayFromLocalStorage } from '../../util/util-local-storage.js';
+import { appendNew, emptyElement } from '../../util/util-ui.js';
+
+// 4. 지하철 노선도 출력
 export const launchMapPrintManager = (menu, container) => {
-  console.log('added4');
+  emptyElement(container);
+  printSubwayMap(menu, container);
+};
+
+const printSubwayMap = (menu, container) => {
+  const lineList = getArrayFromLocalStorage('line');
+  const subContainer = appendNew('div', container, '', `${menu}`);
+  lineList.map((line) => printLine(subContainer, line));
+};
+
+const printLine = (subContainer, line) => {
+  let ul;
+
+  appendNew('h2', subContainer, line.name);
+  ul = appendNew('ul', subContainer);
+  line.stationList.map((stationName) => appendNew('li', ul, stationName));
 };
