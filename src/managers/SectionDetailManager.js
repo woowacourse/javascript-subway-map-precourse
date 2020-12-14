1973220600e3c497b1c1c2518228cc2e4eba1bc9
nxxc <nomad.nxxc@gmail.com>
2020-12-14 23:14:10 +0900

    [feat] 사용자가 등록한 노선을 버튼으로 표시
    - 구간 관리 메뉴 선택시 사용자가 등록한 노선을 선택할 수 있도록 표시함.

---
+++ src/managers/SectionDetailManager.js
@@ -0,0 +1,7 @@
+import Component from '../factory/Component.js';
+
+export default class SectionDetailManager extends Component {
+  constructor(props) {
+    super(props);
+  }
+}
--- src/managers/SectionManager.js
+++ src/managers/SectionManager.js
@@ -1,7 +1,29 @@
 import Component from '../factory/Component.js';
+import { SECTION_SELECTOR } from '../share/selector.js';
+import SectionDetailManager from './SectionDetailManager.js';
 
 export default class SectionManager extends Component {
   constructor(props) {
     super(props);
+
+    this.sectionLineMenu = this.container.querySelector(
+      `#${SECTION_SELECTOR.LINE_MENU_ID}`,
+    );
+    this.sectionDetailManager = new SectionDetailManager({
+      containerId: SECTION_SELECTOR.DETAIL_CONTAINER_ID,
+      syncData: this.props.syncData,
+    });
+
+    this.sectionDetailManager.hide();
+  }
+
+  template() {
+    return this.state.lineList
+      .map((line) => `<button>${line.name}호선</button>`)
+      .join('');
+  }
+
+  render() {
+    this.sectionLineMenu.innerHTML = this.template();
   }
 }
