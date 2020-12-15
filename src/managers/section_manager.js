b4f4268cecb2966c236413fd0c1346feffa76f12
dawit jeong <wjdekdnlt1215@gmail.com>
2020-12-15 22:00:40 +0900

    add isExist, isOrderCorrect

--- src/managers/section_manager.js
+++ src/managers/section_manager.js
@@ -1,1 +1,7 @@
-const SectionManager = function () {};
+const SectionManager = function () {
+  this.isExist = (sectionName, sections) =>
+    sections.indexOf(sectionName) !== -1;
+
+  this.isOrderCorrect = (order, sectionLength) =>
+    order >= 0 && order <= sectionLength;
+};
