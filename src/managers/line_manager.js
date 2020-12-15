5af94d22dc6e12b805abaa91437223ca134ee3dd
dawit jeong <wjdekdnlt1215@gmail.com>
2020-12-15 20:17:45 +0900

    add getSelectors

--- src/managers/line_manager.js
+++ src/managers/line_manager.js
@@ -1,1 +1,6 @@
-const LineManager = function () {};
+const LineManager = function () {
+  this.getSelectors = () => [
+    document.getElementById("line-start-station-selector"),
+    document.getElementById("line-end-station-selector"),
+  ];
+};
