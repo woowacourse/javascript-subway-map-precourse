33f4186c16b7eecb8e899a42f548f678dc40e427
kiminseob <kis6473@naver.com>
2020-12-15 16:12:28 +0900

    feat: 지하철 구간 등록 예외 검증 기능 구현
    - 순서의 범위는 1이상 하행종점 순서 이하여야 한다.

---
+++ src/main/subway-section.js
@@ -0,0 +1,29 @@
+import {SECTION, STORAGE} from '../constants.js';
+import {getStations, getLines} from './subway-local-storage.js';
+
+export default class SubwaySection {
+  constructor() {
+    this.stationList = getStations(STORAGE.STATION.KEY);
+    this.lineList = getLines(STORAGE.LINE.KEY);
+    this.lineName = '';
+  }
+
+  setCurrentLineName = (lineName) => {
+    this.lineName = lineName;
+  }
+
+  isValidOrder(order) {
+    return this.hasValidOrder(order);
+  }
+
+  hasValidOrder(order) {
+    if (isNaN(order)
+      || order <= 0
+      || order.length === 0
+      || order >= this.lineList[this.lineName].length) {
+      return false;
+    };
+
+    return true;
+  }
+}
