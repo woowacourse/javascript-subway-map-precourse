ef2b95359968f1b304ee0826323f6f3201c3ac58
dawit jeong <wjdekdnlt1215@gmail.com>
2020-12-15 21:14:30 +0900

    add isContainedInLengthTwoSections

--- src/managers/station_manager.js
+++ src/managers/station_manager.js
@@ -1,1 +1,15 @@
-const StationManager = function () {};
+import { getFormattedLines } from "../common/function";
+
+const StationManager = function () {
+  this.isContainedInLengthTwoSections = (stationName) => {
+    let result = false;
+    getFormattedLines().forEach((line) => {
+      if (
+        line.sections.length === 2 &&
+        line.sections.indexOf(stationName) !== -1
+      )
+        result = true;
+    });
+    return result;
+  };
+};
