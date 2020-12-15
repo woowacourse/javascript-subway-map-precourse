fc32f3f35bd9e32807cb0e222c95054b398c9cb9
cuza22 <yeji.woo97@gmail.com>
2020-12-15 13:29:32 +0900

    [index.js] final

--- src/index.js
+++ src/index.js
@@ -1,16 +1,35 @@
-import {MENU,TEMPLATE} from './constants.js';
-import {clearTemplate,setTemplateVisible} from './template.js';
+import {MENU, TEMPLATE} from './constants.js';
+import {initStorage} from './util/storageAccess.js';
+import {clearTemplate, setTemplateVisible} from './ui/init-ui.js';
+import {resetStationTable} from './ui/station-ui.js';
+import {resetLineTable, resetStartOption, resetEndOption} from './ui/line-ui.js';
+import {resetLineButtons} from './ui/section/section-selection-ui.js';
+import {createMap} from './ui/map-ui.js';
 
-MENU.STATION.addEventListener('click', (e) => {
-    clearTemplate();
-    setTemplateVisible(TEMPLATE.STATION);
+initStorage();
+
+MENU.STATION.addEventListener('click', () => {
+  clearTemplate();
+  resetStationTable();
+  setTemplateVisible(TEMPLATE.STATION);
 });
-MENU.LINE.addEventListener('click', (e) => {
-    clearTemplate();
-    setTemplateVisible(TEMPLATE.LINE);});
-MENU.SECTION.addEventListener('click', (e) => {
-    clearTemplate();
-    setTemplateVisible(TEMPLATE.SECTION);});
-MENU.MAP.addEventListener('click', (e) => {
-    clearTemplate();
-    setTemplateVisible(TEMPLATE.MAP);});
+
+MENU.LINE.addEventListener('click', () => {
+  clearTemplate();
+  resetLineTable();
+  resetStartOption();
+  resetEndOption();
+  setTemplateVisible(TEMPLATE.LINE);
+});
+
+MENU.SECTION.addEventListener('click', () => {
+  clearTemplate();
+  resetLineButtons();
+  setTemplateVisible(TEMPLATE.SECTION);
+});
+
+MENU.MAP.addEventListener('click', () => {
+  clearTemplate();
+  createMap();
+  setTemplateVisible(TEMPLATE.MAP);
+});
