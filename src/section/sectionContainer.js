b8eadf15dc4a23240f8872790f29165421de4da5
ginger-kang <kdhoon07@gmail.com>
2020-12-15 18:25:45 +0900

    feat: get line data and station data from localStorage

---
+++ src/section/sectionContainer.js
@@ -0,0 +1,19 @@
+export default function sectionContainer() {
+  let lineData = [];
+  let stationData = [];
+
+  const getLocalData = () => {
+    stationData = JSON.parse(window.localStorage.getItem("stationList"));
+    lineData = JSON.parse(window.localStorage.getItem("lineList"));
+  };
+
+  const init = () => {
+    const sectionContainer = document.querySelector(
+      "#section-manager-container",
+    );
+    getLocalData();
+    sectionContainer.style.display = "block";
+  };
+
+  init();
+}
