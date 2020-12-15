9d08421584b4f7e28d83593dc4650bcd6dbb336c
Lisboa <suna9267@gmail.com>
2020-12-15 23:24:29 +0900

    local strage에 있는 기존의 역과 노선인지 체크하여 페이지에 출력하는 함수 생성

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 
 window.arrStations = new Array();
 window.arrLine = new Array();
+
+
+
+function GetStationsArray() {
+    if(localStorage.getItem("stations") != null) {
+        arrStations = JSON.parse(localStorage.getItem("stations"));
+        for(let i = 0; i < arrStations.length; i++) {
+            createStationHTML(arrStations[i]);
+        }
+    }
+}
+
+function GetLinesArray() {
+    if(localStorage.getItem("lines") != null) {
+        arrLine  = JSON.parse(localStorage.getItem("lines"));
+        for(let i=0; i < arrLine.length; i++) {
+            makeTableOfLine(arrLine[i]);
+            let length = arrLine [i].section.stations.length;
+            if(length > 0) {
+                createLineHTML(arrLine[i], arrLine[i].name, 
+                                arrLine[i].section.stations[0], arrLine[i].section.stations[length - 1]);
+                createStationInSectionHTML(arrLine[i]);
+            }
+        }
+    }
+}
