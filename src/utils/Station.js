abf48efb8add1f96e031670dfa02efc2833f35f1
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2020-12-15 16:28:31 +0900

    refactor : 사용하지 않는 메소드 삭제

--- src/utils/Station.js
+++ src/utils/Station.js
@@ -2,12 +2,4 @@ export default function Station(id, name) {
   this.id = id;
   this.name = name;
   this.line = [];
-
-  this.setLine = (lineName) => {
-    if (this.line.includes(lineName)) {
-      alert(`${lineName}에 이미 ${name}역이 포함되어 있습니다.`);
-    } else {
-      this.line.push(lineName);
-    }
-  };
 }
