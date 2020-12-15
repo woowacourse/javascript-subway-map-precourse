50f1f8c05edf79c498ca220de7c764b1cdd655d9
dawit jeong <wjdekdnlt1215@gmail.com>
2020-12-15 21:42:47 +0900

    add createSectionAddSelect, createSectionAddInput, createSectionAddButton, createTr, createDeleteButton

--- src/managers/section_manager.js
+++ src/managers/section_manager.js
@@ -1,1 +1,32 @@
-const SectionManager = function () {};
+import { appendChildren, getAdvancedEle } from "../common/visualization";
+
+const SectionManager = function () {
+  this.createSectionAddSelect = () =>
+    getAdvancedEle("select", { id: "section-selector" });
+
+  this.createSectionAddInput = () =>
+    getAdvancedEle("input", {
+      id: "section-order-input",
+      type: "number",
+      placeholder: "순서",
+    });
+
+  this.createSectionAddButton = () =>
+    getAdvancedEle("button", { id: "section-add-button" }, "등록");
+
+  this.createTr = (indexTd, sectionTd, deleteButtonTd) => {
+    const tr = document.createElement("tr");
+    appendChildren(tr, indexTd, sectionTd, deleteButtonTd);
+    return tr;
+  };
+
+  this.createDeleteButton = (section) =>
+    getAdvancedEle(
+      "button",
+      {
+        class: "section-delete-button",
+        "data-section-name": section,
+      },
+      "노선에서 제거"
+    );
+};
