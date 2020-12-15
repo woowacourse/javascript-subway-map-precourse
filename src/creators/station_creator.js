930fa9eca80c7567b06b1e35de1a9b4a4bf01a45
dawit jeong <wjdekdnlt1215@gmail.com>
2020-12-15 21:03:27 +0900

    add createAddWrapper, createAddTitle, createAddInput, createAddButton

--- src/creators/station_creator.js
+++ src/creators/station_creator.js
@@ -1,1 +1,17 @@
-const StationCreator = function () {};
+import { getAdvancedEle } from "../common/visualization";
+
+const StationCreator = function () {
+  this.createAddWrapper = () =>
+    getAdvancedEle("div", { id: "station-add-wrapper" });
+
+  this.createAddTitle = () => getAdvancedEle("div", null, "역 이름");
+
+  this.createAddInput = () =>
+    getAdvancedEle("input", {
+      id: "station-name-input",
+      placeholder: "역 이름을 입력해주세요.",
+    });
+
+  this.createAddButton = () =>
+    getAdvancedEle("button", { id: "station-add-button" }, "역 추가");
+};
