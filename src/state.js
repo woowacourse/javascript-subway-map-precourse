89ccdebad82ca4d26171fc90ee37ec2c6243d0fd
dawit jeong <wjdekdnlt1215@gmail.com>
2020-12-15 22:43:58 +0900

    update state

--- src/state.js
+++ src/state.js
@@ -6,7 +6,6 @@ const State = function () {
     lines: getLocalStorageDataBy("lines") || "[]",
     currentPage: "0",
     selectedLineIndex: null,
-    alreadySelectionButtonClicked: false,
   };
   // currentPage "0": 초기 상태, "1": 역 관리, "2": 노선 관리, "3": 구간 관리, "4": 지하철 노선도 관리
   this.setState = (dataName, nextState) => (this.state[dataName] = nextState);
