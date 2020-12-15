743a6fbe1d76cb0c4ecb4357ca5bd11b68f15dbb
sunyoung <sunnyk5780@gmail.com>
2020-12-15 15:23:26 +0900

    Refactor: change to manage stations state by App

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,52 @@
 import Header from "./Header.js";
 import Navigator from "./Navigator.js";
 import StationManager from "./StationManager.js";
+import localStorageManager from "../util/localStorage.js";
+import { STORAGE_KEY } from "../util/constants.js";
 
 export default function App($app) {
   this.$app = $app;
-
   this.header = new Header({ $target: this.$app });
   this.navigator = new Navigator({ $target: this.$app });
 
   this.$main = document.createElement("main");
   this.$app.append(this.$main);
 
+  this.stations = localStorageManager.getItem({
+    key: STORAGE_KEY.station,
+    defaultValue: [],
+  });
+
+  this.onAddStation = (stationName) => {
+    const nextStations = [...this.stations, stationName];
+    this.setState({ nextStations });
+  };
+
+  this.onDeleteStation = (stationIndex) => {
+    const nextStations = [...this.stations];
+    nextStations.splice(stationIndex, 1);
+
+    this.setState({ nextStations });
+  };
+
   this.stationManager = new StationManager({
     $target: this.$main,
+    stations: this.stations,
     isShow: true,
+    onAddStation: this.onAddStation,
+    onDeleteStation: this.onDeleteStation,
   });
+
+  this.setState = ({ nextStations }) => {
+    if (nextStations) {
+      this.stations = nextStations;
+
+      localStorageManager.setItem({
+        key: STORAGE_KEY.station,
+        item: nextStations,
+      });
+
+      this.stationManager.setState({ nextStations: this.stations });
+    }
+  };
 }
--- src/components/StationManager.js
+++ src/components/StationManager.js
@@ -1,54 +1,33 @@
 import StationNameInput from "./StationNameInput.js";
 import StationList from "./StationList.js";
-import localStorageManager from "../util/localStorage.js";
-import { STORAGE_KEY } from "../util/constants.js";
 
-export default function StationManager({ $target, isShow }) {
+export default function StationManager({ $target, stations, isShow, onAddStation, onDeleteStation }) {
   this.$container = document.createElement("div");
   this.$container.className = "station-management";
   $target.append(this.$container);
 
   this.isShow = isShow;
-  this.stations = localStorageManager.getItem({
-    key: STORAGE_KEY.station,
-    defaultValue: [],
-  });
+  this.stations = stations;
 
   this.isExistStationName = (stationName) => {
     return this.stations.includes(stationName);
   };
 
-  this.onAddStation = (stationName) => {
-    const nextStations = [...this.stations, stationName];
-    this.setState({ nextStations });
-  };
-
-  this.onDeleteStation = (stationIndex) => {
-    const nextStations = [...this.stations];
-    nextStations.splice(stationIndex, 1);
-
-    this.setState({ nextStations });
-  };
-
   this.stationNameInput = new StationNameInput({
     $target: this.$container,
     isExistStationName: this.isExistStationName,
-    onAddStation: this.onAddStation,
+    onAddStation: onAddStation,
   });
+
   this.stationList = new StationList({
     $target: this.$container,
     stations: this.stations,
-    onDeleteStation: this.onDeleteStation,
+    onDeleteStation: onDeleteStation,
   });
 
   this.setState = ({ nextIsShow, nextStations }) => {
     if (nextStations) {
       this.stations = nextStations;
-      localStorageManager.setItem({
-        key: STORAGE_KEY.station,
-        item: nextStations,
-      });
-
       this.stationList.setState({ nextStations: this.stations });
     }
 
