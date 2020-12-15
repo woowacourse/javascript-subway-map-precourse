f56aca343629703c13ccb3eb2886201cba303bf2
nxxc <nomad.nxxc@gmail.com>
2020-12-15 12:47:32 +0900

    [feat] 구간 수정시 노선의 최소 길이 확인
    - 노선은 최소 2개역을 포함해야 하기 때문에 길이가 2인경우 삭제가 불가능
    하게 함

--- src/factory/Line.js
+++ src/factory/Line.js
@@ -1,4 +1,8 @@
-import { checkOverlap } from '../share/utils.js';
+import { checkOverlap, checkValueLength } from '../share/utils.js';
+
+const MIN_SECTION_LENGTH = 2;
+const ALERT_MESSAGE_SECTION_INCLUDES_STATION = '이미 노선에 포함되어있습니다';
+const ALERT_MESSAGE_SECTION_MINLENGTH = '노선은 최소 2개역을 포함해야 합니다.';
 
 export default class Line {
   constructor(props) {
@@ -13,7 +17,7 @@ export default class Line {
 
   addStationToSection({ stationName, index }) {
     if (!checkOverlap(stationName, this.section)) {
-      alert('이미 노선에 포함되어있습니다');
+      alert(ALERT_MESSAGE_SECTION_INCLUDES_STATION);
       return;
     }
     this.section.splice(index, 0, stationName);
@@ -22,10 +26,16 @@ export default class Line {
   }
 
   deleteStationFromSection({ index }) {
+    if (this.checkSectionLength()) {
+      alert(ALERT_MESSAGE_SECTION_MINLENGTH);
+      return;
+    }
     this.section.splice(index, 1);
     this.updateStartStation();
     this.updateEndStation();
   }
+
+  checkSectionLength = () => this.section.length === MIN_SECTION_LENGTH;
 
   updateStartStation() {
     [this.startStation] = this.section;
