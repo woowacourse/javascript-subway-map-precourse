21ac8617f3c6c73d5346214050e43ba04e1b4c7a
lulu-kang <dalkang.hy@gmail.com>
2020-12-15 09:14:49 +0900

    feat: 지하철 노선도 출력 함수 구현

--- src/map-manager.js
+++ src/map-manager.js
@@ -1,5 +1,27 @@
 export default function Map() {
+  this.printMapData = function(lineName, lineArray, parsedLineLength) {
+    let map = document.querySelector(".map")
+    map.innerHTML += `<h3>${lineName}</h3><ul><li>${lineArray[0]}</li><li>${lineArray[parsedLineLength - 1]}</li></ul>`
+  }
 
+  this.getMapData = function() {
+    const key = "line";
+    const parsedMapData = JSON.parse(localStorage.getItem(key));
+    let i;
+    
+    for (i = 0; i < parsedMapData.length; i++) {
+      const lineName = parsedMapData[i].name
+      const lineArray = parsedMapData[i].line
+      const parsedLineLength = parsedMapData[i].line.length
+      this.printMapData(lineName, lineArray, parsedLineLength)
+    }
+  }
+
+  this.init = function() {
+    this.getMapData();
+  }
+
+  this.init();
 }
 
 new Map();
