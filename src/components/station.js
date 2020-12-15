cbeae2a41f4a7ccaeea55739e577c5cf0fb23406
min1378 <qwes123@naver.com>
2020-12-15 22:12:29 +0900

    feat: 지하철 역을 등록할 수 있도록 구현

--- src/components/station.js
+++ src/components/station.js
@@ -1,9 +1,33 @@
 export default {
+  isAddedEvent: false,
   station: [],
   init(page) {
     this.page = page;
-    this.station = ["test"];
+    this.station = [];
     this.render();
+    if (!this.isAddedEvent) {
+      this.addEvents();
+    }
+    this.isAddedEvent = true;
+  },
+  addEvents() {
+    const $btnAdd = document.querySelector("#station-add-button");
+    $btnAdd.addEventListener("click", () => this.clickAdd());
+  },
+  clickAdd() {
+    const $input = document.querySelector("#station-name-input");
+    const { value: name } = $input;
+    if (!name) {
+      alert("역의 이름이 없습니다. 역 이름을 입력해주세요.");
+      return;
+    }
+    if (this.station.indexOf(name) > -1) {
+      alert("이미 등록된 역입니다. 다른 역을 입력해주세요.");
+      return;
+    }
+    this.station.push(name);
+    this.render();
+    $input.value = "";
   },
   render() {
     console.log(this.station);
