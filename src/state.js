10dd217727ad337f1da0bd4e5fd9a020170248ef
dawit jeong <wjdekdnlt1215@gmail.com>
2020-12-15 19:01:16 +0900

    go before the test commit

--- src/state.js
+++ src/state.js
@@ -2,20 +2,20 @@ import { getLocalStorageDataBy } from "./local_storage.js";
 
 const State = function () {
   this.state = {
-    stations: getLocalStorageDataBy("stations"),
-    lines: getLocalStorageDataBy("lines"),
+    stations: getLocalStorageDataBy("stations") || "[]",
+    lines: getLocalStorageDataBy("lines") || "[]",
     currentPage: "0",
-  }; // "0": 초기 상태, "1": 역 관리, "2": 노선 관리, "3": 구간 관리, "4": 지하철 노선도 관리
-
-  this.isEmptyPage = () => this.state.currentPage === "0";
-  this.isAlreadyRenderedPage = (page) => this.state.currentPage === page;
-
+    selectedLineIndex: null,
+    alreadySelectionButtonClicked: false,
+  };
+  // currentPage "0": 초기 상태, "1": 역 관리, "2": 노선 관리, "3": 구간 관리, "4": 지하철 노선도 관리
   this.setState = (dataName, nextState) => {
     this.state[dataName] = nextState;
   };
 
   this.resultDIV = document.getElementById("result");
-  this.clearResultDIV = () => {
-    resultDIV.innerHTML = "";
-  };
+  this.VALUE_IN_ARRAY = "valueInArray";
+  this.INDEX_OF_DATA = "indexOfData";
 };
+
+export const { VALUE_IN_ARRAY, INDEX_OF_DATA } = new State();
